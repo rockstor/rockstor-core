32c343c46af71714548804b583545e602263c1b7
Suman Chakravartula <suman@rockstor.com>
2013-06-03 19:39:50 -0700

    Add urls for sm/sprobes API such that: GET ^api/sm/sprobes/ returns metadata of all available smart probes GET ^api/sm/sprobes/probe_name/ returns metadata of all instances of the probe_name in reverse chronological order GET ^api/sm/sprobes/probe_name/id/ returns probe data for the (probe_name, id) POST ^api/sm/sprobes/probe_name/ start the given smart probe, create a new instance. POST ^api/sm/sprobes/probe_name/id/status/ return metadata for the (probe_name, id) POST ^api/sm/sprobes/probe_name/id/stop/ stop the smart probe identified by (probe_name, id)

--- src/rockstor/urls.py
+++ src/rockstor/urls.py
@@ -21,9 +21,10 @@ from storageadmin.views import (DiskView, SystemDiskView, PoolView, ShareView,
                                 SnapshotView, InfoView, SetupWizardView,
                                 ShareIscsiView, AppliancesView, LoginView,
                                 UserView, ShareSambaView, SupportView,
-                                DashboardConfigView, ShareNFSView,
-                                RecipeView)
-from smart_manager.views import (SmartManagerView, ServiceView, StapView)
+                                DashboardConfigView, ShareNFSView)
+from smart_manager.views import (SmartManagerView, ServiceView, SProbeView,
+                                 SProbeView2)
+
 
 import os.path
 import socketio.sdjango
@@ -111,19 +112,29 @@ urlpatterns = patterns('',
     url(r'^api/users/(?P<id>\d+)$', UserView.as_view(), name='user-view'),
     url(r'^api/users/$', UserView.as_view(), name='user-view'),
 
-    url(r'^api/sm/services/$', ServiceView.as_view(), name='service-view'),
-    url(r'^api/sm/services/(?P<sname>[A-Za-z_]+)/$', ServiceView.as_view(), name='service-view'),
-    url(r'^api/sm/stap/$', StapView.as_view(), name='stap-view'),
-    url(r'^api/sm/stap/(?P<tname>[A-Za-z0-9_]+)/$', StapView.as_view(), name='stap-view'),
-    url(r'^api/sm/(?P<mname>[A-Za-z0-9_]+)/$', SmartManagerView.as_view(), name='sm-view'),
+
+
     url(r'^api/support/$', SupportView.as_view(), name='support-view'),
     url(r'^api/support/(?P<caseid>[0-9]+)/$', SupportView.as_view(), name='support-view'),
 
     # Dashboard config
     url(r'^api/dashboardconfig/$', DashboardConfigView.as_view(), name='dashboardconfig-view'),
-    
-    # Recipes
-    url(r'^api/recipes/nfs/start$', RecipeView.as_view(), name='recipe-view'),
-    url(r'^api/recipes/nfs/(?P<recipe_id>[0-9]+)$', RecipeView.as_view(), name='recipe-view'),
+
+    # Smart manager stuff
+    # Services
+    url(r'^api/sm/services/$', ServiceView.as_view(), name='service-view'),
+    url(r'^api/sm/services/(?P<sname>[A-Za-z_]+)/$', ServiceView.as_view(),
+        name='service-view'),
+
+    # Smart probes
+    url(r'^api/sm/sprobes/$', SProbeView.as_view(), name='probe-view'),
+    url(r'^api/sm/sprobes/(?P<pname>[a-z-]+)/$',
+        SProbeView.as_view(), name='probe-view'),
+    url(r'^api/sm/sprobes/(?P<pname>[a-z-]+)/(?P<pid>[0-9]+)/$',
+        SProbeView2.as_view(), name='probe-view2'),
+    url(r'^api/sm/sprobes/(?P<pname>[a-z-]+)/(?P<pid>[0-9]+)/(?P<command>[a-z]+)/$',
+        SProbeView2.as_view(), name='probe-view2'),
+    url(r'^api/sm/(?P<mname>[A-Za-z0-9_]+)/$', SmartManagerView.as_view(),
+        name='sm-view'),
 
 )
