bdbaf63bcf29445938f894add57ce4506df95828
Suman Chakravartula <suman@rockstor.com>
2013-06-03 19:32:21 -0700

    Update smart manager daemon to purge data older than defined in django settings file. Refactor and enhance stap.py to make it more robust, log nicely and implement the smart probe state machine.

--- src/rockstor/smart_manager/stap.py
+++ src/rockstor/smart_manager/stap.py
@@ -30,8 +30,17 @@ from system.osi import run_command
 import models
 import zmq
 import os
+import time
+import subprocess
+import fcntl
+from smart_manager.agents import process_nfsd_calls
+from smart_manager.models import SProbe
+from django.conf import settings
 
 STAP_RUN = '/usr/bin/staprun'
+
+import logging
+logger = logging.getLogger(__name__)
 
 class Stap(Process):
 
@@ -45,7 +54,7 @@ class Stap(Process):
         context = zmq.Context()
         pull_socket = context.socket(zmq.PULL)
         pull_socket.RCVTIMEO = 500
-        pull_socket.bind('tcp://127.0.0.1:10000')
+        pull_socket.bind('tcp://%s:%d' % self.address)
         while True:
             if (os.getppid() != self.ppid):
                 break
@@ -53,25 +62,79 @@ class Stap(Process):
             task = None
             try:
                 task = pull_socket.recv_json()
+                logger.info('received task: %s' % (repr(task)))
+
+                #this happens when smart manager dies while running some probes
+                #and restarted later to clean up
+                if (task['action'] == 'stop'):
+                    continue
             except:
+                #will sleeping here help? or some other zmq based wakeup?
                 continue
 
-            cmd = [STAP_RUN, task['module'], '-c', 'sleep 10']
-            out, err, rc = run_command(cmd)
-            model = getattr(models, task['tap_class'])
-            #need to make it more generic so we can just throw a blob of data
-            #at any model in here.
-            if (task['tap'] == 'iostats'):
-                for line in out[2:]:
-                    fields = line.split()
-                    m_i = model(proc_name=fields[0], num_open=fields[1],
-                                num_read=fields[2], sum_read=fields[3],
-                                avg_read=fields[4], num_write=fields[5],
-                                sum_write=fields[6], avg_write=fields[7])
-                    self.q.put(m_i)
-            elif (task['tap'] == 'hello'):
-                m_i = model(message=out)
-                self.q.put(m_i)
+            #wait a little till the recipe instance is saved by the
+            #API. non-issue most of the time.
+            start_tap = False
+            num_tries = 0
+            while True:
+                try:
+                    ro = SProbe.objects.get(id=task['roid'])
+                    start_tap = True
+                    logger.info('start_tap is true')
+                    break
+                except:
+                    logger.error('waiting for recipe object. num_tries = %d' %
+                                 num_tries)
+                    time.sleep(1)
+                    num_tries = num_tries + 1
+                    if (num_tries > 20):
+                        break
+
+            if (not start_tap):
+                logger.error('not starting the tap')
+                ro.state = 'error'
+                self.q.put(ro)
+                continue
+
+            logger.info('running command')
+            cmd = [STAP_RUN, task['module'],]
+            rp = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE,
+                                  stderr=subprocess.PIPE)
+            fcntl.fcntl(rp.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
+            logger.info('started tap')
+            ro.state = 'running'
+            self.q.put(ro)
+            probe_stopped = False
+            while True:
+                try:
+                    part_out = rp.stdout.read()
+                    self.__process_output(part_out, task['tap'], ro)
+                    try:
+                        stop_task = pull_socket.recv_json()
+                        #should we reload from db to see 'stopped' state?
+                        rp.terminate()
+                        probe_stopped = True
+                        logger.info('received stop')
+                    except:
+                        pass
+                except IOError:
+                    #occurs when there's no output to read
+                    pass
+
+                if (rp.poll() is not None):
+                    if (probe_stopped is not True):
+                        msg = ('Probe process died. returncode: %s. '
+                               'stderr: %s' % (rp.returncode, repr(rp.stderr)))
+                        logger.error(msg)
+                        ro = SProbe.objects.get(id=task['roid'])
+                        ro.state = 'error'
+                        self.q.put(ro)
+                time.sleep(.5)
 
         pull_socket.close()
         context.term()
+        logger.info('terminated context. exiting')
+
+    def __process_output(self, part_out, tap, ro):
+        if (tap == 'nfs-distrib'):
+            process_nfsd_calls(self.q, part_out, ro, logger)
