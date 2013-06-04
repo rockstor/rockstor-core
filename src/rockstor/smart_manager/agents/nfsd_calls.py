0d65e523c5c98cd7a3e314f2d4b18b1b56cca872
Suman Chakravartula <suman@rockstor.com>
2013-06-04 12:56:14 -0700

    Add support for parsing nfsd client smart probe data

--- src/rockstor/smart_manager/agents/nfsd_calls.py
+++ src/rockstor/smart_manager/agents/nfsd_calls.py
@@ -8,23 +8,32 @@ from django.utils.timezone import utc
 def process_nfsd_calls(queue, output, ro, l):
 
     for line in output.split('\n'):
+        if (line == ''):
+            continue
         fields = line.split()
         if (len(fields) < 9):
             l.info('ignoring incomplete sprobe output: %s' % repr(fields))
             continue
         fields[0] = datetime.datetime.fromtimestamp(float(fields[0])).replace(tzinfo=utc)
-        fields.insert(0, ro)
-        if (len(fields) == 11):
-            queue.put(NFSDClientDistribution(*fields))
+        if (len(fields) == 10):
+            no = NFSDClientDistribution(rid=ro, ts=fields[0],
+                                        num_lookup=fields[1],
+                                        num_read=fields[2],
+                                        num_write=fields[3],
+                                        num_create=fields[4],
+                                        num_commit=fields[5],
+                                        num_remove=fields[6],
+                                        sum_read=fields[7],
+                                        sum_write=fields[8])
         else:
-            no = NFSDCallDistribution(rid=fields[0], ts=fields[1],
-                                      num_lookup=fields[2], num_read=fields[3],
-                                      num_write=fields[4],
-                                      num_create=fields[5],
-                                      num_commit=fields[6],
-                                      num_remove=fields[7], sum_read=fields[8],
-                                      sum_write=fields[9])
-            queue.put(no)
+            no = NFSDCallDistribution(rid=ro, ts=fields[0],
+                                      num_lookup=fields[1], num_read=fields[2],
+                                      num_write=fields[3],
+                                      num_create=fields[4],
+                                      num_commit=fields[5],
+                                      num_remove=fields[6], sum_read=fields[7],
+                                      sum_write=fields[8])
+        queue.put(no)
     return True
 
 
