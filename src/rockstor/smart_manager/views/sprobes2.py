9a494e6958e8535dde7269859f837b7e0100d3ed
Suman Chakravartula <suman@rockstor.com>
2013-06-04 12:59:25 -0700

    Add support for nfsd client smart probe to the SProbeView2

--- src/rockstor/smart_manager/views/sprobes2.py
+++ src/rockstor/smart_manager/views/sprobes2.py
@@ -29,8 +29,9 @@ from django.conf import settings
 from django.db import transaction
 from smart_manager import serializers
 from smart_manager.serializers import (SProbeSerializer,
-                                       NFSDCallDistributionSerializer)
-from smart_manager.models import NFSDCallDistribution
+                                       NFSDCallDistributionSerializer,
+                                       NFSDClientDistributionSerializer)
+from smart_manager.models import (NFSDCallDistribution, NFSDClientDistribution)
 import os
 import zmq
 
@@ -61,6 +62,10 @@ class SProbeView2(APIView):
         if (pname == 'nfs-distrib'):
             dos = NFSDCallDistribution.objects.filter(rid=ro).order_by('ts')
             return Response(NFSDCallDistributionSerializer(dos).data)
+        elif (pname == 'nfs-client-distrib'):
+            dos = NFSDClientDistribution.objects.filter(rid=ro).order_by('ts')
+            return Response(NFSDClientDistributionSerializer(dos).data)
+        return Response()
 
     @transaction.commit_on_success
     def post(self, request, pname, pid, command):
