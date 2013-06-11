c24205742e93edc59dad8de6ceb2dddaf4f65495
Madhuri Chakravarthula <mchakravartula@gmail.com>
2013-06-11 13:07:37 +0100

    Display error message in pool resize popup when No disks selected.

--- src/rockstor/templates/storageadmin/js/js/views/pool_detail.js
+++ src/rockstor/templates/storageadmin/js/js/views/pool_detail.js
@@ -113,7 +113,9 @@ PoolDetailView = Backbone.View.extend({
                     _this.$('button[rel]').overlay().close();
                   },
                   error: function(request, status, error) {
-                    alert(request.responseText);
+                    //alert(request.responseText);
+                   // showError(request.responseText)
+                    _this.$('#disk-alert').html("No disks Selected");
                   }
                 });
               });
