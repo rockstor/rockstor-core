8d81537921d68cf5dd87819ab7f4149efc83d48c
Sujeet Sreenivasan <sujeetsr@gmail.com>
2013-06-24 17:37:18 -0700

    Disk Utilization widget template and view
    Placeholder view and template for disk utilization widget

---
+++ src/rockstor/templates/storageadmin/js/js/views/dashboard/disk_utilization.js
@@ -0,0 +1,59 @@
+/*
+ *
+ * @licstart  The following is the entire license notice for the 
+ * JavaScript code in this page.
+ * 
+ * Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+ * This file is part of RockStor.
+ * 
+ * RockStor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ * 
+ * RockStor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ * 
+ */
+
+
+DiskUtilizationWidget = RockStorWidgetView.extend({
+
+  initialize: function() {
+    this.constructor.__super__.initialize.apply(this, arguments);
+    this.template = window.JST.dashboard_widgets_disk_utilization;
+    this.displayName = this.options.displayName;
+  },
+
+  render: function() {
+    // call render of base
+    this.constructor.__super__.render.apply(this, arguments);
+    var _this = this;
+    $(this.el).html(this.template({ 
+      module_name: this.module_name,
+      displayName: this.displayName
+    }));
+    return this;
+  },
+
+});
+RockStorWidgets.available_widgets.push({ 
+    name: 'disk_utilization', 
+    displayName: 'Disk Utilization', 
+    view: 'DiskUtilizationWidget',
+    description: 'Display disk utilization',
+    defaultWidget: false,
+    rows: 1,
+    cols: 2,
+    category: 'Storage', 
+});
+
+
