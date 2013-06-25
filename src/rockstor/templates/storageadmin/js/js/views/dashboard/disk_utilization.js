b794e72aa0a7b39c18034896aa8bf2f2377277ea
Sujeet Sreenivasan <sujeetsr@gmail.com>
2013-06-25 11:32:48 -0700

    Disk activity with updates
    Add d3 functions to render and update activity table.

--- src/rockstor/templates/storageadmin/js/js/views/dashboard/disk_utilization.js
+++ src/rockstor/templates/storageadmin/js/js/views/dashboard/disk_utilization.js
@@ -31,20 +31,101 @@ DiskUtilizationWidget = RockStorWidgetView.extend({
     this.constructor.__super__.initialize.apply(this, arguments);
     this.template = window.JST.dashboard_widgets_disk_utilization;
     this.displayName = this.options.displayName;
+    this.disks = new DiskCollection();
+    this.created = false;
+    this.intervalId = null;
   },
 
   render: function() {
     // call render of base
     this.constructor.__super__.render.apply(this, arguments);
     var _this = this;
-    $(this.el).html(this.template({ 
-      module_name: this.module_name,
-      displayName: this.displayName
-    }));
+    this.disks.fetch({
+      success: function(request) {
+        $(_this.el).html(_this.template({ 
+          module_name: _this.module_name,
+          displayName: _this.displayName,
+          disks: _this.disks
+        }));
+        _this.intervalId = window.setInterval(function() {
+          return function() { _this.getData(_this); }
+        }(), 2000)
+      },
+      error: function(request, response) {
+          logger.debug('failed to fetch disks in disk_utilization');
+      }
+    });
     return this;
   },
 
+  getData: function(context) {
+    var _this = context;
+    var data = [
+      {name: 'sdb', reads: Math.floor(200 + Math.random()*50), writes: Math.floor(100 + Math.random()*50)},
+      {name: 'sdc', reads: Math.floor(300 + Math.random()*50), writes: Math.floor(200 + Math.random()*50)},
+      {name: 'sdd', reads: Math.floor(50 + Math.random()*50), writes: Math.floor(400 + Math.random()*50)},
+      {name: 'sde', reads: Math.floor(10 + Math.random()*50), writes: Math.floor(20 + Math.random()*50)},
+    ];
+    if (!_this.created) {
+      _this.createRows(data); 
+      _this.$("#disk-utilization-table").tablesorter();
+    } else {
+      _this.updateRows(data); 
+    }
+
+  },
+  
+  createRows: function(data) {
+    var columns = ["name", "reads", "writes"];
+    var rows = d3.select(this.el)
+    .select("table#disk-utilization-table")
+    .select("tbody")
+    .selectAll("tr.data-utilization-row")
+    .data(data, function(d) { return d.name })
+    .enter()
+    .append("tr")
+    .attr("class","data-utilization-row");
+   
+    var cells = rows.selectAll("td")
+    .data(function(row) {
+      return _.map(columns, function(c) {
+        return {name: c, value: row[c]}
+      });
+    }, function(d) { return d.name; })
+    .enter()
+    .append("td")
+    .text(function(d) { return d.value });
+   
+    this.created = true;
+    
+  },
+
+  updateRows: function(data) {
+    var columns = ["name", "reads", "writes"];
+    var rows = d3.select(this.el)
+    .select("table#disk-utilization-table")
+    .select("tbody")
+    .selectAll("tr.data-utilization-row")
+    .data(data, function(d) { return d.name });
+   
+    var cells = rows.selectAll("td")
+    .data(function(row) {
+      return _.map(columns, function(c) {
+        return {name: c, value: row[c]}
+      });
+    }, function(d) { return d.name; })
+    .text(function(d) { return d.value });
+    
+  },
+
+  cleanup: function() {
+    if (!_.isNull(this.intervalId)) {
+      window.clearInterval(this.intervalId);
+    }
+  }
+
 });
+
 RockStorWidgets.available_widgets.push({ 
     name: 'disk_utilization', 
     displayName: 'Disk Utilization', 
