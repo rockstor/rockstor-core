cb040b55d901388579f2baf4ddca6a09b119071c
Suman Chakravartula <suman@rockstor.com>
2013-07-25 11:49:58 -0700

    Refactor smart probe views using AdvancedSProbeView parent class

---
+++ src/rockstor/smart_manager/views/nfs_share_distrib.py
@@ -0,0 +1,28 @@
+"""
+Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from advanced_sprobe import AdvancedSProbeView
+from smart_manager.models import NFSDShareDistribution
+from smart_manager.serializers import NFSDShareDistributionSerializer
+
+
+class NFSDShareDistribView(AdvancedSProbeView):
+
+    serializer_class = NFSDShareDistributionSerializer
+    model_obj = NFSDShareDistribution
+    pname = 'nfs-share-distrib'
