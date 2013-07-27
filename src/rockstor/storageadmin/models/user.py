228a2f874dae6360981e44224035b654ea4ed840
Suman Chakravartula <suman@rockstor.com>
2013-07-26 18:30:10 -0700

    Add model for User. A user can be an admin user or non-admin user. An admin user can both login via rockstor ui-s and is also a regular unix user. A non admin user is to be used for io and other needs.

---
+++ src/rockstor/storageadmin/models/user.py
@@ -0,0 +1,30 @@
+"""
+Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from django.db import models
+
+
+class User(models.Model):
+    name = models.CharField(max_length=4096, unique=True)
+    password = models.CharField(max_length=128)
+    uid = models.IntegerField()
+    gid = models.IntegerField()
+    admin = models.BooleanField(default=False)
+
+    class Meta:
+        app_label = 'storageadmin'
