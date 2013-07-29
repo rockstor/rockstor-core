07094a9faf274b304229729abc21f51374ba1c83
Sujeet Sreenivasan <sujeetsr@gmail.com>
2013-07-29 16:19:55 -0700

    Dashboard widget cleanup
    Call widget cleanup when a widget is removed from the dashboard
    Add appropriate cleanup methods in all widgets

--- src/rockstor/templates/storageadmin/js/js/rockstor.js
+++ src/rockstor/templates/storageadmin/js/js/rockstor.js
@@ -185,6 +185,9 @@ RockStorWidgetView = Backbone.View.extend({
     if (!_.isUndefined(event) && !_.isNull(event)) {
       event.preventDefault();
     }
+    // cleanup widget
+    this.cleanup();
+    // remove widget li and rearrange
     var li = $(event.currentTarget).closest('li');
     console.log(li); 
     var ul = $(this.el).closest('ul'); // list
@@ -192,12 +195,18 @@ RockStorWidgetView = Backbone.View.extend({
     li.remove();
     ul.trigger('ss-rearrange');
     this.parentView.saveWidgetConfiguration();
+    // remove from parent array
+    this.parentView.removeWidget();
   },
   
   download: function(event) {
     if (!_.isUndefined(event) && !_.isNull(event)) {
       event.preventDefault();
     }
+  },
+
+  cleanup: function() {
+    logger.debug("In RockStorWidgetView close");
   }
 
 });
