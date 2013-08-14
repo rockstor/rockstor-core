44682f39f2a24176091262054ca2438d898f9ef9
Sujeet Sreenivasan <sujeetsr@gmail.com>
2013-08-14 11:06:49 -0700

    CPU Utilization
    Add ajax call to get data from backend with group parameter
    Adjust cpu data bar width acc to max of 16cpus

--- src/rockstor/templates/storageadmin/js/js/views/dashboard/cpu_usage_widget.js
+++ src/rockstor/templates/storageadmin/js/js/views/dashboard/cpu_usage_widget.js
@@ -31,19 +31,14 @@ CpuUsageWidget = RockStorWidgetView.extend({
     this.constructor.__super__.initialize.apply(this, arguments);
     this.template = window.JST.dashboard_widgets_cpuusage;
     this.numSamples = 60;
-    this.cpu_data = [];
-    //this.modes = ['smode', 'umode', 'umode_nice', 'idle'];
+    this.maxCpus = 16;
     this.modes = ['smode', 'umode', 'umode_nice'];
     this.colors = ["#E41A1C", "#377EB8", "#4DAF4A", "#FFFFFF"];
-    //this.colors = ["#CCC1F5", "#A39BC2", "#BAB8C2", "#FFFFFF"];
-    this.numCpus = 4; // TODO get from backend
+    this.numCpus = null; 
     this.cpuData = {};
     this.avg = this.genEmptyCpuData(this.numSamples);
     this.cpuNames = [];
     this.allCpuGraphData = null;
-    for (var i=0; i < this.numSamples; i++) {
-      this.cpu_data.push({umode: 0, umode_nice: 0, smode: 0, idle: 0});
-    }
     this.updateInterval = 2000;
     this.allCpuGraphOptions = { 
       grid : { 
@@ -62,7 +57,7 @@ CpuUsageWidget = RockStorWidgetView.extend({
 			series: {
         stack: true,
         stackpercent : false,
-        bars: { show: true, barWidth: 10*this.numCpus/300, fillColor: {colors:[{opacity: 1},{opacity: 1}]}, align: "center" },
+        bars: { show: true, barWidth: 0.9, fillColor: {colors:[{opacity: 1},{opacity: 1}]}, align: "center" },
         lines: { show: false, fill: false },
         shadowSize: 0	// Drawing is faster without shadows
 			},
@@ -70,7 +65,6 @@ CpuUsageWidget = RockStorWidgetView.extend({
         min: 0, 
         max: 100,
         ticks: 4,
-        //tickLength: 2,
         tickFormatter: this.pctTickFormatter,
       },
       xaxis: { 
@@ -80,7 +74,6 @@ CpuUsageWidget = RockStorWidgetView.extend({
       legend: { show: false },
       
     }
-
 
     this.graphOptions = { 
       grid : { 
@@ -97,7 +90,6 @@ CpuUsageWidget = RockStorWidgetView.extend({
 			series: {
         stack: true,
         stackpercent : false,
-        //bars: { show: true, barWidth: 0.4, fillColor: {colors:[{opacity: 1},{opacity: 1}]}, align: "center" },
         lines: { show: true, fill: 0.5, lineWidth: 1 },
         shadowSize: 0	
 			},
@@ -117,12 +109,15 @@ CpuUsageWidget = RockStorWidgetView.extend({
       //tooltip: true,
       //tooltipOpts: { content: "<b>%s</b> (%p.2%)" }
     };
-    this.prev_cpu_data = null;
   },
  
   allCpuTickFormatter: function(cpuNames, context) {
     return function(val, axis) {
-      return context.cpuNames[val-1];
+      if (!_.isUndefined(context.cpuNames[val-1])) {
+        return context.cpuNames[val-1];
+      } else {
+        return "";
+      }
     }
   },
   
@@ -133,10 +128,8 @@ CpuUsageWidget = RockStorWidgetView.extend({
   pctTickFormatter: function(val, axis) {
     return val + "%";
   },
-  
 
   render: function() {
-    // call render of base
     this.constructor.__super__.render.apply(this, arguments);
     var _this = this;
     $(this.el).html(this.template({
@@ -146,7 +139,6 @@ CpuUsageWidget = RockStorWidgetView.extend({
       width: this.defaultWidth,
       displayName: this.displayName
     }));
-    
     
     this.intervalId = window.setInterval(function() {
       return function() { _this.getData(_this); }
@@ -158,32 +150,26 @@ CpuUsageWidget = RockStorWidgetView.extend({
 
   getData: function(context) {
     var _this = context;
-    // showLoadingIndicator('service-loading-indicator', _this);
-    /*
+    
     $.ajax({
-      url: "/api/sm/sprobes/cpumetric/?format=json&group=cpu&limit=1", 
+      url: "/api/sm/sprobes/cpumetric/?format=json&group=name&limit=1", 
       type: "GET",
       dataType: "json",
       global: false, // dont show global loading indicator
       success: function(data, status, xhr) {
-        _this.updateGraph(data);
+        if (_.isNull(_this.numCpus)) {
+          _this.numCpus = data.length;
+        }
+        _this.parseData(data); 
+        _this.updateGraph();
       },
       error: function(xhr, status, error) {
         logger.debug(error);
       }
-
     });
-    */
-    var rawData = _this.genRawData(); 
-    _this.parseData(rawData); 
-    _this.updateGraph();
-    
-    
   },
 
-
   cleanup: function() {
-    logger.debug('clearing setInterval in cpu_usage_widget'); 
     if (!_.isUndefined(this.intervalId)) {
       window.clearInterval(this.intervalId);
     }
@@ -192,7 +178,18 @@ CpuUsageWidget = RockStorWidgetView.extend({
   parseData: function(data) {
     var _this = this;
     var tmpSum = {};
-    _.each(data, function(d) {
+    _.each(data, function(dRaw) {
+      // convert mode value into percentage
+      var d = _.clone(dRaw);
+      var modes = ['smode', 'umode', 'umode_nice', 'idle'];
+      var sum = 0;
+      _.each(modes, function(m) {
+        sum = sum + dRaw[m];
+      });
+      _.each(modes, function(m) {
+        d[m] = parseInt((dRaw[m]/sum)*100);
+      });
+
       var cpu = _this.cpuData[d.name];
       if (_.isUndefined(cpu)) {
         cpu = _this.genEmptyCpuData(_this.numSamples );
@@ -222,6 +219,10 @@ CpuUsageWidget = RockStorWidgetView.extend({
         var dm = _this.cpuData[name][mode];
         tmp.push([j+1, dm[dm.length-1]]);
       });
+      // Add empty data so that bar width is acc to maxCpus .
+      for (var k=_this.numCpus; k<_this.maxCpus; k++) {
+        tmp.push([k+1, null]);
+      }
       _this.allCpuGraphData.push({
         "label": mode, 
         "data": tmp, 
@@ -273,65 +274,13 @@ CpuUsageWidget = RockStorWidgetView.extend({
 
 
   updateGraph: function(data) {
-    this.allCpuGraphOptions.xaxis.ticks = this.cpuNames.length;
+    //this.allCpuGraphOptions.xaxis.ticks = this.cpuNames.length;
+    this.allCpuGraphOptions.xaxis.ticks = this.maxCpus;
     $.plot($("#cpuusage-all"), this.allCpuGraphData, this.allCpuGraphOptions);
     //$("#cpuusage-all").bind("plotclick", function(event, pos, item) {
     //});
     $.plot($("#cpuusage"), this.avgGraphData, this.graphOptions);
   },
-
-  /* Calculate utilization as difference in data for two consecutive times */
-  /*
-  modifyData: function(data) {
-    var tmp = [];
-    var res = [];
-    var new_data = [];
-    var totals = [];
-    if (data.length > this.numSamples) {
-      data = data.slice(data.length - this.numSamples);
-    }
-    // calculate diffs from previous for each mode.
-    for (var m = 0; m < this.modes.length; m++) {
-      var mode = this.modes[m];
-      tmp[m] = [];
-      res[m] = [];
-      for (var i=0; i< data.length; i++) {
-        if (!_.isNull(this.prev_cpu_data) && this.prev_cpu_data.length == data.length) {
-          var diff = data[i][mode] - this.prev_cpu_data[i][mode];
-          tmp[m].push(diff);
-        } else {
-          tmp[m].push(0);
-        }
-      }
-    }
-    // calculate totals for each sample
-    for (var i=0; i< data.length; i++) {
-      totals[i] = 0;
-      for (var m = 0; m < this.modes.length; m++) {
-        totals[i] = totals[i] + tmp[m][i];
-      }
-      for (var m = 0; m < this.modes.length; m++) {
-        if (totals[i] != 0) {
-          tmp[m][i] = (tmp[m][i] / totals[i]) * 100;
-        } else {
-          tmp[m][i] = 0;
-        }
-        
-      }
-    }
-    // create series with x as index and y as util percent
-    for (var m = 0; m < this.modes.length; m++) {
-      res[m] = [];
-      for (var i=0; i < this.numSamples; i++) {
-        res[m].push([i, tmp[m][i]]);
-      }
-      new_data.push({ "label": this.modes[m], "data": res[m], "color": this.colors[m] });
-    }
-
-    this.prev_cpu_data = data;
-    return new_data;
-  },
-  */
 
 });
 
