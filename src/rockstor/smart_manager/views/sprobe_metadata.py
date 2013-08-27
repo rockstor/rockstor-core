45b921a88995abb6a2e358355e7cce1f10527849
Suman Chakravartula <suman@rockstor.com>
2013-08-27 11:59:42 -0700

    Turn off pagination when a single smart probe meta data document is requested. return 404 if it does not exist.

--- src/rockstor/smart_manager/views/sprobe_metadata.py
+++ src/rockstor/smart_manager/views/sprobe_metadata.py
@@ -34,7 +34,11 @@ class SProbeMetadataView(AdvancedSProbeView):
 
     def get_queryset(self, *args, **kwargs):
         if ('pid' in kwargs):
-            return SProbe.objects.filter(id=kwargs['pid'])
+            self.paginate_by = 0
+            try:
+                return SProbe.objects.get(id=kwargs['pid'])
+            except:
+                return []
 
         limit = self.request.QUERY_PARAMS.get('limit',
                                               settings.PAGINATION['max_limit'])
