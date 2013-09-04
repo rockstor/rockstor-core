db7fdcc23b4b855cfc85b89a74ec0b75b2e9e4c7
Suman Chakravartula <suman@rockstor.com>
2013-09-03 20:04:55 -0700

    Add model for replication.

--- src/rockstor/smart_manager/models/__init__.py
+++ src/rockstor/smart_manager/models/__init__.py
@@ -33,3 +33,4 @@ from share_usage import ShareUsage
 from nfsd_uid_gid import NFSDUidGidDistribution
 from task_def import TaskDefinition
 from task import Task
+from share_replication import (Replica, ReplicaTrail)
