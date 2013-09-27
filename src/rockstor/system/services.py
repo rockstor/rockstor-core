4dc7e6e19dd71f39f9e5f4453634f9acf7b4ba77
Suman Chakravartula <suman@rockstor.com>
2013-09-27 11:08:04 -0700

    Bugfix: Services stay in on state even when they fail to start.

--- src/rockstor/system/services.py
+++ src/rockstor/system/services.py
@@ -26,13 +26,13 @@ SERVICE_BIN = '/sbin/service'
 CHKCONFIG_BIN = '/sbin/chkconfig'
 
 
-def init_service_op(service_name, command):
+def init_service_op(service_name, command, throw=True):
     supported_services = ('nfs', 'smb', 'sshd', 'ypbind', 'rpcbind', 'ntpd')
     if (service_name not in supported_services):
         raise Exception('unknown service: %s' % service_name)
 
     cmd = [SERVICE_BIN, service_name, command]
-    out, err, rc = run_command(cmd)
+    out, err, rc = run_command(cmd, throw=throw)
     return out, err, rc
 
 def chkconfig(service_name, switch):
@@ -40,8 +40,8 @@ def chkconfig(service_name, switch):
 
 def service_status(service_name):
     if (service_name == 'nis'):
-        out, err, rc = init_service_op('rpcbind', 'status')
-        if (rc == 0):
+        out, err, rc = init_service_op('rpcbind', 'status', throw=False)
+        if (rc != 0):
             return out, err, rc
-        return init_service_op('ypbind', 'status')
-    return init_service_op(service_name, 'status')
+        return init_service_op('ypbind', 'status', throw=False)
+    return init_service_op(service_name, 'status', throw=False)
