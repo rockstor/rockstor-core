227c6004f633fce35cefc18f610ea763df9af0e9
Sujeet Sreenivasan <sujeetsr@gmail.com>
2013-09-26 17:29:10 -0700

    Display replica trail for replicas

--- src/rockstor/templates/storageadmin/js/js/router.js
+++ src/rockstor/templates/storageadmin/js/js/router.js
@@ -55,6 +55,7 @@ var AppRouter = Backbone.Router.extend({
     "run_probe": "runProbe",
     "probeDetail/:probeName/:probeId": "showProbeDetail",
     "replication": "showReplication",
+    "replication/:replicaId/trails": "showReplicaTrails",
     "add_replication_task": "addReplicationTask",
     "*path": "showHome"
   },
@@ -299,6 +300,16 @@ var AppRouter = Backbone.Router.extend({
     this.renderSidebar("storage", "replication");
     this.cleanup();
     this.currentLayout = new ReplicationView();
+    $('#maincontent').empty();
+    $('#maincontent').append(this.currentLayout.render().el);
+  },
+
+  showReplicaTrails: function(replicaId) {
+    this.renderSidebar("storage", "replication");
+    this.cleanup();
+    this.currentLayout = new ReplicaTrailsView({
+      replicaId: replicaId
+    });
     $('#maincontent').empty();
     $('#maincontent').append(this.currentLayout.render().el);
   },
