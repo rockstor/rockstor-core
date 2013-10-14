d2528aaa83337e488356d55e0bdf0c6c7d89491b
Suman Chakravartula <suman@rockstor.com>
2013-10-14 10:59:30 -0700

    Update disks console to use api_error handler

--- src/rockstor/cli/disks_console.py
+++ src/rockstor/cli/disks_console.py
@@ -18,7 +18,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 from base_console import BaseConsole
-from rest_util import (api_call, print_disk_info)
+from rest_util import (api_error, api_call, print_disk_info)
+from storageadmin.exceptions import RockStorAPIException
 
 
 class DisksConsole(BaseConsole):
@@ -28,6 +29,7 @@ class DisksConsole(BaseConsole):
         self.prompt = prompt + ' Disks>'
         self.baseurl = ('%sdisks' % BaseConsole.url)
 
+    @api_error
     def do_list(self, args):
         """
         List brief information about disks in the system.
@@ -52,6 +54,7 @@ class DisksConsole(BaseConsole):
         disk_info = api_call(url)
         print_disk_info(disk_info)
 
+    @api_error
     def do_scan(self, args):
         """
         Scan the system for any new disks since the last scan.
@@ -64,3 +67,13 @@ class DisksConsole(BaseConsole):
         disk_info = api_call(url, data=None, calltype='post')
         print_disk_info(disk_info)
 
+    @api_error
+    def do_delete(self, args):
+        """
+        Delete a offlined disk
+
+        delete disk_name
+        """
+        url = ('%s/%s' % (self.baseurl, args))
+        api_call(url, calltype='delete')
+        print_disk_info(api_call(self.baseurl))
