07e89bc161236a7ac806f1ec03bc3db72aaa4486
Sujeet Sreenivasan <sujeetsr@gmail.com>
2013-10-15 10:21:08 -0700

    Network configuration support
    Add views and templates for network interface list and edit pages

---
+++ src/rockstor/templates/storageadmin/js/js/views/edit_network.js
@@ -0,0 +1,93 @@
+/*
+ *
+ * @licstart  The following is the entire license notice for the 
+ * JavaScript code in this page.
+ * 
+ * Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+ * This file is part of RockStor.
+ * 
+ * RockStor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ * 
+ * RockStor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ * 
+ */
+
+EditNetworkView = RockstoreLayoutView.extend({
+  events: {
+    "click #cancel": "cancel"
+  },
+
+  initialize: function() {
+    // call initialize of base
+    this.constructor.__super__.initialize.apply(this, arguments);
+    // set template
+    this.template = window.JST.network_edit_network;
+    this.name = this.options.name;
+    this.network = new NetworkInterface({name: this.name});
+  },
+  
+  render: function() {
+    var _this = this;
+    this.network.fetch({
+      success: function(collection, response, options) {
+        _this.renderNetwork();
+      }
+    });
+    return this;
+  },
+
+  renderNetwork: function() {
+    var _this = this;
+    $(this.el).html(this.template({network: this.network}));
+    this.validator = this.$("#edit-network-form").validate({
+      onfocusout: false,
+      onkeyup: false,
+      rules: {
+      },
+      messages: {
+      },
+      submitHandler: function() {
+        var button = _this.$('#submit');
+        if (buttonDisabled(button)) return false;
+        var network = new Network({name: _this.name});
+        var data = _this.$('#edit-network-form').getJSON();
+        network.save(data, {
+          success: function(model, response, options) {
+            console.log("network saved successfully");
+            app_router.navigate("network", {trigger: true});
+          },
+          error: function(model, xhr, options) {
+            enableButton(button);
+            var msg = parseXhrError(xhr)
+            if (_.isObject(msg)) {
+              _this.validator.showErrors(msg);
+            } else {
+              _this.$(".messages").html("<label class=\"error\">" + msg + "</label>");
+            }
+          }
+        });
+        return false;  
+      }
+    });
+  },
+
+  cancel: function() {
+    app_router.navigate("network", {trigger: true});
+  }
+
+});
+
+
+
