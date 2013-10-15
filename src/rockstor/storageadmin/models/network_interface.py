83c90c11723ce5ad126e24039e95db5120616602
Suman Chakravartula <suman@rockstor.com>
2013-10-14 17:14:09 -0700

    Fixes #285
    update network model to include a type(for replication, management, io etc..)

--- src/rockstor/storageadmin/models/network_interface.py
+++ src/rockstor/storageadmin/models/network_interface.py
@@ -28,6 +28,7 @@ class NetworkInterface(models.Model):
     network = models.CharField(max_length=100, null=True)
     netmask = models.CharField(max_length=100, null=True)
     ipaddr = models.CharField(max_length=100, null=True)
+    itype = models.CharField(max_length=100, default='management')
 
     class Meta:
         app_label = 'storageadmin'
