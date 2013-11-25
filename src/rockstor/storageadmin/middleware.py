5a9d59aa226df4bf7392e1bc186f3aee80c19fc8
Suman Chakravartula <suman@rockstor.com>
2013-11-24 18:51:52 -0800

    write a custom middleware for logging exceptions for prod deploy instead of emailing ADMINS.

---
+++ src/rockstor/storageadmin/middleware.py
@@ -0,0 +1,30 @@
+"""
+Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+"""
+Simple exception middleware that writes to rockstor log instead of emailing
+ADMINS.
+"""
+class ProdExceptionMiddleware(object):
+
+    def process_exception(self, request, exception):
+        """just log the exception"""
+        logger.exception(exception)
