3677392acfcf60ba0842117779d622d75965a54b
Suman Chakravartula <suman@rockstor.com>
2014-02-02 16:37:22 -0800

    Remove handling pending replication from the sender as a new sender is not supposed to be started for a pending trail to begin with.
    Update log messaging.

--- src/rockstor/smart_manager/replication/sender.py
+++ src/rockstor/smart_manager/replication/sender.py
@@ -69,8 +69,9 @@ class Sender(Process):
         super(Sender, self).__init__()
 
     def _clean_exit(self, msg, exception):
-        logger.info(msg)
+        logger.error(msg)
         logger.exception(exception)
+        #@todo: ctx.term()
         sys.exit(3)
 
     def run(self):
@@ -80,27 +81,21 @@ class Sender(Process):
             meta_push.connect('tcp://%s:%d' % (self.receiver_ip,
                                                self.meta_port))
         except Exception, e:
-            msg = ('could not connect to target(%s:%d)' %
-                   (self.receiver_ip, self.meta_port))
+            msg = ('Failed to connect to receiver(%s) on meta port'
+                   '(%d) for snap_name: %s. Aborting.' %
+                   (self.receiver_ip, self.meta_port, self.snap_name))
             self._clean_exit(msg, e)
 
-        if (self.rt is None or self.rt.status == 'succeeded' or
-            self.rt.status == 'failed'):
-            #1. create a new replica trail if it's the very first time
-            # of if the last one succeeded
-            try:
-                self.rt2 = create_replica_trail(self.replica.id,
-                                                self.snap_name, logger)
-                self.rt2_id = self.rt2['id']
-            except Exception, e:
-                msg = 'foobar'
-                self._clean_exit(msg, e)
-        elif (self.rt is not None and self.rt.status == 'pending'):
-            #assume that the last sender bailed for whatever reason and
-            #retry with this sender, but update the same rt object.
-            self.rt2 = self.rt
-            self.rt2_id = self.rt2.id
-        logger.info('rt2 = %s' % self.rt2)
+        #1. create a new replica trail if it's the very first time
+        # of if the last one succeeded
+        try:
+            self.rt2 = create_replica_trail(self.replica.id,
+                                            self.snap_name, logger)
+            self.rt2_id = self.rt2['id']
+        except Exception, e:
+            msg = ('Failed to create local replica trail for snap_name:'
+                   ' %s. Aborting.' % self.snap_name)
+            self._clean_exit(msg, e)
 
         #2. create a snapshot only if it's not already from a previous
         #failed attempt.
@@ -108,7 +103,8 @@ class Sender(Process):
             try:
                 create_snapshot(self.replica.share, self.snap_name, logger)
             except Exception, e:
-                msg = ('failed to create snapshot: %s' % self.snap_name)
+                msg = ('Failed to create snapshot: %s. Aborting.' %
+                       self.snap_name)
                 self._clean_exit(msg, e)
 
         #let the receiver know that following diff is coming
@@ -118,55 +114,57 @@ class Sender(Process):
         try:
             self.q.get(block=True, timeout=60)
         except Exception, e:
-            e_msg = ('timed out(60 seconds) waiting for begin_ok from the '
-                     'receiver. ')
-            #@todo: proper rollback
-            sys.exit(3)
-        logger.info('get returned')
+            msg = ('Timeout occured(60 seconds) while waiting for begin_ok '
+                   'from the receiver(%s). Aborting.' % self.receiver_ip)
+            self._clean_exit(msg, e)
+
+        logger.debug('get returned')
 
         snap_path = ('%s%s/%s_%s' % (settings.MNT_PT, self.replica.pool,
                                      self.replica.share, self.snap_name))
-        logger.info('current snap: %s' % snap_path)
+        logger.debug('current snap: %s' % snap_path)
         cmd = [BTRFS, 'send', snap_path]
         if (self.rt is not None):
             prev_snap = ('%s%s/%s_%s' % (settings.MNT_PT, self.replica.pool,
                                          self.replica.share,
                                          self.rt.snap_name))
-            logger.info('there was a previous snap: %s' % prev_snap)
+            logger.info('Sending incremental replica between %s -- %s' %
+                        (prev_snap, snap_path))
             cmd = [BTRFS, 'send', '-p', prev_snap, snap_path]
-        logger.info('btrfs send cmd: %s' % cmd)
+        else:
+            logger.info('Sending full replica: %s' % snap_path)
 
         sp = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE,
                               stderr=subprocess.PIPE)
         fcntl.fcntl(sp.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
-        logger.info('send started. snap: %s' % snap_path)
+        logger.debug('send started. snap: %s' % snap_path)
         alive = True
         fatal_exception = False
         while alive:
             try:
                 if (sp.poll() is not None):
-                    logger.info('send process finished. rc: %d. stderr: %s' %
-                                (sp.returncode, sp.stderr.read()))
+                    logger.debug('send process finished. rc: %d. stderr: %s'
+                                 % (sp.returncode, sp.stderr.read()))
                     alive = False
                 fs_data = sp.stdout.read()
                 self.pub.put('%s%s' % (self.snap_id, fs_data))
                 self.kb_sent = self.kb_sent + len(fs_data)
-                logger.info('send process still alive. kb_sent: %s' %
-                            self.kb_sent)
+                logger.debug('send process still alive. kb_sent: %s' %
+                             self.kb_sent)
             except IOError:
                 pass
             except Exception, e:
-                logger.error('exception occured during send')
                 logger.exception(e)
                 if (alive):
-                    logger.info('terminating the send process')
+                    logger.info('Terminating the child send process for '
+                                'snap_name: %s' % self.snap_name)
                     sp.terminate()
-                logger.info('sender exiting')
+                msg = ('Exception occured while transferring data '
+                       'for snap_name: %s' % self.snap_name)
                 fatal_exception = True
             finally:
                 if (fatal_exception is True):
-                    #@todo: cleanup/rollback work.
-                    sys.exit(3)
+                    self._clean_exit(msg, e)
                 if (not alive):
                     if (sp.returncode != 0):
                         self.pub.put('%sEND_FAIL' % self.snap_id)
