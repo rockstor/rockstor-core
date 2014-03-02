97d5efe535fcae397ccc3783dcec5b379cc88ad9
Suman Chakravartula <suman@rockstor.com>
2014-03-02 12:37:49 -0800

    add mount utitlity methods for backup task workers

--- src/rockstor/backup/util.py
+++ src/rockstor/backup/util.py
@@ -16,9 +16,9 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import re
 from cli.rest_util import api_call
 from storageadmin.exceptions import RockStorAPIException
+
 
 BASE_URL = 'https://localhost/api/'
 
@@ -27,7 +27,7 @@ def create_trail(pid, logger):
         url = ('%splugin/backup/trail/policy/%d' % (BASE_URL, pid))
         td = api_call(url, calltype='post', save_error=False)
         logger.info('new policy trail created for policy(%d)' % pid)
-        return td['id']
+        return td
     except Exception,  e:
         logger.error('Failed to create new policy trail for policy(%d)' % pid)
         raise e
@@ -42,4 +42,28 @@ def update_trail(tid, data, logger):
         logger.error('Failed to update policy trail(%s) status to %s' %
                      (url, data['status']))
         raise e
+
+from system.osi import (is_mounted, run_command)
+import os
+MOUNT = '/bin/mount'
+def mount_source(source_ip, source_share):
+    mnt_pt = ('/mnt/backup/%s_%s' % (source_ip, source_share))
+    if (is_mounted(mnt_pt)):
+        return True
+    if (not os.path.isdir(mnt_pt)):
+        os.makedirs(mnt_pt)
+    cmd = [MOUNT, '%s:%s' % (source_ip, source_share), mnt_pt]
+    return run_command(cmd)
+
+def create_snapshot(sname, snap_name, logger):
+    try:
+        url = ('%sshares/%s/snapshots/%s' % (BASE_URL, sname, snap_name))
+        snap_details = api_call(url, calltype='post', save_error=False)
+        return logger.debug('created snapshot. url: %s. details = %s' %
+                           (url, snap_details))
+    except RockStorAPIException, e:
+        return logger.debug('Failed to create snapshot: %s. It may already '
+                            'exist. error: %s' % (url, e.detail))
+    except Exception:
+        raise
 
