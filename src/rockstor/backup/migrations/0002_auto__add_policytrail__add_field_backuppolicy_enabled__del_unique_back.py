05003064bc331b14df318e40db2b238d341428da
Suman Chakravartula <suman@rockstor.com>
2014-03-05 16:57:00 -0800

    create one consolidated migration for backup for this branch/PR.

--- src/rockstor/backup/migrations/0002_auto__add_policytrail__add_field_backuppolicy_enabled__del_unique_back.py
+++ src/rockstor/backup/migrations/0002_auto__add_policytrail__add_field_backuppolicy_enabled__del_unique_back.py
@@ -8,13 +8,61 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Removing unique constraint on 'BackupPolicy', fields ['dest_share']
+        db.delete_unique(u'backup_backuppolicy', ['dest_share'])
+
+        # Removing unique constraint on 'BackupPolicy', fields ['source_path']
+        db.delete_unique(u'backup_backuppolicy', ['source_path'])
+
+        # Removing unique constraint on 'BackupPolicy', fields ['source_ip']
+        db.delete_unique(u'backup_backuppolicy', ['source_ip'])
+
         # Removing unique constraint on 'BackupPolicy', fields ['notify_email']
         db.delete_unique(u'backup_backuppolicy', ['notify_email'])
 
+        # Adding model 'PolicyTrail'
+        db.create_table(u'backup_policytrail', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('policy', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['backup.BackupPolicy'])),
+            ('start', self.gf('django.db.models.fields.DateTimeField')(null=True, db_index=True)),
+            ('status', self.gf('django.db.models.fields.CharField')(default='start', max_length=255)),
+            ('snap_created', self.gf('django.db.models.fields.DateTimeField')(null=True)),
+            ('sync_started', self.gf('django.db.models.fields.DateTimeField')(null=True)),
+            ('error', self.gf('django.db.models.fields.CharField')(max_length=2048, null=True)),
+            ('status_ts', self.gf('django.db.models.fields.DateTimeField')(null=True)),
+        ))
+        db.send_create_signal('backup', ['PolicyTrail'])
+
+        # Adding field 'BackupPolicy.enabled'
+        db.add_column(u'backup_backuppolicy', 'enabled',
+                      self.gf('django.db.models.fields.BooleanField')(default=True),
+                      keep_default=False)
+
+        # Adding unique constraint on 'BackupPolicy', fields ['source_ip', 'source_path']
+        db.create_unique(u'backup_backuppolicy', ['source_ip', 'source_path'])
+
 
     def backwards(self, orm):
+        # Removing unique constraint on 'BackupPolicy', fields ['source_ip', 'source_path']
+        db.delete_unique(u'backup_backuppolicy', ['source_ip', 'source_path'])
+
+        # Deleting model 'PolicyTrail'
+        db.delete_table(u'backup_policytrail')
+
+        # Deleting field 'BackupPolicy.enabled'
+        db.delete_column(u'backup_backuppolicy', 'enabled')
+
         # Adding unique constraint on 'BackupPolicy', fields ['notify_email']
         db.create_unique(u'backup_backuppolicy', ['notify_email'])
+
+        # Adding unique constraint on 'BackupPolicy', fields ['source_ip']
+        db.create_unique(u'backup_backuppolicy', ['source_ip'])
+
+        # Adding unique constraint on 'BackupPolicy', fields ['source_path']
+        db.create_unique(u'backup_backuppolicy', ['source_path'])
+
+        # Adding unique constraint on 'BackupPolicy', fields ['dest_share']
+        db.create_unique(u'backup_backuppolicy', ['dest_share'])
 
 
     models = {
