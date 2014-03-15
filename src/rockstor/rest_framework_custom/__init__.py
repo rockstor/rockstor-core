b1bc9adcb589621a04d5692a6989f93ff340de5f
Suman Chakravartula <suman@rockstor.com>
2014-03-15 11:51:08 -0700

    Refactor generic sprobe view.

--- src/rockstor/rest_framework_custom/__init__.py
+++ src/rockstor/rest_framework_custom/__init__.py
@@ -17,3 +17,4 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from generic_view import GenericView
+from generic_sprobe import GenericSProbeView
--- src/rockstor/smart_manager/views/net_stat.py
+++ src/rockstor/smart_manager/views/net_stat.py
@@ -16,12 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from generic_sprobe import GenericSProbeView
+import rest_framework_custom as rfc
 from smart_manager.models import NetStat
 from smart_manager.serializers import NetStatSerializer
 
 
-class NetStatView(GenericSProbeView):
+class NetStatView(rfc.GenericSProbeView):
 
     serializer_class = NetStatSerializer
     model_obj = NetStat
