0f1f33d9387ccf74331a69c988c61ce0ef0d7300
Suman Chakravartula <suman@rockstor.com>
2014-03-20 15:10:31 -0700

    Move generic sprobe to smart manager module as it's too specific to be in rfc.

--- src/rockstor/smart_manager/views/net_stat.py
+++ src/rockstor/smart_manager/views/net_stat.py
@@ -19,9 +19,10 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 import rest_framework_custom as rfc
 from smart_manager.models import NetStat
 from smart_manager.serializers import NetStatSerializer
+from generic_sprobe import GenericSProbeView
 
 
-class NetStatView(rfc.GenericSProbeView):
+class NetStatView(GenericSProbeView):
 
     serializer_class = NetStatSerializer
     model_obj = NetStat
