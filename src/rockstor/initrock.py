4ba58414b34f8084a6fa33013362ce3f8ea6010e
Suman Chakravartula <suman@rockstor.com>
2014-03-27 15:02:37 -0700

    fix misc bugs and add logging to initrock

--- src/rockstor/initrock.py
+++ src/rockstor/initrock.py
@@ -20,37 +20,88 @@ import os
 import shutil
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
 from system.osi import run_command
+import logging
+import sys
 
 SYSCTL = '/usr/bin/systemctl'
+DJANGO = '/opt/rockstor/bin/django'
 
 def main():
+    loglevel = logging.INFO
+    if (len(sys.argv) > 1 and sys.argv[1] == '-x'):
+        loglevel = logging.DEBUG
+    logging.basicConfig(format='%(asctime)s: %(message)s', level=loglevel)
+    logging.info('Please be patient. This script could take a few minutes')
+    shutil.copyfile('/opt/rockstor/conf/django-hack',
+                    '/opt/rockstor/bin/django')
     run_command([SYSCTL, 'enable', 'postgresql'])
+    logging.debug('Progresql enabled')
     shutil.rmtree('/var/lib/pgsql/data')
+    logging.info('initializing Postgresql...')
     run_command(['/usr/bin/postgresql-setup', 'initdb'])
-    run_command(['cp', '-f', '/opt/rockstor/conf/pg_hba.conf',
-                 '/var/lib/pgsql/data/'])
+    logging.info('Done.')
     run_command([SYSCTL, 'restart', 'postgresql'])
     run_command([SYSCTL, 'status', 'postgresql'])
+    logging.debug('Postgresql restarted')
+    logging.info('Creating app databases...')
     run_command(['su', '-', 'postgres', '-c', '/usr/bin/createdb smartdb'])
+    logging.debug('smartdb created')
     run_command(['su', '-', 'postgres', '-c', '/usr/bin/createdb storageadmin'])
+    logging.debug('storageadmin created')
     run_command(['su', '-', 'postgres', '-c', '/usr/bin/createdb backup'])
+    logging.debug('backup created')
+    logging.info('Done')
+    logging.info('Initializing app databases...')
     run_command(['sudo', '-u', 'postgres', 'psql', '-c',
                  "CREATE ROLE rocky WITH SUPERUSER LOGIN PASSWORD 'rocky'"])
+    logging.debug('rocky ROLE created')
     run_command(['sudo', '-u', 'postgres', 'psql', 'storageadmin', '-f',
                  '/opt/rockstor/conf/storageadmin.sql.in'])
+    logging.debug('storageadmin app database loaded')
     run_command(['sudo', '-u', 'postgres', 'psql', 'smartdb', '-f',
                  '/opt/rockstor/conf/smartdb.sql.in'])
+    logging.debug('smartdb app database loaded')
     run_command(['sudo', '-u', 'postgres', 'psql', 'backup', '-f',
                  '/opt/rockstor/conf/backup.sql.in'])
+    logging.debug('backup app database loaded')
     run_command(['sudo', '-u', 'postgres', 'psql', 'storageadmin', '-c', "select setval('south_migrationhistory_id_seq', (select max(id) from south_migrationhistory))"])
+    logging.debug('storageadmin migration history copied')
     run_command(['sudo', '-u', 'postgres', 'psql', 'smartdb', '-c', "select setval('south_migrationhistory_id_seq', (select max(id) from south_migrationhistory))"])
+    logging.debug('smartdb migration history copied')
     run_command(['sudo', '-u', 'postgres', 'psql', 'backup', '-c', "select setval('south_migrationhistory_id_seq', (select max(id) from south_migrationhistory))"])
+    logging.debug('backup migration history copied')
+    logging.info('Done')
+    run_command(['cp', '-f', '/opt/rockstor/conf/postgresql.conf',
+                 '/var/lib/pgsql/data/'])
+    logging.debug('postgresql.conf copied')
+    run_command(['cp', '-f', '/opt/rockstor/conf/pg_hba.conf',
+                 '/var/lib/pgsql/data/'])
+    logging.debug('pg_hba.conf copied')
+    run_command([SYSCTL, 'restart', 'postgresql'])
+    logging.info('Postgresql restarted')
+    logging.info('Running app database migrations...')
+    run_command([DJANGO, 'migrate', 'storageadmin', '--database=default',
+                 '--noinput'])
+    logging.debug('storageadmin migrated')
+    run_command([DJANGO, 'migrate', 'smart_manager',
+                 '--database=smart_manager', '--noinput'])
+    logging.debug('smart manager migrated')
+    run_command([DJANGO, 'migrate', 'backup', '--database=backup',
+                 '--noinput'])
+    logging.debug('backup migrated')
+    logging.info('Done')
+    logging.info('Running prepdb...')
+    run_command(['/opt/rockstor/bin/prep_db',])
+    logging.info('Done')
     shutil.copy('/opt/rockstor/conf/rockstor.service', '/etc/systemd/system/')
     run_command([SYSCTL, 'enable', 'rockstor'])
     run_command([SYSCTL, 'start', 'rockstor'])
+    logging.info('Started rockstor service')
+    logging.info('Shutting down firewall...')
     run_command([SYSCTL, 'stop', 'firewalld'])
     run_command([SYSCTL, 'disable', 'firewalld'])
-    print('All set. You can use the web-ui now and start using Rockstor!')
+    logging.info('Done')
+    logging.info('All set. Go to the web-ui now and start using Rockstor!')
 
 if __name__ == '__main__':
     main()
