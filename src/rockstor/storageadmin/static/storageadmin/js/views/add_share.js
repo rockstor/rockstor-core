793ced02663f03a03bb42d7e2c09a0a747074b6c
Madhuri Chakravarthula <mchakravartula@gmail.com>
2014-05-29 21:24:21 +0100

    Implement Slider scale for the share size.

--- src/rockstor/storageadmin/static/storageadmin/js/views/add_share.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/add_share.js
@@ -54,8 +54,12 @@ AddShareView = Backbone.View.extend({
         // tick formatter
         var formatter = d3.format(",.2f");
         var tickFormatter = function(d) {
+        if(d < 1024){
         return formatter(d) + " GB";
-           }
+          }else{
+        return formatter(d/1024) + " TB";
+          }
+        }
         
          
          var callback = function(slider) {
@@ -69,10 +73,13 @@ AddShareView = Backbone.View.extend({
             $("#share_size").val(((value)/1024).toFixed(2)+"TB");
             }
           }   
-         
-        
-        
-        var slider = d3.slider().min(0).max(1024).ticks(10).showRange(true).tickFormat(tickFormatter).callback(callback);
+         var foo = [];
+        for (var i=0;i<2049;i++) {
+          foo.push(i);
+             }
+        var step_values = ['1','500','1024','15',''];
+         var slider = d3.slider().min(0).max(2048).tickValues([1,500,1024,1536,2048]).stepValues(foo).tickFormat(tickFormatter).callback(callback);
+       // var slider = d3.slider().min(0).max(2048).tickValues([1,500,1024,1536,2048]).stepValues([1,50,100,150,200,250,300,350,400,450,500,550,600,650,700,750,800,850,900,950,1000,1024,1229,1332,1434,1536,1639,1741,1844,1946,2048]).tickFormat(tickFormatter).callback(callback);
         d3.select('#slider').call(slider);
         
         $("#share_size").change(function(){
@@ -117,7 +124,19 @@ AddShareView = Backbone.View.extend({
               var share_name = $('#share_name').val();
               var pool_name = $('#pool_name').val();
               var size = $('#share_size').val();
+              var sizeFormat = size.replace(/[^a-z]/gi, ""); 
+              var size_array = size.split(sizeFormat)
+              var size = size_array[0];
+             
+              if(sizeFormat == 'GB' || sizeFormat == 'gb' || sizeFormat == 'Gb'){
+              size = size*1024*1024;
+              alert(size);
+              }else if(sizeFormat == 'TB' || sizeFormat == 'tb' || sizeFormat == 'Tb'){
+              size = size*1024*1024*1024;
+              alert(size);
+              }
               
+              alert(size);
               $.ajax({
                 url: "/api/shares",
                 type: "POST",
