9c34646e1987d41e4ff2a13b6e1d4d1780836090
Sujeet Sreenivasan <sujeetsr@gmail.com>
2014-06-11 20:30:46 -0700

    slider destroy on pool change

--- src/rockstor/storageadmin/static/storageadmin/js/views/add_share.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/add_share.js
@@ -34,6 +34,7 @@ AddShareView =  RockstorLayoutView.extend({
   },
 
   initialize: function() {
+    var _this = this;
     this.constructor.__super__.initialize.apply(this, arguments);
     this.template = window.JST.share_add_share_template;
    
@@ -41,148 +42,190 @@ AddShareView =  RockstorLayoutView.extend({
     this.pools.pageSize = RockStorGlobals.maxPageSize;
     this.poolName = this.options.poolName;
     this.dependencies.push(this.pools);
-    
+    this.tickFormatter = function(d) {
+      var formatter = d3.format(",.2f");
+      if (d > 1024*1024) {
+        return formatter(d/(1024*1024)) + " TB";
+      } else if (d > 1024) {
+        return formatter(d/1024) + " GB";
+      } else {
+        return formatter(d) + " KB";
+      }
+    }
+    this.slider = null;
+    this.sliderCallback = function(slider) {
+      var value = slider.value();
+      _this.$('#share_size').val(_this.tickFormatter(value));
+    }
   },
   
-   render: function() {
-   
-   var _this = this;
+  render: function() {
+
+    var _this = this;
     this.pools.fetch({
       success: function(collection, response) {
         $(_this.el).append(_this.template({pools: _this.pools, poolName: _this.poolName}));
-         
-        
+
         $('#add-share-form :input').tooltip({placement: 'right'});
-        
-        // tick formatter
-        var formatter = d3.format(",.2f");
-        var tickFormatter = function(d) {
-        if(d < 1024){
-        return formatter(d) + " GB";
-          }else{
-        return formatter(d/1024) + " TB";
-          }
-        }
-        
-         var callback = function(slider) {
-        // get current slider value
-         var value = slider.value();
-         if(value < 1024)
-          {
-            $("#share_size").val(value.toFixed(2)+"GB");
-            }else
-            {
-            $("#share_size").val(((value)/1024).toFixed(2)+"TB");
-            }
-          }   
-          
-         
-          var slider_poolsize = function(){  
-          var pool_name = $('#pool_name').val();
-          var selectedPool = _this.pools.find(function(p) { return p.get('name') == pool_name; });
-          var maxPoolSize =  (selectedPool.get('size')- selectedPool.get('usage'))/1024;
-          
-          var foo = [];
-          for (var i=0;i<= maxPoolSize;i++) {
-          foo.push(i);
-             }
-             
-          var foo2 = [];
-          var step = maxPoolSize/5;
-          for (var i=0;i<=maxPoolSize;i=i+step) {
-          foo2.push(i); 
-             }
-          
-          var slider = d3.slider().min(0).max(maxPoolSize).tickValues(foo2).stepValues(foo).tickFormat(tickFormatter).callback(callback);
-          d3.select('#slider').call(slider);
-          
-          };
-          
-          slider_poolsize();
-          
-          $('#pool_name').change(function(){
-          slider_poolsize();
-           });
-          
-          
+
+        _this.renderSlider();
+
+        $('#pool_name').change(function(){
+          _this.renderSlider();
+        });
+
         $("#share_size").change(function(){
           var size = this.value;
           var sizeFormat = size.replace(/[^a-z]/gi, ""); 
-        
+
           var size_array = size.split(sizeFormat)
           var size_value = size_array[0];
-          
-           if(sizeFormat == 'TB' || sizeFormat == 'tb' || sizeFormat == 'Tb'){
-           size_value = size_value*1024;
-           slider.setValue(parseInt(size_value));
-           }else{
-           slider.setValue(parseInt(size));
-           }
-                
+
+          if(sizeFormat == 'TB' || sizeFormat == 'tb' || sizeFormat == 'Tb') {
+            size_value = size_value*1024*1024;
+            slider.setValue(parseInt(size)*1024*1024);
+          } else if (sizeFormat == 'GB' || sizeFormat == 'gb'
+            || sizeFormat == 'Gb') {
+              slider.setValue(parseInt(size)*1024);
+          } else {
+              slider.setValue(parseInt(size));
+          }
         });
-        
+
         $('#add-share-form').validate({
-            onfocusout: false,
-            onkeyup: false,
-            rules: {
-              share_name: 'required',
-              share_size: {
-                required: true,
-                
-              },
+          onfocusout: false,
+          onkeyup: false,
+          rules: {
+            share_name: 'required',
+            share_size: {
+              required: true,
+
             },
-            errorPlacement: function(error, element) {
-              if (element.attr("name") == "share_size") {
-                // insert error for share size after the size format field.
-                error.insertAfter("#size_format");
-              } else {
-                error.insertAfter(element);
-              }
-            },
-            
-            submitHandler: function() {
-              var button = _this.$('#create_share');
-              if (buttonDisabled(button)) return false;
-              disableButton(button);
-              var share_name = $('#share_name').val();
-              var pool_name = $('#pool_name').val();
-              var size = $('#share_size').val();
-              var sizeFormat = size.replace(/[^a-z]/gi, ""); 
-              var size_array = size.split(sizeFormat)
-              var size = size_array[0];
-             
-              if(sizeFormat == 'GB' || sizeFormat == 'gb' || sizeFormat == 'Gb'){
-              size = size*1024*1024;
-              }else if(sizeFormat == 'TB' || sizeFormat == 'tb' || sizeFormat == 'Tb'){
-              size = size*1024*1024*1024;
-              }
-              
-              $.ajax({
-                url: "/api/shares",
-                type: "POST",
-                dataType: "json",
-                data: {sname: share_name, "pool": pool_name, "size": size},
-                success: function() {
-                  enableButton(button);
-                  _this.$('#add-share-form :input').tooltip('hide');
-                  app_router.navigate('shares', {trigger: true})
-                },
-                error: function(xhr, status, error) {
-                  enableButton(button);
-                },
-              });
+          },
+          errorPlacement: function(error, element) {
+            if (element.attr("name") == "share_size") {
+              // insert error for share size after the size format field.
+              error.insertAfter("#size_format");
+            } else {
+              error.insertAfter(element);
             }
+          },
+
+          submitHandler: function() {
+            var button = _this.$('#create_share');
+            if (buttonDisabled(button)) return false;
+            disableButton(button);
+            var share_name = $('#share_name').val();
+            var pool_name = $('#pool_name').val();
+            var size = $('#share_size').val();
+            var sizeFormat = size.replace(/[^a-z]/gi, ""); 
+            var size_array = size.split(sizeFormat)
+            var size = size_array[0];
+
+            if(sizeFormat == 'GB' || sizeFormat == 'gb' || sizeFormat == 'Gb'){
+              size = size*1024*1024;
+            }else if(sizeFormat == 'TB' || sizeFormat == 'tb' || sizeFormat == 'Tb'){
+              size = size*1024*1024*1024;
+            }
+
+            $.ajax({
+              url: "/api/shares",
+              type: "POST",
+              dataType: "json",
+              data: {sname: share_name, "pool": pool_name, "size": size},
+              success: function() {
+                enableButton(button);
+                _this.$('#add-share-form :input').tooltip('hide');
+                app_router.navigate('shares', {trigger: true})
+              },
+              error: function(xhr, status, error) {
+                enableButton(button);
+              },
+            });
+          }
         });
       }
     });
     return this;
   },
 
+  renderSlider: function(){  
+    if (this.slider) {
+      this.slider.destroy();
+    }
+    
+    // Get size of selected pool
+    var pool_name = this.$('#pool_name').val();
+    var selectedPool = this.pools.find(function(p) { return p.get('name') == pool_name; });
+    var maxPoolSize =  (selectedPool.get('size')- selectedPool.get('usage'))/1024;
+    console.log('maxPoolSize is ' + maxPoolSize);
+    
+    // calculate step values and tick values
+    var tmp = this.genTicksForShareSize(1, parseInt(maxPoolSize/1024));
+    console.log(tmp);
+    // Render slider
+    this.slider = d3.slider().min(0).max(maxPoolSize).tickValues(tmp[0]).stepValues(tmp[1]).tickFormat(this.tickFormatter).callback(this.sliderCallback);
+    d3.select('#slider').call(this.slider);
+
+  },
+
   cancel: function(event) {
     event.preventDefault();
     this.$('#add-share-form :input').tooltip('hide');
     app_router.navigate('shares', {trigger: true})
+  },
+
+  /* Tick and step values
+   * Max size
+   * < 100 GB           - tick values every 10GB, steps at 5GB
+   * 100GB - 200GB      - tick values every 20GB, steps at 10GB
+   * 200GB - 300GB      - tick values every 50GB, steps at 10GB
+   * 300GB - 400GB      - tick values every 50GB, steps at 10GB
+   * 400GB - 500GB      - tick values every 50GB, steps at 10GB
+   * 500GB - 1000GB     - tick values every 100GB, steps at 10GB
+   * 1000GB - 2000GB    - tick values every 100GB, steps at 10GB
+   * > 2000GB           - tick values every 200GB, steps at 10GB
+   *
+   */
+  genTicksForShareSize: function(min, max) {
+    var tickValues=[], snapValues, tickStep, snapStep;
+    // Assume inputs are in GB
+    // convert min and max to nearest multiples of 10
+    var tMin = min;
+    var tMax = max;
+    if ((max - min) > 20) {
+      tMin = Math.ceil10(min, 1);
+      tMax = Math.floor10(max, 1);
+    }
+    var range = tMax - tMin;
+    console.log('range is ' + range);
+    if (range <= 10) {
+      snapStep = 1;
+      tickStep = 1;
+    } else if (range <= 100) {
+      snapStep = 5;
+      tickStep = 10;
+    } else if (range <= 500) {
+      snapStep = 10;
+      tickStep = 50;
+    } else if (range <= 1000) {
+      snapStep = 10;
+      tickStep = 100;
+    } else if (range <= 2000) {
+      snapStep = 10;
+      tickStep = 200;
+    } else if (2000 < range) {
+      snapStep = 10;
+      tickStep = 200;
+    }
+    snapValues = d3.range(0, tMax, snapStep);
+    snapValues[0] = 1;
+    for (var i=0; i<= tMax; i+=tickStep) {
+      tickValues.push(i==0 ? 1 : i);
+    }
+    return [tickValues, snapValues];
   }
+
 
 });
 
