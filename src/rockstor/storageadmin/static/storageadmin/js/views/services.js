7c3ff974fa82542f03fe597b3e65ddd08b7d91e6
Sujeet Sreenivasan <sujeetsr@gmail.com>
2014-10-02 22:40:23 -0700

    Show Service monitor status and appropriate messaging
    If the service monitor is running, display its status, but do not allow
    the user to stop it
    If it is not running, display its status, and a warning message.
    The other services are displayed as before in either case.

--- src/rockstor/storageadmin/static/storageadmin/js/views/services.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/services.js
@@ -69,9 +69,12 @@ ServicesView = Backbone.View.extend({
     if (_.isNull(this.adServiceConfig) || _.isUndefined(this.adServiceConfig)) {
       this.adServiceConfig = {};
     }
+    // find service-monitor service
+    var smService = this.collection.find(function(s) { return s.get('name') == 'service-monitor'; });
     $(this.el).append(this.template({
       services: this.collection,
-      adServiceConfig: this.adServiceConfig
+      adServiceConfig: this.adServiceConfig,
+      sm: smService
     }));
     this.$(".ph-pagination").html(this.paginationTemplate({
       collection: this.collection
@@ -137,6 +140,7 @@ ServicesView = Backbone.View.extend({
          this.adServiceConfig.security == 'domain')) {
       this.showJoinDomainStatus();
     }
+    this.displaySmWarning();
   },
   
   startService: function(event) {
@@ -154,10 +158,12 @@ ServicesView = Backbone.View.extend({
         _this.highlightStartEl(serviceName, true);
         _this.setSliderVal(serviceName, 1); 
         _this.setStatusLoading(serviceName, false);
+        _this.displaySmWarning();
         _this.startPolling();
       },
       error: function(xhr, status, error) {
         _this.setStatusError(serviceName, xhr);
+        _this.displaySmWarning();
         _this.startPolling();
       }
     });
@@ -166,6 +172,7 @@ ServicesView = Backbone.View.extend({
   stopService: function(event) {
     var _this = this;
     var serviceName = $(event.currentTarget).data('service-name'); 
+    if (serviceName == 'service-monitor') return;
     // if already stopped, return
     if (this.getSliderVal(serviceName).toString() == "0") return; 
     this.stopPolling();
@@ -274,6 +281,7 @@ ServicesView = Backbone.View.extend({
             _this.updateStatus();
           }, _this.updateFreq - diff);
         }
+        _this.displaySmWarning();
       }
     });
   },
@@ -299,6 +307,15 @@ ServicesView = Backbone.View.extend({
       } else {
         this.$('#join-domain-status').html('<span class="alert alert-success alert-small">Not Joined</span>');
       }
+    }
+  },
+
+  displaySmWarning: function() {
+    var smService = this.collection.find(function(s) { return s.get('name') == 'service-monitor'; });
+    if (smService.get('status')) {
+      this.$('#sm-warning').hide();
+    } else {
+      this.$('#sm-warning').show();
     }
   }
 
