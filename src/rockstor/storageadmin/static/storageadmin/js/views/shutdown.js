f38fe9ddac69c21193a0e90f4f0c5e4811dea23c
Madhuri Chakravarthula <mchakravartula@gmail.com>
2014-10-13 22:23:34 +0100

    Push the missing js file. #466

---
+++ src/rockstor/storageadmin/static/storageadmin/js/views/shutdown.js
@@ -0,0 +1,109 @@
+/*
+ *
+ * @licstart  The following is the entire license notice for the 
+ * JavaScript code in this page.
+ * 
+ * Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+ * This file is part of RockStor.
+ * 
+ * RockStor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ * 
+ * RockStor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ * 
+ */
+
+ShutdownView = RockstorLayoutView.extend({
+  
+  initialize: function() {
+    // call initialize of base
+    this.constructor.__super__.initialize.apply(this, arguments);
+   this.template = window.JST.common_navbar;
+    this.paginationTemplate = window.JST.common_pagination;
+    this.timeLeft = 30;
+  },
+
+ render: function() {
+    var _this = this;
+    
+    if (confirm('Are you sure you want to Shutdown?')) {
+     _this.$('#update-modal').modal('show');
+     this.startForceRefreshTimer();
+     $.ajax({
+        url: "/api/commands/shutdown", 
+        type: "POST",
+        dataType: "json",
+        global: false, // dont show global loading indicator
+        success: function(data, status, xhr) {
+        _this.checkIfUp();
+      },
+      error: function(xhr, status, error) {
+        _this.checkIfUp();
+      }
+     });
+     }else{
+      location.reload(history.go(-1));
+    }
+    
+    return this;
+  },
+ 
+ 
+  checkIfUp: function() {
+    var _this = this;
+    this.isUpTimer = window.setInterval(function() {
+      $.ajax({
+        url: "/api/sm/sprobes/loadavg?limit=1&format=json", 
+        type: "GET",
+        dataType: "json",
+        global: false, // dont show global loading indicator
+        success: function(data, status, xhr) {
+        
+        },
+        error: function(xhr, status, error) {
+        }
+      });
+    }, 5000);
+  },
+ 
+ // countdown timeLeft seconds and then force a window reload 
+  startForceRefreshTimer: function() {
+    var _this = this;
+    this.forceRefreshTimer = window.setInterval(function() {
+      _this.timeLeft = _this.timeLeft - 1;
+      _this.showTimeRemaining();
+      
+    }, 1000);
+  },
+
+  showTimeRemaining: function() {
+    mins = Math.floor(this.timeLeft/60);
+    sec = this.timeLeft - (mins*60);
+    sec = sec >=10 ? '' + sec : '0' + sec
+    this.$('#time-left').html(mins + ':' + sec)
+    if (mins <= 1 && !this.userMsgDisplayed) {
+      this.displayUserMsg();
+      this.userMsgDisplayed = true;
+    }
+  },
+  
+  displayUserMsg: function() {
+    this.$('#user-msg').show('highlight', null, 1000);
+  }
+ 
+  
+});
+
+
+
