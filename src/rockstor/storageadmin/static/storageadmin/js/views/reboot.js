67e924c4240869836d633515121f43c821b12786
Madhuri Chakravarthula <mchakravartula@gmail.com>
2014-10-14 23:49:39 +0100

    Implement re-boot functionality. #466

---
+++ src/rockstor/storageadmin/static/storageadmin/js/views/reboot.js
@@ -0,0 +1,154 @@
+/*
+ *
+ * @licstart  The following is the entire license notice for the 
+ * JavaScript code in this page.
+ * 
+ * Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+ * This file is part of RockStor.
+ * 
+ * RockStor is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License,
+ * or (at your option) any later version.
+ * 
+ * RockStor is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @licend  The above is the entire license notice
+ * for the JavaScript code in this page.
+ * 
+ */
+
+RebootView = RockstorLayoutView.extend({
+  
+  initialize: function() {
+    // call initialize of base
+    this.constructor.__super__.initialize.apply(this, arguments);
+   this.template = window.JST.common_navbar;
+    this.paginationTemplate = window.JST.common_pagination;
+    this.timeLeft = 600;
+    this.rebootStatus = false;
+    this.isStopped=false;
+    this.isStarted=false;
+    this.isReloaded = false;
+ },
+
+ render: function() {
+    var _this = this;
+    if(_this.isStopped && !_this.isStarted){
+    _this.checkIfUp();
+    return;
+    
+    }
+    if(_this.isStopped && _this.isStarted){
+    return this;
+    }
+    
+     if (confirm('Are you sure you want to Reboot?')) {
+     $('#reboot-modal').modal('show');
+     this.startForceRefreshTimer();  
+     
+     $.ajax({
+        url: "/api/commands/reboot", 
+        type: "POST",
+        dataType: "json",
+        global: false, // dont show global loading indicator
+        success: function(data, status, xhr) {
+        _this.checkIfUp();
+      },
+      error: function(xhr, status, error) {
+       _this.checkIfUp();
+      
+        }
+       });
+      }
+    return this;
+  },
+  
+ checkIfUp: function() {
+    var _this = this;
+    
+    this.isUpTimer = window.setInterval(function() {
+      $.ajax({
+        url: "/api/sm/sprobes/loadavg?limit=1&format=json", 
+        type: "GET",
+        dataType: "json",
+        global: false, // dont show global loading indicator
+        success: function(data, status, xhr) {
+       // alert('in success _this.isStopped='+_this.isStopped);
+        if(_this.isStopped && !_this.isStarted){
+        _this.reloadWindow();
+        _this.isStarted=true;
+        }
+        else if(_this.isStopped && _this.isStarted){
+         $('#reboot-message').remove();
+         $('#reboot-timer').removeAttr('src');
+         $('#reboot-time-left').remove();
+        _this.displayUserMsg2();
+        
+        }
+        },
+        error: function(xhr, status, error) {
+         
+        _this.isStopped=true;
+      // alert('in error _this.isStopped='+_this.isStopped);
+        
+        }
+      });
+    }, 5000);
+  },
+  
+  
+  
+  // countdown timeLeft seconds and then force a window reload 
+  startForceRefreshTimer: function() {
+    var _this = this;
+    this.forceRefreshTimer = window.setInterval(function() {
+      _this.timeLeft = _this.timeLeft - 1;
+      _this.showTimeRemaining();
+      if (_this.timeLeft <= 0) {
+        _this.reloadWindow();
+      }
+    }, 1000);
+  },
+
+  showTimeRemaining: function() {
+    mins = Math.floor(this.timeLeft/60);
+    sec = this.timeLeft - (mins*60);
+    sec = sec >=10 ? '' + sec : '0' + sec
+    $('#reboot-time-left').html(mins + ':' + sec)
+    if (mins <= 1 && !this.userMsgDisplayed) {
+      this.displayUserMsg();
+      this.userMsgDisplayed = true;
+    }
+  },
+  
+  reloadWindow: function() {
+    this.clearTimers();
+    this.$('#reboot-modal').modal('hide');
+    location.reload(true);
+  },
+
+  clearTimers: function() {
+    window.clearInterval(this.isUpTimer);
+    window.clearInterval(this.forceRefreshTimer);
+  },
+    
+displayUserMsg: function() {
+    $('#time-left').remove();
+    $('#reboot-user-msg').show('highlight', null, 1000);
+  },
+
+displayUserMsg2: function() {
+    $('#reboot-user-msg2').show('highlight', null, 1000);
+  }
+  
+});
+
+
+
