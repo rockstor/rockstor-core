5ee42184f143a6f4a9843265c721c0e62608129d
Suman Chakravartula <suman@rockstor.com>
2014-10-18 15:49:25 -0700

    Update disk model and rescan api for new persistant attribute information.

--- src/rockstor/storageadmin/views/disk.py
+++ src/rockstor/storageadmin/views/disk.py
@@ -22,8 +22,8 @@ Disk view, for anything at the disk level
 
 from rest_framework.response import Response
 from django.db import transaction
-from storageadmin.models import Disk
-from fs.btrfs import (scan_disks, wipe_disk, btrfs_wipe_disk)
+from storageadmin.models import (Disk, Pool)
+from fs.btrfs import (scan_disks, wipe_disk)
 from storageadmin.serializers import DiskInfoSerializer
 from storageadmin.util import handle_exception
 from django.conf import settings
@@ -55,25 +55,33 @@ class DiskView(rfc.GenericView):
     @transaction.commit_on_success
     def _scan(self):
         disks = scan_disks(settings.MIN_DISK_SIZE)
-        for k,v in disks.items():
-            if (Disk.objects.filter(name=v['name']).exists()):
-                d = Disk.objects.get(name=v['name'])
-                if (d.size != v['size'] or d.parted != v['parted'] or
-                    d.btrfs_uuid != v['btrfs_uuid']):
-                    d.size = v['size']
-                    d.parted = v['parted']
-                    d.btrfs_uuid = v['btrfs_uuid']
-                    d.save()
-                continue
-            new_disk = Disk(name=v['name'], size=v['size'], parted=v['parted'],
-                            btrfs_uuid=v['btrfs_uuid'])
-            new_disk.save()
-        for d in Disk.objects.all():
-            if (d.name not in disks.keys()):
-                d.offline = True
-                d.save()
-        disks = Disk.objects.all()
-        ds = DiskInfoSerializer(disks)
+        for d in disks:
+            if (Disk.objects.filter(serial=d.serial).exists()):
+                do = Disk.objects.get(serial=d.serial)
+                do.size = d.size
+                do.parted = d.parted
+                do.name = d.name
+                do.offline = False
+                if (do.btrfs_uuid != d.btrfs_uuid):
+                    try:
+                        pool = Pool.objects.get(uuid=d.btrfs_uuid)
+                    except:
+                        pool = None
+                    finally:
+                        do.pool = pool
+                        do.btrfs_uuid = d.btrfs_uuid
+                do.save()
+            else:
+                new_disk = Disk(name=d.name, size=d.size, parted=d.parted,
+                                btrfs_uuid=d.btrfs_uuid, model=d.model,
+                                serial=d.serial, transport=d.transport,
+                                vendor=d.vendor)
+                new_disk.save()
+        for do in Disk.objects.all():
+            if (do.name not in [d.name for d in disks]):
+                do.offline = True
+                do.save()
+        ds = DiskInfoSerializer(Disk.objects.all())
         return Response(ds.data)
 
     @transaction.commit_on_success
@@ -87,24 +95,11 @@ class DiskView(rfc.GenericView):
             handle_exception(Exception(e_msg), request)
 
         disk.parted = False
-        disk.save()
-        return Response(DiskInfoSerializer(disk).data)
-
-    @transaction.commit_on_success
-    def _btrfs_wipe(self, dname, request):
-        disk = self._validate_disk(dname, request)
-        try:
-            btrfs_wipe_disk('/dev/%s' % disk.name)
-        except Exception, e:
-            logger.exception(e)
-            e_msg = ('Failed to wipe btrfs metadata due to a system error.')
-            handle_exception(Exception(e_msg), request)
-
         disk.btrfs_uuid = None
         disk.save()
         return Response(DiskInfoSerializer(disk).data)
 
-    def _btrfs_disk_import(sself, dname, request):
+    def _btrfs_disk_import(self, dname, request):
         """stub method for now"""
         e_msg = ('Failed to import any pools, shares and snapshots that '
                  'may be on the disk: %s. Import or backup manually' % dname)
@@ -116,7 +111,7 @@ class DiskView(rfc.GenericView):
         if (command == 'wipe'):
             return self._wipe(dname, request)
         if (command == 'btrfs-wipe'):
-            return self._btrfs_wipe(dname, request)
+            return self._wipe(dname, request)
         if (command == 'btrfs-disk-import'):
             return self._btrfs_disk_import(dname, request)
 
