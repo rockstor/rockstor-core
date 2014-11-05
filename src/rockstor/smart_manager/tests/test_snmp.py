f06d95fe8f8e2537ddfdd66113618f3474346ff4
Suman Chakravartula <suman@rockstor.com>
2014-11-05 07:56:47 -0800

    Add snmp integration tests. #469

---
+++ src/rockstor/smart_manager/tests/test_snmp.py
@@ -0,0 +1,62 @@
+"""
+Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class SNMPTests(APITestCase):
+    multi_db = True
+    fixtures = ['services.json']
+    BASE_URL = '/api/sm/services/snmpd'
+
+    def session_login(self):
+        self.client.login(username='admin', password='admin')
+
+    def test_snmp_0(self):
+        """
+        unauthorized access
+        """
+        response = self.client.get(self.BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_snmp_0_1(self):
+        """
+        config happy path
+        """
+        from smart_manager.models import Service
+        print ('service count = %d' % Service.objects.count())
+        config = {'syslocation': 'Rockstor Labs',
+                  'syscontact': 'rocky@rockstor.com',
+                  'rocommunity': 'public',
+                  'aux': (), }
+        data = {'config': config, }
+        self.session_login()
+        response = self.client.post('%s/config' % self.BASE_URL, data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK,
+                         msg=response.content)
+
+    def test_snmp_1(self):
+        """
+        config without input
+        """
+        self.session_login()
+        response = self.client.post('%s/config' % self.BASE_URL)
+        self.assertEqual(response.status_code,
+                         status.HTTP_500_INTERNAL_SERVER_ERROR,
+                         msg=response.data)
