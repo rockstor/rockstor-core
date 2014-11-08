7c1cf06064222b095517acbed62937072ca58402
Suman Chakravartula <suman@rockstor.com>
2014-11-07 18:19:44 -0800

    Improve user create api. #401

--- src/rockstor/storageadmin/views/user.py
+++ src/rockstor/storageadmin/views/user.py
@@ -24,16 +24,21 @@ from django.contrib.auth.models import User as DjangoUser
 from storageadmin.serializers import SUserSerializer
 from storageadmin.models import User
 import rest_framework_custom as rfc
-from system.users import (useradd, usermod, userdel, get_epasswd, get_users,
+from system.users import (useradd, usermod, userdel, get_users,
                           update_shell, smbpasswd, add_ssh_key)
 from storageadmin.exceptions import RockStorAPIException
-
+import pwd
+from system.ssh import is_pub_key
 import logging
 logger = logging.getLogger(__name__)
 
 
 class UserView(rfc.GenericView):
     serializer_class = SUserSerializer
+    exclude_list = ('root', 'nobody', 'bin', 'daemon', 'adm', 'sync',
+                    'shutdown', 'halt', 'mail', 'operator', 'dbus', 'rpc',
+                    'avahi', 'avahi-autoipd', 'rpcuser', 'nfsnobody',
+                    'postgres', 'ntp', 'nginx', 'postfix', 'sshd', )
 
     def get_queryset(self, *args, **kwargs):
         if ('username' in kwargs):
@@ -42,7 +47,7 @@ class UserView(rfc.GenericView):
                 return User.objects.get(username=kwargs['username'])
             except:
                 return []
-        return self._combined_users(self)
+        return self._combined_users()
 
     def _combined_users(self):
         users = list(User.objects.all())
@@ -50,8 +55,8 @@ class UserView(rfc.GenericView):
         for u in sys_users.keys():
             if (User.objects.filter(username=u).exists()):
                 continue
-            users.append(User(username=u, uid=sys_users[u][0],
-                              gid=sys_users[u][1]))
+            users.append(User(username=u, uid=int(sys_users[u][0]),
+                              gid=int(sys_users[u][1]), admin=False))
         return users
 
     def _validate_input(self, request):
@@ -78,15 +83,22 @@ class UserView(rfc.GenericView):
             handle_exception(Exception(e_msg), request)
         input_fields['shell'] = shell
         email = request.DATA.get('email', None)
-        if (email is not None and type(email) != str):
-            e_msg = ('Email must be a valid string')
+        if (email is not None and type(email) != unicode):
+            e_msg = ('Email must be a valid string. not: %s' % type(email))
             handle_exception(Exception(e_msg), request)
         input_fields['email'] = email
         input_fields['homedir'] = request.DATA.get(
             'homedir', '/home/%s' % username)
         input_fields['uid'] = request.DATA.get('uid', None)
         input_fields['gid'] = request.DATA.get('gid', None)
+        input_fields['public_key'] = self._validate_public_key(request)
         return input_fields
+
+    def _validate_public_key(self, request):
+        public_key = request.DATA.get('public_key', None)
+        if (public_key is not None and not is_pub_key(public_key)):
+            e_msg = ('Public key is invalid')
+            handle_exception(Exception(e_msg), request)
 
     @transaction.commit_on_success
     def post(self, request):
@@ -95,13 +107,19 @@ class UserView(rfc.GenericView):
 
             # Check that a django user with the same name does not exist
             e_msg = ('user: %s already exists. Please choose a different'
-                     ' username' % username)
+                     ' username' % invar['username'])
             if (DjangoUser.objects.filter(
                     username=invar['username']).exists()):
                 handle_exception(Exception(e_msg), request)
             users = self._combined_users()
             for u in users:
                 if (u.username == invar['username']):
+                    handle_exception(Exception(e_msg), request)
+                if (u.uid == invar['uid']):
+                    e_msg = ('uid: %d already exists.' % invar['uid'])
+                    handle_exception(Exception(e_msg), request)
+                if (u.gid == invar['gid']):
+                    e_msg = ('gic: %d already exists.' % invar['gid'])
                     handle_exception(Exception(e_msg), request)
 
             if (invar['admin']):
@@ -111,18 +129,16 @@ class UserView(rfc.GenericView):
                 auser.is_active = True
                 auser.save()
 
-            # Create unix user
-            if (invar['uid'] is None):
-                max_uid = settings.START_UID
-                for u in users:
-                    if (u.uid > max_uid):
-                        max_uid = u.uid
-                invar['uid'] = max_uid + 1
-            useradd(invar['username'], invar['uid'], invar['shell'])
+            useradd(invar['username'], invar['shell'], uid=invar['uid'],
+                    gid=invar['gid'])
+            pw_entries = pwd.getpwnam(invar['username'])
+            invar['uid'] = pw_entries[2]
+            invar['gid'] = pw_entries[3]
             usermod(invar['username'], invar['password'])
             smbpasswd(invar['username'], invar['password'])
             if (invar['public_key'] is not None):
                 add_ssh_key(invar['username'], invar['public_key'])
+            del(invar['password'])
             suser = User(**invar)
             suser.save()
             return Response(SUserSerializer(suser).data)
