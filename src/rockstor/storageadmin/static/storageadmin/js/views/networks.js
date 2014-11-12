143dba634dd67a1cf3688af14f129ce8b6cf27c0
Madhuri Chakravarthula <mchakravartula@gmail.com>
2014-11-12 22:21:39 +0000

    Detect network interfaces before rendering in view. #521

--- src/rockstor/storageadmin/static/storageadmin/js/views/networks.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/networks.js
@@ -1,27 +1,27 @@
 /*
  *
- * @licstart  The following is the entire license notice for the 
+ * @licstart  The following is the entire license notice for the
  * JavaScript code in this page.
- * 
+ *
  * Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
  * This file is part of RockStor.
- * 
+ *
  * RockStor is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
- * 
+ *
  * RockStor is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @licend  The above is the entire license notice
  * for the JavaScript code in this page.
- * 
+ *
  */
 
 /* Services */
@@ -37,14 +37,15 @@ NetworksView = Backbone.View.extend({
 
   render: function() {
     var _this = this;
-    this.collection.fetch()
+    this.collection.fetch();
     return this;
   },
-  
+ 
  
   renderNetworks: function() {
     var _this = this;
     $(this.el).empty();
+    this.scanNetwork();
     $(this.el).append(this.template({
       networks: this.collection
     }));
@@ -52,8 +53,24 @@ NetworksView = Backbone.View.extend({
       collection: this.collection
     }));
   },
-  
+ 
+  scanNetwork: function() {
+        var _this = this;
+        $.ajax({
+          url: "/api/network",
+          type: "POST",
+          dataType: "json",
+          success: function(data, status, xhr) {
+             _this.collection.fetch();
+          },
+          error: function(xhr, status, error) {
+            logger.debug(error);
+          }
+        });
+      },
+ 
 });
 
 // Add pagination
 Cocktail.mixin(NetworksView, PaginationMixin);
+
