2941f2844dad8ab67f82827b767157a061bcfe4f
Sujeet Sreenivasan <sujeetsr@gmail.com>
2014-11-12 21:10:00 -0800

    Fix user save for existing user, add shell

--- src/rockstor/storageadmin/static/storageadmin/js/views/add_user.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/add_user.js
@@ -35,27 +35,31 @@ AddUserView = RockstorLayoutView.extend({
     // set template
     this.template = window.JST.users_add_user;
     this.username = this.options.username;
-    this.user = new User({username: this.username});
-    this.dependencies.push(this.user);
+    if (!_.isUndefined(this.username)) {
+      this.user = new User({username: this.username});
+      this.dependencies.push(this.user);
+    } else {
+      this.user = new User();
+    }
     this.groups = new GroupCollection();
     this.groups.pageSize = RockStorGlobals.maxPageSize;
     this.dependencies.push(this.groups);
   },
 
   render: function() {
-    this.fetch(this.renderExportForm, this);
+    this.fetch(this.renderUserForm, this);
     return this;
   },
 
-  renderExportForm: function() {
+  renderUserForm: function() {
     var _this = this;
     //$(this.el).html(this.template());
     $(this.el).html(this.template({
-        username: this.username,
-        user: this.user,
-	groups: this.groups
+      username: this.username,
+      user: this.user,
+      groups: this.groups
 
-      }));
+    }));
 
     this.$('#user-create-form :input').tooltip({placement: 'right'});
     this.$('#group').chosen();
@@ -77,67 +81,64 @@ AddUserView = RockstorLayoutView.extend({
         }
       },
 
-
-
       submitHandler: function() {
         var username = _this.$("#username").val();
         var password = _this.$("#password").val();
         var admin = _this.$("#admin").prop("checked");
+        var shell = _this.$("#shell").val();
         var public_key = _this.$("#public_key").val();
-	if (_.isEmpty(public_key)) {
-	    public_key = null;
-	}
-	var group = _this.$("#group").val();
+        if (_.isEmpty(public_key)) {
+          public_key = null;
+        }
+        var group = _this.$("#group").val();
         if (group == 'Create a new one') {
-	    group = null
-	}
-	if(_this.username != null && _this.user != null){
-            var user = new User({username: _this.username});
-        	if (!_.isEmpty(password)) {
-                user.set({password: password});
-              } else {
-                user.unset('password');
-              }
-	    if (!_.isEmpty(public_key)) {
-		user.set({public_key: public_key});
-	    } else {
-		user.unset('public_key');
-	    }
-            user.set({admin: admin});
-	    user.set({group: group});
-            user.save(null, {
-                success: function(model, response, options) {
-                    app_router.navigate("users", {trigger: true});
-                },
-                error: function(model, xhr, options) {
-                }
-              });
+          group = null
+        }
+        if(_this.username != null){
+          if (!_.isEmpty(password)) {
+            _this.user.set({password: password});
+          } else {
+            _this.user.unset('password');
+          }
+          if (!_.isEmpty(public_key)) {
+            _this.user.set({public_key: public_key});
+          } else {
+            _this.user.unset('public_key');
+          }
+          _this.user.set({admin: admin});
+          _this.user.set({group: group});
+          _this.user.set({shell: shell});
+          _this.user.save(null, {
+            success: function(model, response, options) {
+              app_router.navigate("users", {trigger: true});
+            },
+            error: function(model, xhr, options) {
+            }
+          });
         } else {
-            // create a dummy user model class that does not have idAttribute
-            // = username, so backbone will treat is as a new object,
-            // ie isNew will return true
-            var tmpUserModel = Backbone.Model.extend({
-                urlRoot: "/api/users"
-              });
-	        var user = new tmpUserModel()
-	        user.save(
-	          {
-	            username: username,
-	            password: password,
-	            admin: admin,
-	            public_key: public_key,
-		    group: group,
-	          },
-	          {
-	            success: function(model, response, options) {
-	              _this.$('#user-create-form :input').tooltip('hide');
-	              app_router.navigate("users", {trigger: true});
-	            },
-	            error: function(model, xhr, options) {
-	              _this.$('#user-create-form :input').tooltip('hide');
-	            }
-	          }
-	        );
+          // create a dummy user model class that does not have idAttribute
+          // = username, so backbone will treat is as a new object,
+          // ie isNew will return true
+          var tmpUserModel = Backbone.Model.extend({
+            urlRoot: "/api/users"
+          });
+          var user = new tmpUserModel()
+          user.save({
+            username: username,
+            password: password,
+            admin: admin,
+            group: group,
+            shell: shell,
+            public_key: public_key
+          }, {
+            success: function(model, response, options) {
+              _this.$('#user-create-form :input').tooltip('hide');
+              app_router.navigate("users", {trigger: true});
+            },
+            error: function(model, xhr, options) {
+              _this.$('#user-create-form :input').tooltip('hide');
+            }
+          });
         }
         return false;
       }
@@ -146,6 +147,7 @@ AddUserView = RockstorLayoutView.extend({
   },
 
   cancel: function() {
+    event.preventDefault();
     app_router.navigate("users", {trigger: true});
   }
 
