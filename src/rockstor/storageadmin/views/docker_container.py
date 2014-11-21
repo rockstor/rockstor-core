90dd5dd049e10c90833b44dec4442507bc18df9b
Suman Chakravartula <suman@rockstor.com>
2014-11-20 19:05:13 -0800

    Add docker image and container models and stub views. #542

---
+++ src/rockstor/storageadmin/views/docker_container.py
@@ -0,0 +1,36 @@
+"""
+Copyright (c) 2012-2014 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import rest_framework_custom as rfc
+from storageadmin.serializer import DockerContainerSerializer
+
+
+class DockerContainerView(rfc.GenericView):
+    serializer_class = DockerContainerSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        pass
+
+    def post(self, request):
+        pass
+
+    def put(self, request):
+        pass
+
+    def delete(self, request):
+        pass
