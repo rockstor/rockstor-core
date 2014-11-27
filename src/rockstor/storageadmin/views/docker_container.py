67c474beeb1b08da4e2269f0649595a342a04b37
Suman Chakravartula <suman@rockstor.com>
2014-11-27 11:50:23 -0800

    add docker to known services for service monitor. #542

--- src/rockstor/storageadmin/views/docker_container.py
+++ src/rockstor/storageadmin/views/docker_container.py
@@ -17,7 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import rest_framework_custom as rfc
-from storageadmin.serializer import DockerContainerSerializer
+from storageadmin.serializers import DockerContainerSerializer
 
 
 class DockerContainerView(rfc.GenericView):
