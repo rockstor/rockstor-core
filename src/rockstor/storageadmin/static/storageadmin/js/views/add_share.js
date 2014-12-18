d87531427ef9fdbc91cac031e20b32ae1ae83842
Suman Chakravartula <suman@rockstor.com>
2014-12-18 15:26:03 -0800

    implement front end support to set compression. #468

--- src/rockstor/storageadmin/static/storageadmin/js/views/add_share.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/add_share.js
@@ -91,6 +91,12 @@ AddShareView = Backbone.View.extend({
 
         $('#add-share-form :input').tooltip({placement: 'right'});
 
+	this.$('#compression').tooltip({
+	    html: true,
+	    placement: 'right',
+	    title: "Choose a compression algorithm for this Share. By default, parent pool's compression algorithm is inherited.<br> If you like to set pool wide compression, don't choose anything here. If you want finer control of this particular Share's compression algorithm, you can set it here.<br><strong>zlib: </strong>slower but higher compression ratio.<br><strong>lzo: </strong>faster compression/decompression, but compression ratio lower than zlib"
+	});
+
         $('#add-share-form').validate({
             onfocusout: false,
             onkeyup: false,
@@ -107,6 +113,10 @@ AddShareView = Backbone.View.extend({
               disableButton(button);
               var share_name = $('#share_name').val();
               var pool_name = $('#pool_name').val();
+	      var compression = $('#compression').val();
+	      if (compression == 'no') {
+		  compression = null;
+	      }
               var size = $('#share_size').val();
               var sizeFormat = size.replace(/[^a-z]/gi, "");
               var size_array = size.split(sizeFormat)
@@ -124,7 +134,7 @@ AddShareView = Backbone.View.extend({
                 url: "/api/shares",
                 type: "POST",
                 dataType: "json",
-                data: {sname: share_name, "pool": pool_name, "size": size_value},
+                data: {sname: share_name, "pool": pool_name, "size": size_value, "compression": compression, },
                 success: function() {
                   enableButton(button);
                   _this.$('#add-share-form :input').tooltip('hide');
