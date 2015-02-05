65ed9986815667aacdc3554b3f18a60361a8fea3
Suman Chakravartula <suman@rockstor.com>
2015-02-05 13:13:58 -0800

    implement replicashare delete api. #574

--- src/rockstor/storageadmin/static/storageadmin/js/views/replication_receive.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/replication_receive.js
@@ -1,27 +1,27 @@
 /*
  *
- * @licstart  The following is the entire license notice for the 
+ * @licstart  The following is the entire license notice for the
  * JavaScript code in this page.
- * 
+ *
  * Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
  * This file is part of RockStor.
- * 
+ *
  * RockStor is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
- * 
+ *
  * RockStor is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @licend  The above is the entire license notice
  * for the JavaScript code in this page.
- * 
+ *
  */
 
 ReplicationReceiveView = RockstorLayoutView.extend({
@@ -92,7 +92,7 @@ renderReceives: function() {
 	this.$('input.service-status').simpleSlider({
 		"theme": "volume",
 		allowedValues: [0,1],
-		snap: true 
+		snap: true
 	});
 
 	this.$('input.service-status').each(function(i, el) {
@@ -114,14 +114,14 @@ deleteReceivedTask: function(event) {
 	var rShare = $(event.currentTarget).attr("data-rshare-name");
 	if(confirm("Delete Received Replication task:  " + rShare + ". Are you sure?")){
 		$.ajax({
-			url: '/api/sm/replicas/' + rId,
+			url: '/api/sm/replicas/rshare/' + rId,
 			type: "DELETE",
 			dataType: "json",
 			success: function() {
 			enableButton(button);
 			_this.collection.fetch({
 				success: function() {
-				_this.renderReplicas();
+				_this.renderReceives();
 			}
 			});
 		},
@@ -134,9 +134,9 @@ deleteReceivedTask: function(event) {
 
 startService: function(event) {
 	var _this = this;
-	var serviceName = this.serviceName; 
+	var serviceName = this.serviceName;
 	// if already started, return
-	if (this.getSliderVal(serviceName).toString() == "1") return; 
+	if (this.getSliderVal(serviceName).toString() == "1") return;
 	this.stopPolling();
 	this.setStatusLoading(serviceName, true);
 	$.ajax({
@@ -145,7 +145,7 @@ startService: function(event) {
 		dataType: "json",
 		success: function(data, status, xhr) {
 		_this.highlightStartEl(serviceName, true);
-		_this.setSliderVal(serviceName, 1); 
+		_this.setSliderVal(serviceName, 1);
 		_this.setStatusLoading(serviceName, false);
 		_this.startPolling();
 		_this.displayReplicationWarning(serviceName);
@@ -159,9 +159,9 @@ startService: function(event) {
 
 stopService: function(event) {
 	var _this = this;
-	var serviceName = $(event.currentTarget).data('service-name'); 
+	var serviceName = $(event.currentTarget).data('service-name');
 	// if already stopped, return
-	if (this.getSliderVal(serviceName).toString() == "0") return; 
+	if (this.getSliderVal(serviceName).toString() == "0") return;
 	this.stopPolling();
 	this.setStatusLoading(serviceName, true);
 	$.ajax({
@@ -170,7 +170,7 @@ stopService: function(event) {
 		dataType: "json",
 		success: function(data, status, xhr) {
 		_this.highlightStartEl(serviceName, false);
-		_this.setSliderVal(serviceName, 0); 
+		_this.setSliderVal(serviceName, 0);
 		_this.setStatusLoading(serviceName, false);
 		_this.startPolling();
 		_this.displayReplicationWarning(serviceName);
@@ -217,10 +217,10 @@ updateStatus: function() {
 		var serviceName = service.get('name');
 		if (service.get('status')) {
 			_this.highlightStartEl(serviceName, true);
-			_this.setSliderVal(serviceName, 1); 
+			_this.setSliderVal(serviceName, 1);
 		} else {
 			_this.highlightStartEl(serviceName, false);
-			_this.setSliderVal(serviceName, 0); 
+			_this.setSliderVal(serviceName, 0);
 		}
 		var currentTime = new Date().getTime();
 		var diff = currentTime - _this.startTime;
@@ -229,7 +229,7 @@ updateStatus: function() {
 			_this.updateStatus();
 		} else {
 			// wait till updateFreq msec has elapsed since startTime
-			_this.timeoutId = window.setTimeout( function() { 
+			_this.timeoutId = window.setTimeout( function() {
 				_this.updateStatus();
 			}, _this.updateFreq - diff);
 		}
@@ -276,11 +276,9 @@ displayReplicationWarning: function(serviceName) {
 
 cleanup: function() {
 	this.stopPolling();
-}	  
+}
 
 });
 
 // Add pagination
 Cocktail.mixin(ReplicationReceiveView, PaginationMixin);
-
-
