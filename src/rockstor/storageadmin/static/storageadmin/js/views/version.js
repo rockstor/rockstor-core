2ce9ee9dec27ea2d5868798c4430db50d780c46a
Suman Chakravartula <suman@rockstor.com>
2015-03-05 12:38:42 -0800

    Add web-ui support to enable/disable auto update. #604

--- src/rockstor/storageadmin/static/storageadmin/js/views/version.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/version.js
@@ -25,151 +25,196 @@
  */
 
 VersionView = RockstorLayoutView.extend({
-  events: {
-    'click #update': 'update',
-    'click #donateYes': 'donateYes',
-    'click #donateNo': 'donateNo'
-  },
+    events: {
+	'click #update': 'update',
+	'click #donateYes': 'donateYes',
+	'click #donateNo': 'donateNo',
+	'click #enableAuto': 'enableAutoUpdate',
+	'click #disableAuto': 'disableAutoUpdate',
+    },
 
-  initialize: function() {
-    // call initialize of base
-    this.constructor.__super__.initialize.apply(this, arguments);
-    this.template = window.JST.update_version_info;
+    initialize: function() {
+	// call initialize of base
+	this.constructor.__super__.initialize.apply(this, arguments);
+	this.template = window.JST.update_version_info;
 
-    this.paginationTemplate = window.JST.common_pagination;
-    this.timeLeft = 300;
-  },
+	this.paginationTemplate = window.JST.common_pagination;
+	this.timeLeft = 300;
+    },
 
-  render: function() {
-    var _this = this;
-    $.ajax({
-      url: "/api/commands/update-check",
-      type: "POST",
-      dataType: "json",
-      success: function(data, status, xhr) {
-        _this.currentVersion = data[0];
-        _this.mostRecentVersion = data[1];
-        _this.changeList = data[2];
-        _this.renderVersionInfo();
-      },
-      error: function(xhr, status, error) {
-      }
-    });
-    return this;
-  },
+    render: function() {
+	var _this = this;
+	$.ajax({
+	    url: "/api/commands/update-check",
+	    type: "POST",
+	    dataType: "json",
+	    success: function(data, status, xhr) {
+		_this.currentVersion = data[0];
+		_this.mostRecentVersion = data[1];
+		_this.changeList = data[2];
+		//_this.renderVersionInfo();
+		_this.checkAutoUpdateStatus();
+	    },
+	    error: function(xhr, status, error) {
+	    }
+	});
+	return this;
+    },
 
-  renderVersionInfo: function() {
-    var _this = this;
-    $(this.el).html(this.template({
-      currentVersion: this.currentVersion,
-      mostRecentVersion: this.mostRecentVersion,
-      changeList: this.changeList
-    }));
-    this.$('#update-modal').modal({
-      keyboard: false,
-      backdrop: 'static',
-      show: false
-    });
-    // Show or hide custom contrib textfield
-    this.$('#contrib-custom').click(function(e) {
-      _this.$('#custom-amount').css('display', 'inline');
-    });
-    this.$('#contrib20').click(function(e) {
-      _this.$('#custom-amount').css('display', 'none');
-    });
-    this.$('#contrib30').click(function(e) {
-      _this.$('#custom-amount').css('display', 'none');
-    });
-  },
+    checkAutoUpdateStatus: function() {
+	var _this = this;
+	$.ajax({
+	    url: '/api/commands/auto-update-status',
+	    type: 'POST',
+	    dataType: 'json',
+	    success: function(data, status, xhr) {
+		_this.autoUpdateEnabled = data.enabled;
+		_this.renderVersionInfo();
+	    },
+	    error: function(xhr, status, error) {
+	    }
+	});
+	return this;
+    },
 
-  donateYes: function() {
-    contrib = this.$('input[type="radio"][name="contrib"]:checked').val();
-    if (contrib=='custom') {
-      contrib = $('#custom-amount').val();
-    }
-    if (_.isNull(contrib) || _.isEmpty(contrib) || isNaN(contrib)) {
-      contrib = 0; // set contrib to 0, let user input the number on paypal
-    }
-    this.$('input[name="amount"]').val(contrib);
-    this.$('#contrib-form').submit()
-    this.update();
-  },
+    renderVersionInfo: function() {
 
-  donateNo: function() {
-    this.update();
-  },
+	var _this = this;
+	$(this.el).html(this.template({
+	    currentVersion: this.currentVersion,
+	    mostRecentVersion: this.mostRecentVersion,
+	    changeList: this.changeList,
+	    autoUpdateEnabled: this.autoUpdateEnabled
+	}));
+	this.$('#update-modal').modal({
+	    keyboard: false,
+	    backdrop: 'static',
+	    show: false
+	});
+	// Show or hide custom contrib textfield
+	this.$('#contrib-custom').click(function(e) {
+	    _this.$('#custom-amount').css('display', 'inline');
+	});
+	this.$('#contrib20').click(function(e) {
+	    _this.$('#custom-amount').css('display', 'none');
+	});
+	this.$('#contrib30').click(function(e) {
+	    _this.$('#custom-amount').css('display', 'none');
+	});
+    },
 
-  update: function() {
-    this.$('#update-modal').modal('show');
-    this.startForceRefreshTimer();
-    $.ajax({
-      url: "/api/commands/update",
-      type: "POST",
-      dataType: "json",
-      global: false, // dont show global loading indicator
-      success: function(data, status, xhr) {
-        _this.checkIfUp();
-      },
-      error: function(xhr, status, error) {
-        _this.checkIfUp();
-      }
-    });
-  },
+    donateYes: function() {
+	contrib = this.$('input[type="radio"][name="contrib"]:checked').val();
+	if (contrib=='custom') {
+	    contrib = $('#custom-amount').val();
+	}
+	if (_.isNull(contrib) || _.isEmpty(contrib) || isNaN(contrib)) {
+	    contrib = 0; // set contrib to 0, let user input the number on paypal
+	}
+	this.$('input[name="amount"]').val(contrib);
+	this.$('#contrib-form').submit()
+	this.update();
+    },
 
-  checkIfUp: function() {
-    var _this = this;
-    this.isUpTimer = window.setInterval(function() {
-      $.ajax({
-        url: "/api/sm/sprobes/loadavg?limit=1&format=json",
-        type: "GET",
-        dataType: "json",
-        global: false, // dont show global loading indicator
-        success: function(data, status, xhr) {
-          _this.reloadWindow();
-        },
-        error: function(xhr, status, error) {
-          // server is not up, continue checking
-        }
-      });
-    }, 5000);
-  },
+    donateNo: function() {
+	this.update();
+    },
 
-  // countdown timeLeft seconds and then force a window reload
-  startForceRefreshTimer: function() {
-    var _this = this;
-    this.forceRefreshTimer = window.setInterval(function() {
-      _this.timeLeft = _this.timeLeft - 1;
-      _this.showTimeRemaining();
-      if (_this.timeLeft <= 0) {
-        _this.reloadWindow();
-      }
-    }, 1000);
-  },
+    update: function() {
+	this.$('#update-modal').modal('show');
+	this.startForceRefreshTimer();
+	$.ajax({
+	    url: "/api/commands/update",
+	    type: "POST",
+	    dataType: "json",
+	    global: false, // dont show global loading indicator
+	    success: function(data, status, xhr) {
+		_this.checkIfUp();
+	    },
+	    error: function(xhr, status, error) {
+		_this.checkIfUp();
+	    }
+	});
+    },
 
-  showTimeRemaining: function() {
-    mins = Math.floor(this.timeLeft/60);
-    sec = this.timeLeft - (mins*60);
-    sec = sec >=10 ? '' + sec : '0' + sec
-    this.$('#time-left').html(mins + ':' + sec)
-    if (mins <= 1 && !this.userMsgDisplayed) {
-      this.displayUserMsg();
-      this.userMsgDisplayed = true;
-    }
-  },
+    checkIfUp: function() {
+	var _this = this;
+	this.isUpTimer = window.setInterval(function() {
+	    $.ajax({
+		url: "/api/sm/sprobes/loadavg?limit=1&format=json",
+		type: "GET",
+		dataType: "json",
+		global: false, // dont show global loading indicator
+		success: function(data, status, xhr) {
+		    _this.reloadWindow();
+		},
+		error: function(xhr, status, error) {
+		    // server is not up, continue checking
+		}
+	    });
+	}, 5000);
+    },
 
-  reloadWindow: function() {
-    this.clearTimers();
-    this.$('#update-modal').modal('hide');
-    location.reload(true);
-  },
+    // countdown timeLeft seconds and then force a window reload
+    startForceRefreshTimer: function() {
+	var _this = this;
+	this.forceRefreshTimer = window.setInterval(function() {
+	    _this.timeLeft = _this.timeLeft - 1;
+	    _this.showTimeRemaining();
+	    if (_this.timeLeft <= 0) {
+		_this.reloadWindow();
+	    }
+	}, 1000);
+    },
 
-  clearTimers: function() {
-    window.clearInterval(this.isUpTimer);
-    window.clearInterval(this.forceRefreshTimer);
-  },
+    showTimeRemaining: function() {
+	mins = Math.floor(this.timeLeft/60);
+	sec = this.timeLeft - (mins*60);
+	sec = sec >=10 ? '' + sec : '0' + sec
+	this.$('#time-left').html(mins + ':' + sec)
+	if (mins <= 1 && !this.userMsgDisplayed) {
+	    this.displayUserMsg();
+	    this.userMsgDisplayed = true;
+	}
+    },
 
-  displayUserMsg: function() {
-    this.$('#user-msg').show('highlight', null, 1000);
-  }
+    reloadWindow: function() {
+	this.clearTimers();
+	this.$('#update-modal').modal('hide');
+	location.reload(true);
+    },
+
+    clearTimers: function() {
+	window.clearInterval(this.isUpTimer);
+	window.clearInterval(this.forceRefreshTimer);
+    },
+
+    displayUserMsg: function() {
+	this.$('#user-msg').show('highlight', null, 1000);
+    },
+
+    enableAutoUpdate: function() {
+	return this.toggleAutoUpdate('enable-auto-update');
+    },
+
+    disableAutoUpdate: function() {
+	return this.toggleAutoUpdate('disable-auto-update');
+    },
+
+    toggleAutoUpdate: function(updateFlag) {
+	var _this = this;
+	$.ajax({
+            url: "/api/commands/" + updateFlag,
+            type: "POST",
+            dataType: "json",
+	    success: function(data, status, xhr) {
+		_this.reloadWindow();
+            },
+            error: function(xhr, status, error) {
+		// server is not up, continue checking
+            }
+	});
+    },
+
 
 });
