1bd26368ed4f3afcee169224b851847a9c757f25
Suman Chakravartula <suman@rockstor.com>
2015-04-24 18:11:25 -0700

    add helper methods for smartctl. #498

---
+++ src/rockstor/system/smart.py
@@ -0,0 +1,38 @@
+"""
+Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from osi import run_command
+
+SMART = '/usr/sbin/smartctl'
+
+
+def info(device):
+    #get smart info of the device, such as availability
+    o, e, rc = run_command([SMART, '--info', device])
+
+def capabilities(device):
+    #such information as which tests does the device supported_services
+    o, e, rc = run_command([SMART, '-c', device])
+
+def run_test(device, test):
+    #start a smart test(short, long or conveyance)
+    o, e, rc = run_command([SMART, '-t', test, device])
+
+def test_results(device, test):
+    #get results of a specific test
+    o, e, rc = run_command([SMART, '-l', test, device])
