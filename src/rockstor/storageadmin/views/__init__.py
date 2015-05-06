493ebb1b3818acc6271de275c032f276400094f2
Suman Chakravartula <suman@rockstor.com>
2015-05-06 15:21:25 -0700

    refactor share list, detail and nfs views for drf upgrade. #637

--- src/rockstor/storageadmin/views/__init__.py
+++ src/rockstor/storageadmin/views/__init__.py
@@ -19,7 +19,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from home import (login_page, login_submit, logout_user, home)
 from snapshot import SnapshotView
-from share import ListShareView
+from share import (ShareListView, ShareDetailView)
+from share_nfs import (ShareNFSListView, ShareNFSDetailView)
 from pool import (PoolListView, PoolDetailView)
 from disk import (DiskListView, DiskDetailView)
 from command import CommandView
@@ -30,7 +31,6 @@ from login import LoginView
 from user import UserView
 from support import SupportView
 from dashboardconfig import DashboardConfigView
-from share_nfs import ListShareNFSView
 from network import NetworkListView
 from network import NetworkDetailView
 from pool_scrub import PoolScrubView
