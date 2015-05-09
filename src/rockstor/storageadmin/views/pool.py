1dd4ef34a14dab8741e08c615c25f513152d1242
Sam Richards <sbrichards@gmail.com>
2015-05-08 17:39:22 -0700

    Consolidate raid0 & raid1 disk check in post view

--- src/rockstor/storageadmin/views/pool.py
+++ src/rockstor/storageadmin/views/pool.py
@@ -227,11 +227,8 @@ class PoolListView(PoolMixin, rfc.GenericView):
                 e_msg = ('Unsupported raid level. use one of: %s' %
                          self.RAID_LEVELS)
                 handle_exception(Exception(e_msg), request)
-            if (raid_level == self.RAID_LEVELS[1] and len(disks) == 1):
-                e_msg = ('More than one disk is required for the raid '
-                         'level: %s' % raid_level)
-                handle_exception(Exception(e_msg), request)
-            if (raid_level == self.RAID_LEVELS[2] and len(disks) < 2):
+            # consolidated raid0 & raid 1 disk check
+            if (raid_level in self.RAID_LEVELS[1:3] and len(disks) == 1):
                 e_msg = ('At least two disks are required for the raid level: '
                          '%s' % raid_level)
                 handle_exception(Exception(e_msg), request)
