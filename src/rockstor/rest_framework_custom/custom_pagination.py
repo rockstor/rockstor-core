086a74a33d588cea8339e1c091fff1aab09660e1
Suman Chakravartula <suman@rockstor.com>
2015-06-04 16:48:34 -0700

    Add custom pagination class to support page_size query parameter. #645

---
+++ src/rockstor/rest_framework_custom/custom_pagination.py
@@ -0,0 +1,23 @@
+"""
+Copyright (c) 2012-2015 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from rest_framework import pagination
+
+
+class CustomPagination(pagination.PageNumberPagination):
+    page_size_query_param = 'page_size'
