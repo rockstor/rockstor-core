80d5f62ab9a701f822b69a829845306badfbd35f
Suman Chakravartula <suman@rockstor.com>
2015-07-13 14:02:52 -0700

    add basic prototype for config restore. #392

--- src/rockstor/storageadmin/views/config_backup.py
+++ src/rockstor/storageadmin/views/config_backup.py
@@ -26,56 +26,14 @@ from storageadmin.serializers import ConfigBackupSerializer
 from storageadmin.util import handle_exception
 import rest_framework_custom as rfc
 from django.core.management import call_command
+from system.osi import run_command
 
 import logging
 logger = logging.getLogger(__name__)
 
 class ConfigBackupMixin(object):
     serializer_class = ConfigBackupSerializer
-
-    @staticmethod
-    def _restart_samba():
-        out = status()
-        if (out[2] == 0):
-            restart_samba()
-
-    @classmethod
-    def _validate_input(cls, request, smbo=None):
-        options = {}
-        def_opts = cls.DEF_OPTS
-        if (smbo is not None):
-            def_opts = cls.DEF_OPTS.copy()
-            def_opts['comment'] = smbo.comment
-            def_opts['browsable'] = smbo.browsable
-            def_opts['guest_ok'] = smbo.guest_ok
-            def_opts['read_only'] = smbo.read_only
-
-        options['comment'] = request.data.get('comment', def_opts['comment'])
-        options['browsable'] = request.data.get('browsable',
-                                                def_opts['browsable'])
-
-        options['custom_config'] = request.data.get('custom_config', [])
-        if (type(options['custom_config']) != list):
-            e_msg = ('custom config must be a list of strings')
-            handle_exception(Exception(e_msg), request)
-        if (options['browsable'] not in cls.BOOL_OPTS):
-            e_msg = ('Invalid choice for browsable. Possible '
-                     'choices are yes or no.')
-            handle_exception(Exception(e_msg), request)
-        options['guest_ok'] = request.data.get('guest_ok',
-                                               def_opts['guest_ok'])
-        if (options['guest_ok'] not in cls.BOOL_OPTS):
-            e_msg = ('Invalid choice for guest_ok. Possible '
-                     'options are yes or no.')
-            handle_exception(Exception(e_msg), request)
-        options['read_only'] = request.data.get('read_only',
-                                                def_opts['read_only'])
-        if (options['read_only'] not in cls.BOOL_OPTS):
-            e_msg = ('Invalid choice for read_only. Possible '
-                     'options are yes or no.')
-            handle_exception(Exception(e_msg), request)
-        return options
-
+    cb_dir = os.path.join(settings.STATIC_ROOT, 'config-backups')
 
 class ConfigBackupListView(ConfigBackupMixin, rfc.GenericView):
     queryset = ConfigBackup.objects.all()
@@ -83,17 +41,17 @@ class ConfigBackupListView(ConfigBackupMixin, rfc.GenericView):
     @transaction.atomic
     def post(self, request):
         from datetime import datetime
-        filename = ('backup-%s.tgz' % datetime.now().strftime('%Y-%m-%d-%H%M%S'))
-        cbo = ConfigBackup(filename=filename)
-        cbo.save()
-        logger.debug('filename = %s' % filename)
-        logger.debug('cbo filename = %s' % cbo.filename)
-        cb_dir = os.path.join(settings.STATIC_ROOT, 'config_backups')
-        if (not os.path.isdir(cb_dir)):
-            os.mkdir(cb_dir)
-        fp = os.path.join(cb_dir, filename)
+        filename = ('backup-%s.json' % datetime.now().strftime('%Y-%m-%d-%H%M%S'))
+        if (not os.path.isdir(self.cb_dir)):
+            os.mkdir(self.cb_dir)
+        fp = os.path.join(self.cb_dir, filename)
         with open(fp, 'w') as dfo:
             call_command('dumpdata', 'storageadmin', stdout=dfo)
+        run_command(['/usr/bin/gzip', fp])
+        gz_name = ('%s.gz' % filename)
+        size = os.stat(os.join(self.cb_dir, gz_name)).st_size
+        cbo = ConfigBackup(filename=filename, size=size)
+        cbo.save()
         return Response(ConfigBackupSerializer(cbo).data)
 
 
@@ -101,14 +59,20 @@ class ConfigBackupDetailView(ConfigBackupMixin, rfc.GenericView):
 
     @transaction.atomic
     def delete(self, request, backup_id):
-        logger.debug('in config backup delete')
         try:
-            ConfigBackup.objects.get(id=backup_id).delete()
+            cbo = ConfigBackup.objects.get(id=backup_id)
+            fp = os.path.join(self.cb_dir, cbo.filename)
+            if (os.path.isfile(fp)):
+                os.remove(fp)
+            cbo.delete()
             return Response()
         except:
-            e_msg = ('Config backup for the id(%s) does not exist' % smb_id)
+            e_msg = ('Config backup for the id(%s) does not exist' % backup_id)
             handle_exception(Exception(e_msg), request)
 
     @transaction.atomic
-    def post(self, request, backup_id, command):
+    def post(self, request, backup_id):
+        command = request.data.get('command', 'restore')
+        if (command == 'restore'):
+            logger.debug('restored!!!')
         return Response()
