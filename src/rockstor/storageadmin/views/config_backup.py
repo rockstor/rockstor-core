8c4d3759ad53ec9f64c15cd190c64b8c82cd9147
Suman Chakravartula <suman@rockstor.com>
2015-07-13 16:33:21 -0700

    add restore stub code with a simple ztask. #392

--- src/rockstor/storageadmin/views/config_backup.py
+++ src/rockstor/storageadmin/views/config_backup.py
@@ -27,9 +27,30 @@ from storageadmin.util import handle_exception
 import rest_framework_custom as rfc
 from django.core.management import call_command
 from system.osi import run_command
+from datetime import datetime
 
 import logging
 logger = logging.getLogger(__name__)
+
+from django_ztask.decorators import task
+@task()
+def restore_config(cbid):
+    cbo = ConfigBackup.objects.get(id=cbid)
+    fp = os.path.join(settings.STATIC_ROOT, 'config-backups', cbo.filename)
+    import gzip
+    import json
+    from cli.rest_util import api_call
+    gfo = gzip.open(fp)
+    ml = json.load(gfo)
+    gfo.close()
+    base_url = 'https://localhost/api'
+    for i in ml:
+        if (i['model'] == 'storageadmin.user'):
+            ufields = i['fields']
+            url = ('%s/users' % base_url)
+            ufields['password'] = 'rockstor'
+            api_call(url, data=ufields, calltype='post', save_error=False)
+
 
 class ConfigBackupMixin(object):
     serializer_class = ConfigBackupSerializer
@@ -52,21 +73,21 @@ class ConfigBackupListView(ConfigBackupMixin, rfc.GenericView):
 
     @transaction.atomic
     def post(self, request):
-        from datetime import datetime
-        filename = ('backup-%s.json' % datetime.now().strftime('%Y-%m-%d-%H%M%S'))
-        if (not os.path.isdir(self.cb_dir)):
-            os.mkdir(self.cb_dir)
-        fp = os.path.join(self.cb_dir, filename)
-        with open(fp, 'w') as dfo:
-            call_command('dumpdata', 'storageadmin', stdout=dfo)
-        run_command(['/usr/bin/gzip', fp])
-        gz_name = ('%s.gz' % filename)
-        fp = os.path.join(self.cb_dir, gz_name)
-        md5sum = self._md5sum(fp)
-        size = os.stat(fp).st_size
-        cbo = ConfigBackup(filename=gz_name, md5sum=md5sum, size=size)
-        cbo.save()
-        return Response(ConfigBackupSerializer(cbo).data)
+        with self._handle_exception(request):
+            filename = ('backup-%s.json' % datetime.now().strftime('%Y-%m-%d-%H%M%S'))
+            if (not os.path.isdir(self.cb_dir)):
+                os.mkdir(self.cb_dir)
+            fp = os.path.join(self.cb_dir, filename)
+            with open(fp, 'w') as dfo:
+                call_command('dumpdata', 'storageadmin', stdout=dfo)
+            run_command(['/usr/bin/gzip', fp])
+            gz_name = ('%s.gz' % filename)
+            fp = os.path.join(self.cb_dir, gz_name)
+            md5sum = self._md5sum(fp)
+            size = os.stat(fp).st_size
+            cbo = ConfigBackup(filename=gz_name, md5sum=md5sum, size=size)
+            cbo.save()
+            return Response(ConfigBackupSerializer(cbo).data)
 
     @staticmethod
     def _md5sum(fp):
@@ -76,20 +97,37 @@ class ConfigBackupDetailView(ConfigBackupMixin, rfc.GenericView):
 
     @transaction.atomic
     def delete(self, request, backup_id):
-        try:
-            cbo = ConfigBackup.objects.get(id=backup_id)
+        with self._handle_exception(request):
+            cbo = self._validate_input(backup_id)
             fp = os.path.join(self.cb_dir, cbo.filename)
             if (os.path.isfile(fp)):
                 os.remove(fp)
             cbo.delete()
             return Response()
-        except ConfigBackup.DoesNotExist:
-            e_msg = ('Config backup for the id(%s) does not exist' % backup_id)
-            handle_exception(Exception(e_msg), request)
 
     @transaction.atomic
     def post(self, request, backup_id):
-        command = request.data.get('command', 'restore')
-        if (command == 'restore'):
-            logger.debug('restored!!!')
+        with self._handle_exception(request):
+            command = request.data.get('command', 'restore')
+            if (command == 'restore'):
+                cbo = self._validate_input(backup_id)
+                # models that need to be restored.
+                #1. User, Group, Accesskeys?
+                #2. SambaShare
+                #3. NFSExport, NFSExportGroup
+                #4. Service configs
+                #5. Appliances?
+                #6. Scheduled Tasks
+                #7. SFTP, AFP
+                logger.debug('restore starting...')
+                restore_config.async(cbo.id)
+                logger.debug('restore submitted...')
         return Response()
+
+    @staticmethod
+    def _validate_input(backup_id):
+        try:
+            return ConfigBackup.objects.get(id=backup_id)
+        except ConfigBackup.DoesNotExist:
+            e_msg = ('Contif backup for the id(%s) does not exist' % backup_id)
+            handle_exception(Exception(e_msg), request)
