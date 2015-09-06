493fff3c93420a2fad5df32e1914104ae32c9b90
Philip Guyton <philip@yewtreeapps.com>
2015-09-06 19:01:13 +0100

    add nut-server to allowed init_service_op list

--- src/rockstor/system/services.py
+++ src/rockstor/system/services.py
@@ -47,7 +47,7 @@ def init_service_op(service_name, command, throw=True):
     """
     supported_services = ('nfs', 'smb', 'sshd', 'ypbind', 'rpcbind', 'ntpd',
                           'winbind', 'nslcd', 'netatalk', 'snmpd', 'docker',
-                          'smartd')
+                          'smartd', 'nut-server')
     if (service_name not in supported_services):
         raise Exception('unknown service: %s' % service_name)
 
