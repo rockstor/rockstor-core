cf164ff7d1adb264167c8403774c97010b035c0b
Priyanka Ganti <ganti.priya@gmail.com>
2015-11-03 16:04:04 -0800

    add modal in the template file and show modal in the view file

--- src/rockstor/storageadmin/static/storageadmin/js/views/shares.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/shares.js
@@ -1,27 +1,27 @@
 /*
  *
- * @licstart  The following is the entire license notice for the 
+ * @licstart  The following is the entire license notice for the
  * JavaScript code in this page.
- * 
+ *
  * Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
  * This file is part of RockStor.
- * 
+ *
  * RockStor is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation; either version 2 of the License,
  * or (at your option) any later version.
- * 
+ *
  * RockStor is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * @licend  The above is the entire license notice
  * for the JavaScript code in this page.
- * 
+ *
  */
 
 /*
@@ -35,16 +35,16 @@ SharesView = RockstorLayoutView.extend({
 
   initialize: function() {
     this.constructor.__super__.initialize.apply(this, arguments);
-    
+
     this.template = window.JST.share_shares;
     this.shares_table_template = window.JST.share_shares_table;
     this.pagination_template = window.JST.common_pagination;
-    
+
     this.pools = new PoolCollection();
     this.collection = new ShareCollection();
     this.dependencies.push(this.pools);
     this.dependencies.push(this.collection);
-    
+
     this.pools.on("reset", this.renderShares, this);
     this.collection.on("reset", this.renderShares, this);
   },
@@ -55,10 +55,10 @@ SharesView = RockstorLayoutView.extend({
   },
 
   renderShares: function() {
-    if (this.$('[rel=tooltip]')) { 
+    if (this.$('[rel=tooltip]')) {
       this.$('[rel=tooltip]').tooltip('hide');
     }
-    if (!this.pools.fetched || !this.collection.fetched) { 
+    if (!this.pools.fetched || !this.collection.fetched) {
       return false;
     }
     $(this.el).html(this.template({
@@ -73,13 +73,13 @@ SharesView = RockstorLayoutView.extend({
       collection: this.collection
     }));
     this.$("#shares-table").tablesorter({
-       headers: { 
-            // assign the fourth column (we start counting zero) 
-            4: { 
-                // disable it by setting the property sorter to false 
-                sorter: false 
+       headers: {
+            // assign the fourth column (we start counting zero)
+            4: {
+                // disable it by setting the property sorter to false
+                sorter: false
             }
-         }    
+         }
     });
     this.$('[rel=tooltip]').tooltip({placement: 'bottom'});
   },
@@ -88,8 +88,12 @@ SharesView = RockstorLayoutView.extend({
     var _this = this;
     var button = $(event.currentTarget);
     if (buttonDisabled(button)) return false;
-    name = button.attr('data-name');
-    if(confirm("Delete share:  " + name + " ...Are you sure?")){
+    shareName = button.attr('data-name');
+    // set share name in confirm dialog
+    _this.$('#pass-share-name').html(name);
+    //show the dialog
+    _this.$('#delete-share-modal').modal();
+    /* if(confirm("Delete share:  " + name + " ...Are you sure?")){
       $.ajax({
         url: "/api/shares/"+name,
         type: "DELETE",
@@ -102,11 +106,10 @@ SharesView = RockstorLayoutView.extend({
           enableButton(button);
         }
       });
-    }
+    } */
   }
 
 });
 
 // Add pagination
 Cocktail.mixin(SharesView, PaginationMixin);
-
