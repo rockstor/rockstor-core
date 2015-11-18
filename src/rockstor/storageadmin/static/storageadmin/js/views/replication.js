212bbe8271551bcb0782f3315f0e236d6d57a0e8
Priyanka Ganti <ganti.priya@gmail.com>
2015-11-17 18:03:16 -0800

    remove slider related code

--- src/rockstor/storageadmin/static/storageadmin/js/views/replication.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/replication.js
@@ -28,8 +28,6 @@ ReplicationView = RockstorLayoutView.extend({
     events: {
 	'click a[data-action=delete]': 'deleteTask',
 	'switchChange.bootstrapSwitch': 'switchStatus',
-	'click .slider-start': 'enable',
-	'click .slider-stop': 'disable'
     },
 
     initialize: function() {
@@ -58,84 +56,71 @@ ReplicationView = RockstorLayoutView.extend({
     },
 
     render: function() {
-	this.fetch(this.renderReplicas, this);
-	return this;
+    	this.fetch(this.renderReplicas, this);
+    	return this;
     },
 
     renderReplicas: function() {
+    	var _this = this;
+    	this.otherAppliances =  this.appliances.filter(function(appliance) {
+    	    return appliance.get('current_appliance') == false;
+    	});
+    	this.freeShares = this.shares.reject(function(share) {
+    	    return !_.isUndefined(_this.collection.find(function(replica) {
+    		return replica.get('share') == share.get('name');
+    	    })) ;
+    	});
+    	// remove existing tooltips
+    	if (this.$('[rel=tooltip]')) {
+    	    this.$('[rel=tooltip]').tooltip('hide');
+    	}
+    	var shares = this.collection.map(function(replica) {
+    	    return replica.get('share');
+    	});
+    	_.each(shares, function(share) {
+    	    _this.replicaShareMap[share] = _this.collection.filter(function(replica) {
+    		return replica.get('share') == share;
+    	    });
+    	});
+    	this.collection.each(function(replica, index) {
+    	    var tmp = _this.replicaTrails.filter(function(replicaTrail) {
+    		return replicaTrail.get('replica') == replica.id;
+    	    });
+    	    _this.replicaTrailMap[replica.id] = _.sortBy(tmp, function(replicaTrail) {
+    		return moment(replicaTrail.get('snapshot_created')).valueOf();
+    	    }).reverse();
+    	});
+    	$(this.el).html(this.template({
+    	    replicationService: this.replicationService,
+    	    replicas: this.collection,
+    	    replicaShareMap: this.replicaShareMap,
+    	    replicaTrailMap: this.replicaTrailMap,
+    	    otherAppliances: this.otherAppliances,
+    	    freeShares: this.freeShares
+    	}));
 
-	var _this = this;
-	this.otherAppliances =  this.appliances.filter(function(appliance) {
-	    return appliance.get('current_appliance') == false;
-	});
-	this.freeShares = this.shares.reject(function(share) {
-	    return !_.isUndefined(_this.collection.find(function(replica) {
-		return replica.get('share') == share.get('name');
-	    })) ;
-	});
-	// remove existing tooltips
-	if (this.$('[rel=tooltip]')) {
-	    this.$('[rel=tooltip]').tooltip('hide');
-	}
-	var shares = this.collection.map(function(replica) {
-	    return replica.get('share');
-	});
-	_.each(shares, function(share) {
-	    _this.replicaShareMap[share] = _this.collection.filter(function(replica) {
-		return replica.get('share') == share;
-	    });
-	});
-	this.collection.each(function(replica, index) {
-	    var tmp = _this.replicaTrails.filter(function(replicaTrail) {
-		return replicaTrail.get('replica') == replica.id;
-	    });
-	    _this.replicaTrailMap[replica.id] = _.sortBy(tmp, function(replicaTrail) {
-		return moment(replicaTrail.get('snapshot_created')).valueOf();
-	    }).reverse();
-	});
-	$(this.el).html(this.template({
-	    replicationService: this.replicationService,
-	    replicas: this.collection,
-	    replicaShareMap: this.replicaShareMap,
-	    replicaTrailMap: this.replicaTrailMap,
-	    otherAppliances: this.otherAppliances,
-	    freeShares: this.freeShares
+    	// Display Service Warning
+    	if (!this.replicationService.get('status')) {
+    	    this.$('#replication-warning').show();
+    	} else {
+    	    this.$('#replication-warning').hide();
+    	}
 
-	}));
-
-	// Display Service Warning
-	if (!this.replicationService.get('status')) {
-	    this.$('#replication-warning').show();
-	} else {
-	    this.$('#replication-warning').hide();
-	}
-
- //code optimization
-  this.$("[type='checkbox']").bootstrapSwitch();
-  if (typeof this.current_status == 'undefined') {
-    this.current_status = this.replicationService.get('status');
-  }
-  this.$('input[name="replica-service-checkbox"]').bootstrapSwitch('state', this.current_status, true);
-  this.$("[type='checkbox']").bootstrapSwitch('onColor','success'); //left side text color
-  this.$("[type='checkbox']").bootstrapSwitch('offColor','danger'); //right side text color
+     //initalize Bootstrap Switch
+      this.$("[type='checkbox']").bootstrapSwitch();
+      if (typeof this.current_status == 'undefined') {
+        this.current_status = this.replicationService.get('status');
+      }
+      this.$('input[name="replica-service-checkbox"]').bootstrapSwitch('state', this.current_status, true);
+      this.$("[type='checkbox']").bootstrapSwitch('onColor','success'); //left side text color
+      this.$("[type='checkbox']").bootstrapSwitch('offColor','danger'); //right side text color
 
 
-	this.$('[rel=tooltip]').tooltip({ placement: 'bottom'});
-	this.$(".ph-pagination").html(this.paginationTemplate({
-	    collection: this.collection
-	}));
-	this.$('#replicas-table').tablesorter();
-	this.$('input.replication-status').simpleSlider({
-	    "theme": "volume",
-	    allowedValues: [0,1],
-	    snap: true
-	});
-	this.$('input.replication-status').each(function(i, el) {
-	    var slider = $(el).data('slider-object');
-	    // disable track and dragger events to disable slider
-	    slider.trackEvent = function(e) {};
-	    slider.dragger.unbind('mousedown');
-	});
+    	this.$('[rel=tooltip]').tooltip({ placement: 'bottom'});
+    	this.$(".ph-pagination").html(this.paginationTemplate({
+    	    collection: this.collection
+    	}));
+    	this.$('#replicas-table').tablesorter();
     },
 
     switchStatus: function(event,state){
@@ -205,102 +190,94 @@ ReplicationView = RockstorLayoutView.extend({
     },
 
     deleteTask: function(event) {
-	var _this = this;
-	if (event) { event.preventDefault(); }
-	var button = $(event.currentTarget);
-	if (buttonDisabled(button)) return false;
-	var rTaskId = $(event.currentTarget).attr("data-task-id");
-	var rTaskName = $(event.currentTarget).attr("data-task-name");
-	if(confirm("Delete Replication task:  " + rTaskName + ". Are you sure?")){
-	    $.ajax({
-		url: '/api/sm/replicas/' + rTaskId,
-		type: "DELETE",
-		dataType: "json",
-		success: function() {
-		    enableButton(button);
-		    _this.collection.fetch({
-			success: function() {
-			    _this.renderReplicas();
-			}
-		    });
-		},
-		error: function(xhr, status, error) {
-		    enableButton(button);
-		}
-	    });
-	}
-    },
-
-    setSliderVal: function(serviceName, val) {
-	this.$('input[data-replica-id="' + serviceName + '"]').simpleSlider('setValue',val);
-    },
-
-    getSliderVal: function(serviceName) {
-	return this.$('input[data-replica-id="' + serviceName + '"]').data('slider-object').value;
+    	var _this = this;
+    	if (event) { event.preventDefault(); }
+    	var button = $(event.currentTarget);
+    	if (buttonDisabled(button)) return false;
+    	var rTaskId = $(event.currentTarget).attr("data-task-id");
+    	var rTaskName = $(event.currentTarget).attr("data-task-name");
+    	if(confirm("Delete Replication task:  " + rTaskName + ". Are you sure?")){
+    	    $.ajax({
+    		url: '/api/sm/replicas/' + rTaskId,
+    		type: "DELETE",
+    		dataType: "json",
+    		success: function() {
+    		    enableButton(button);
+    		    _this.collection.fetch({
+    			success: function() {
+    			    _this.renderReplicas();
+    			}
+    		    });
+    		},
+    		error: function(xhr, status, error) {
+    		    enableButton(button);
+    		}
+    	    });
+    	}
     },
 
     startService: function(event){
-	var _this = this;
-	var serviceName = this.serviceName;
-	this.setStatusLoading(serviceName, true);
-	$.ajax({
-	    url: "/api/sm/services/replication/start",
-	    type: "POST",
-	    dataType: "json",
-	    success: function(data, status, xhr) {
-		_this.setStatusLoading(serviceName, false);
-    _this.current_status = true;
-		//hide replication service warning
-		_this.$('#replication-warning').hide();
-	    },
-	    error: function(xhr, status, error) {
-		_this.setStatusError(serviceName, xhr);
-	    }
-	});
+    	var _this = this;
+    	var serviceName = this.serviceName;
+    	this.setStatusLoading(serviceName, true);
+    	$.ajax({
+    	    url: "/api/sm/services/replication/start",
+    	    type: "POST",
+    	    dataType: "json",
+    	    success: function(data, status, xhr) {
+    		_this.setStatusLoading(serviceName, false);
+        _this.current_status = true;
+    		//hide replication service warning
+    		_this.$('#replication-warning').hide();
+    	    },
+    	    error: function(xhr, status, error) {
+    		_this.setStatusError(serviceName, xhr);
+    	    }
+    	});
     },
 
     stopService: function(event) {
-  	var _this = this;
-	var serviceName = this.serviceName;
-  	this.setStatusLoading(serviceName, true);
-  	$.ajax({
-  	    url: "/api/sm/services/replication/stop",
-  	    type: "POST",
-  	    dataType: "json",
-  	    success: function(data, status, xhr) {
-  		_this.setStatusLoading(serviceName, false);
-      _this.current_status = false;
-		//display replication service warning
-		_this.$('#replication-warning').show();
-  	    },
-  	    error: function(xhr, status, error) {
-  		_this.setStatusError(serviceName, xhr);
-  	    }
-  	});
+      var _this = this;
+    	var serviceName = this.serviceName;
+      	this.setStatusLoading(serviceName, true);
+      	$.ajax({
+      	    url: "/api/sm/services/replication/stop",
+      	    type: "POST",
+      	    dataType: "json",
+      	    success: function(data, status, xhr) {
+      		_this.setStatusLoading(serviceName, false);
+          _this.current_status = false;
+    		//display replication service warning
+    		_this.$('#replication-warning').show();
+      	    },
+      	    error: function(xhr, status, error) {
+      		_this.setStatusError(serviceName, xhr);
+      	    }
+      	});
     },
 
     setStatusLoading: function(serviceName, show) {
-	var statusEl = this.$('div.command-status[data-service-name="'+serviceName+'"]');
-	if (show) {
-	    statusEl.html('<img src="/static/storageadmin/img/ajax-loader.gif"></img>');
-	} else {
-	    statusEl.empty();
-	}
+    	var statusEl = this.$('div.command-status[data-service-name="'+serviceName+'"]');
+    	if (show) {
+    	    statusEl.html('<img src="/static/storageadmin/img/ajax-loader.gif"></img>');
+    	} else {
+    	    statusEl.empty();
+    	}
     },
 
     setStatusError: function(serviceName, xhr) {
-	var statusEl = this.$('div.command-status[data-service-name="' + serviceName + '"]');
-	var msg = parseXhrError(xhr);
-	// remove any existing error popups
-	$('body').find('#' + serviceName + 'err-popup').remove();
-	// add icon and popup
-	statusEl.empty();
-	var icon = $('<i>').addClass('icon-exclamation-sign').attr('rel', '#' + serviceName + '-err-popup');
-	statusEl.append(icon);
-	var errPopup = this.$('#' + serviceName + '-err-popup');
-	var errPopupContent = this.$('#' + serviceName + '-err-popup > div');
-	errPopupContent.html(msg);
-	statusEl.click(function(){ errPopup.overlay().load(); });
+    	var statusEl = this.$('div.command-status[data-service-name="' + serviceName + '"]');
+    	var msg = parseXhrError(xhr);
+    	// remove any existing error popups
+    	$('body').find('#' + serviceName + 'err-popup').remove();
+    	// add icon and popup
+    	statusEl.empty();
+    	var icon = $('<i>').addClass('icon-exclamation-sign').attr('rel', '#' + serviceName + '-err-popup');
+    	statusEl.append(icon);
+    	var errPopup = this.$('#' + serviceName + '-err-popup');
+    	var errPopupContent = this.$('#' + serviceName + '-err-popup > div');
+    	errPopupContent.html(msg);
+    	statusEl.click(function(){ errPopup.overlay().load(); });
     }
 
 });
