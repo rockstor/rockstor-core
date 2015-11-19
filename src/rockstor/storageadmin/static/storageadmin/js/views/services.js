a7c6fac760ae569128253e119ef2ac7b3cfb9678
Priyanka Ganti <ganti.priya@gmail.com>
2015-11-19 13:51:15 -0800

    change slider to bootstrap switch in services

--- src/rockstor/storageadmin/static/storageadmin/js/views/services.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/services.js
@@ -30,9 +30,8 @@
 ServicesView = Backbone.View.extend({
 
     events: {
-	'click .slider-stop': "stopService",
-	'click .slider-start': "startService",
-	'click .configure': "configureService"
+	'click .configure': "configureService",
+  'switchChange.bootstrapSwitch': 'switchStatus',
     },
 
     initialize: function() {
@@ -94,34 +93,30 @@ ServicesView = Backbone.View.extend({
 	this.$(".ph-pagination").html(this.paginationTemplate({
 	    collection: this.collection
 	}));
-	this.$('input.service-status').simpleSlider({
-	    "theme": "volume",
-	    allowedValues: [0,1],
-	    snap: true
-	});
 
-	this.$('input.service-status').each(function(i, el) {
-	    var slider = $(el).data('slider-object');
-	    // disable track and dragger events to disable slider
-	    slider.trackEvent = function(e) {};
-	    slider.dragger.unbind('mousedown');
-	});
+  //Initialize bootstrap switch
+  this.$("[type='checkbox']").bootstrapSwitch();
+  this.$("[type='checkbox']").bootstrapSwitch('onColor','success'); //left side text color
+  this.$("[type='checkbox']").bootstrapSwitch('offColor','danger'); //right side text color
 
     },
 
-    startService: function(event) {
+    switchStatus: function(event,state){
+      var serviceName = $(event.target).attr('data-service-name');
+        if (state){
+          this.startService(serviceName);
+        }else {
+          this.stopService(serviceName);
+        }
+    },
+
+    startService: function(serviceName) {
 	var _this = this;
-	var serviceName = $(event.currentTarget).data('service-name');
-	// if already started, return
-	if (this.getSliderVal(serviceName).toString() == "1") return;
-	this.setStatusLoading(serviceName, true);
 	$.ajax({
 	    url: "/api/sm/services/" + serviceName + "/start",
 	    type: "POST",
 	    dataType: "json",
 	    success: function(data, status, xhr) {
-		_this.highlightStartEl(serviceName, true);
-		_this.setSliderVal(serviceName, 1);
 		_this.setStatusLoading(serviceName, false);
 	    },
 	    error: function(xhr, status, error) {
@@ -130,20 +125,13 @@ ServicesView = Backbone.View.extend({
 	});
     },
 
-    stopService: function(event) {
+    stopService: function(serviceName) {
 	var _this = this;
-	var serviceName = $(event.currentTarget).data('service-name');
-	if (serviceName == 'service-monitor') return;
-	// if already stopped, return
-	if (this.getSliderVal(serviceName).toString() == "0") return;
-	this.setStatusLoading(serviceName, true);
 	$.ajax({
 	    url: "/api/sm/services/" + serviceName + "/stop",
 	    type: "POST",
 	    dataType: "json",
 	    success: function(data, status, xhr) {
-		_this.highlightStartEl(serviceName, false);
-		_this.setSliderVal(serviceName, 0);
 		_this.setStatusLoading(serviceName, false);
 	    },
 	    error: function(xhr, status, error) {
@@ -181,23 +169,6 @@ ServicesView = Backbone.View.extend({
 	var errPopupContent = this.$('#' + serviceName + '-err-popup > div');
 	errPopupContent.html(msg);
 	statusEl.click(function(){ errPopup.overlay().load(); });
-    },
-
-    highlightStartEl: function(serviceName, on) {
-	var startEl = this.$('div.slider-start[data-service-name="' + serviceName + '"]');
-	if (on) {
-	    startEl.addClass('on');
-	} else {
-	    startEl.removeClass('on');
-	}
-    },
-
-    setSliderVal: function(serviceName, val) {
-	this.$('input[data-service-name="' + serviceName + '"]').simpleSlider('setValue',val);
-    },
-
-    getSliderVal: function(serviceName) {
-	return this.$('input[data-service-name="' + serviceName + '"]').data('slider-object').value;
     },
 
     cleanup: function() {
