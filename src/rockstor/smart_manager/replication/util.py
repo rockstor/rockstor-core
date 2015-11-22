1c77d10f7ceb42b69df7c8c27099059db0952ea5
Suman Chakravartula <suman@rockstor.com>
2015-11-22 10:47:48 -0800

    set the url in api_wrapper to account for mgmt interface. #886

--- src/rockstor/smart_manager/replication/util.py
+++ src/rockstor/smart_manager/replication/util.py
@@ -17,13 +17,11 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import re
+import sys
 from django.utils.timezone import utc
 from storageadmin.exceptions import RockStorAPIException
 from storageadmin.models import Appliance
-import sys
-
-BASE_URL = 'https://localhost/api/'
-
+from cli import APIWrapper
 import logging
 
 
@@ -51,11 +49,11 @@ class ReplicationMixin(object):
             self.raw = APIWrapper(client_id=a.client_id,
                                   client_secret=a.client_secret,
                                   url=url)
-        return self.raw.api_call(url='%s/api/shares/%s' % (url, sname))
+        return self.raw.api_call(url='shares/%s' % sname)
 
     def update_replica_status(self, rtid, data):
         try:
-            url = ('%ssm/replicas/trail/%d' % (BASE_URL, rtid))
+            url = ('sm/replicas/trail/%d' % rtid)
             return self.law.api_call(url, data=data, calltype='put')
         except Exception, e:
             msg = ('Exception while updating replica(%s) status to %s: %s' %
@@ -64,7 +62,7 @@ class ReplicationMixin(object):
 
     def disable_replica(self, rid):
         try:
-            url = ('%ssm/replicas/%d' % (BASE_URL, rid))
+            url = ('sm/replicas/%d' % rid)
             headers = {'content-type': 'application/json', }
             return self.law.api_call(url, data={'enabled': False, }, calltype='put',
                                      save_error=False, headers=headers)
@@ -74,18 +72,18 @@ class ReplicationMixin(object):
             raise Exception(msg)
 
     def create_replica_trail(self, rid, snap_name):
-        url = ('%ssm/replicas/trail/replica/%d' % (BASE_URL, rid))
+        url = ('sm/replicas/trail/replica/%d' % rid)
         return self.law.api_call(url, data={'snap_name': snap_name, },
                                  calltype='post', save_error=False)
 
     def rshare_id(self, sname):
-        url = ('%ssm/replicas/rshare/%s' % (BASE_URL, sname))
+        url = ('sm/replicas/rshare/%s' % sname)
         rshare = self.law.api_call(url, save_error=False)
         return rshare['id']
 
     def create_rshare(self, data):
         try:
-            url = ('%ssm/replicas/rshare' % BASE_URL)
+            url = 'sm/replicas/rshare'
             rshare = self.law.api_call(url, data=data, calltype='post', save_error=False)
             return rshare['id']
         except RockStorAPIException, e:
@@ -94,12 +92,12 @@ class ReplicationMixin(object):
             raise e
 
     def create_receive_trail(self, rid, data):
-        url = ('%ssm/replicas/rtrail/rshare/%d' % (BASE_URL, rid))
+        url = ('sm/replicas/rtrail/rshare/%d' % rid)
         rt = self.law.api_call(url, data=data, calltype='post', save_error=False)
         return rt['id']
 
     def update_receive_trail(self, rtid, data):
-        url = ('%ssm/replicas/rtrail/%d' % (BASE_URL, rtid))
+        url = ('sm/replicas/rtrail/%d' % rtid)
         try:
             return self.law.api_call(url, data=data, calltype='put', save_error=False)
         except Exception, e:
@@ -116,16 +114,16 @@ class ReplicationMixin(object):
             raise Exception(msg)
 
     def prune_receive_trail(self, rid):
-        url = ('%ssm/replicas/rtrail/rshare/%d' % (BASE_URL, rid))
+        url = ('sm/replicas/rtrail/rshare/%d' % rid)
         return self.prune_trail(url)
 
     def prune_replica_trail(self, rid):
-        url = ('%ssm/replicas/trail/replica/%d' % (BASE_URL, rid))
+        url = ('sm/replicas/trail/replica/%d' % rid)
         return self.prune_trail(url)
 
     def create_snapshot(self, sname, snap_name, logger, snap_type='replication'):
         try:
-            url = ('%sshares/%s/snapshots/%s' % (BASE_URL, sname, snap_name))
+            url = ('shares/%s/snapshots/%s' % (sname, snap_name))
             return self.law.api_call(url, data={'snap_type': snap_type, }, calltype='post',
                                      save_error=False)
         except RockStorAPIException, e:
@@ -136,7 +134,7 @@ class ReplicationMixin(object):
 
     def delete_snapshot(self, sname, snap_name, logger):
         try:
-            url = ('%sshares/%s/snapshots/%s' % (BASE_URL, sname, snap_name))
+            url = ('shares/%s/snapshots/%s' % (sname, snap_name))
             return self.law.api_call(url, calltype='delete', save_error=False)
         except RockStorAPIException, e:
             if (e.detail == 'Snapshot(%s) does not exist.' % snap_name):
@@ -145,7 +143,7 @@ class ReplicationMixin(object):
 
     def create_share(self, sname, pool, logger):
         try:
-            url = ('%sshares' % BASE_URL)
+            url = 'shares'
             data = {'pool': pool,
                     'replica': True,
                     'sname': sname, }
