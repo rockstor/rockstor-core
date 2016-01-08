c453ea171ccd971984840b8e46b9f314171edb8d
Suman Chakravartula <suman@rockstor.com>
2016-01-07 16:30:59 -0800

    add container-env api. #858

--- src/rockstor/storageadmin/urls/rockons.py
+++ src/rockstor/storageadmin/urls/rockons.py
@@ -18,7 +18,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from django.conf.urls import patterns, url
 from storageadmin.views import (RockOnView, RockOnIdView, RockOnVolumeView,
-                                RockOnPortView, RockOnCustomConfigView)
+                                RockOnPortView, RockOnCustomConfigView,
+                                RockOnEnvironmentView)
 
 urlpatterns = patterns(
     '',
@@ -26,6 +27,7 @@ urlpatterns = patterns(
     url(r'^/volumes/(?P<rid>\d+)$', RockOnVolumeView.as_view(), ),
     url(r'^/ports/(?P<rid>\d+)$', RockOnPortView.as_view(), ),
     url(r'^/customconfig/(?P<rid>\d+)$', RockOnCustomConfigView.as_view(), ),
+    url(r'^/environment/(?P<rid>\d+)$', RockOnEnvironmentView.as_view(), ),
     url(r'^/(?P<command>update)$', RockOnView.as_view(), ),
     url(r'^/(?P<rid>\d+)$', RockOnIdView.as_view(), ),
     url(r'^/(?P<rid>\d+)/(?P<command>install|uninstall|update|start|stop|state_update|status_update)$',
