1d7676fd51d450f1902149df0a3403fb2273371b
Priyanka Ganti <ganti.priya@gmail.com>
2016-02-09 18:13:14 -0800

    remove commented out code

--- src/rockstor/storageadmin/static/storageadmin/js/views/snapshots_table.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/snapshots_table.js
@@ -25,7 +25,6 @@
  */
 
 SnapshotsTableModule = SnapshotsCommonView.extend({
-//SnapshotsTableModule  = RockstorModuleView.extend({
 	events: {
 		"click #js-snapshot-add": "add",
 		"click #js-snapshot-cancel": "cancel",
@@ -184,45 +183,6 @@ SnapshotsTableModule = SnapshotsCommonView.extend({
 		app_router.navigate(url, {trigger: true});
 
 	},
-
-	/* selectSnapshot: function(event) {
-		var _this = this;
-		name = $(event.currentTarget).attr('data-name');
-		var checked = $(event.currentTarget).prop('checked');
-		this.selectSnapshotWithName(name, checked);
-		this.toggleDeleteButton();
-	},
-
-	selectSnapshotWithName: function(name, checked) {
-		if (checked) {
-			if (!RockstorUtil.listContains(this.selectedSnapshots, 'name', name)) {
-				RockstorUtil.addToList(
-						this.selectedSnapshots, this.collection, 'name', name);
-			}
-		} else {
-			if (RockstorUtil.listContains(this.selectedSnapshots, 'name', name)) {
-				RockstorUtil.removeFromList(this.selectedSnapshots, 'name', name);
-			}
-		}
-	},
-	
-	toggleDeleteButton: function(){
-    	if(this.selectedSnapshots.length == 0){
-    		$("#js-snapshot-delete-multiple").css("visibility", "hidden");
-    	}else{
-    		$("#js-snapshot-delete-multiple").css("visibility", "visible");
-    	}
-    },
-
-	selectAllSnapshots: function(event) {
-		var _this = this;
-		var checked = $(event.currentTarget).prop('checked');
-		this.$('.js-snapshot-select').prop('checked', checked)
-		this.$('.js-snapshot-select').each(function() {
-			_this.selectSnapshotWithName($(this).attr('data-name'), checked);
-		});
-		this.toggleDeleteButton();
-	}, */
 
 	deleteMultipleSnapshots: function(event) {
 		var _this = this;
