d57ad2baea3ebad7fc3913089dce16444fc43260
Philip Guyton <philip@yewtreeapps.com>
2016-03-08 17:27:10 +0000

    add specialized parser for in progress smart self test log entry #1207
    Detect when we have failed to retrieve all 6 columns and if the cause
    is down to in-progress test log entry then do additional parsing to
    re-create our expected 6 columns. Cause is down to splitting on two
    or more spaces and in this instance we have only one space between
    columns. Special case so treat as such.

--- src/rockstor/system/smart.py
+++ src/rockstor/system/smart.py
@@ -254,11 +254,31 @@ def test_logs(device, test_mode=TESTMODE):
                              o[i + 1]) is not None):
                     for j in range(i + 2, len(o)):
                         if (re.match('# ', o[j]) is not None):
+                            # slit the line into fields using 2 or more spaces
                             fields = re.split(r'\s\s+', o[j].strip()[2:])
                             logger.debug('fields extracted are as follows %s', fields)
-                            logger.debug('problematic field 3 = %s', fields[3])
+                            # Some Seagate drives add an ongoing test progress
+                            # report to the top of this log but there is then
+                            # only one space delimiter and we loose a column.
+                            if len(fields) == 5:  # it's normally 6 fields
+                                # we are missing a column (fast check)
+                                if re.match('Self-test routine in progress',
+                                            fields[2]):
+                                    # An ongoing self-test entry is to blame.
+                                    status_fields = fields[2].split()
+                                    # Move our last two line fields along one.
+                                    fields.insert(5, fields[4])
+                                    fields[4] = fields[3]
+                                    # Move end of status field percentage to
+                                    # freshly freed up column in line list.
+                                    fields[3] = status_fields[-1]
+                                    # Remove our % remaining in status field.
+                                    fields[2] = ' '.join(status_fields[:-1])
+                            # Remove the % char from this columns value
+                            # and change % Remaining to % Completed.
                             fields[3] = 100 - int(fields[3][:-1])
                             test_d[fields[0]] = fields[1:]
+                            logger.debug('post processed fields = ------- %s', fields)
                         else:
                             log_l.append(o[j])
     return (test_d, log_l)
