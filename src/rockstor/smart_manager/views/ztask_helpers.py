851d383e446ff19036f9b23bde61bb58492eff0a
Suman Chakravartula <suman@rockstor.com>
2016-04-17 21:17:00 -0700

    async task to restart rockstor/nginx. #560

---
+++ src/rockstor/smart_manager/views/ztask_helpers.py
@@ -0,0 +1,9 @@
+import tempfile
+import shutil
+from django.conf import settings
+from django_ztask.decorators import task
+from system import services
+
+@task()
+def restart_rockstor(ip, port):
+    services.update_nginx(ip, port)
