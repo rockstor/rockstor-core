d78a1fe66624f2e3a6576d3df1dea02b96da9861
Mirko Arena <mirko.arena@gmail.com>
2016-05-30 13:10:34 +0200

    #1290 - Db Migration adding pincard model with onetoone rel over django users part 2

---
+++ src/rockstor/storageadmin/models/pincard.py
@@ -0,0 +1,31 @@
+"""
+Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+This file is part of RockStor.
+
+RockStor is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published
+by the Free Software Foundation; either version 2 of the License,
+or (at your option) any later version.
+
+RockStor is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from django.db import models
+from django.contrib.auth.models import User as DjangoUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+class Pincard(models.Model):
+    user = models.OneToOneField(DjangoUser, on_delete=models.CASCADE)
+    pin_number = models.IntegerField(validators=[MinValueValidator(1),
+                                                 MaxValueValidator(24)])
+    pin_code = models.CharField(max_length=32,null=False)
+
+    class Meta:
+        unique_together = ('user','pin_number')
+        app_label = 'storageadmin'
