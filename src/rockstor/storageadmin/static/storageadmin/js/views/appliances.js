262986a22f15d0fa872cdf7a74e3d929b163d3a1
Priyanka Ganti <ganti.priya@gmail.com>
2016-06-01 21:12:13 -0700

    add data tables functions to appliances table

--- src/rockstor/storageadmin/static/storageadmin/js/views/appliances.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/appliances.js
@@ -47,6 +47,7 @@ AppliancesView = RockstorLayoutView.extend({
 
   renderApplianceList: function() {
     $(this.el).html(this.template({collection: this.collection, appliances: this.collection.toJSON()}));
+    this.renderDataTables();
   },
 
   newAppliance: function() {
