a25ae2904e2172953e3367d71add63a285819956
Steven Franzen <sfranzen85@gmail.com>
2016-08-20 00:11:51 +0200

    Fix: argument to docker run should still be -d

--- src/rockstor/storageadmin/views/rockon_helpers.py
+++ src/rockstor/storageadmin/views/rockon_helpers.py
@@ -40,7 +40,7 @@ from rockon_discourse import (discourse_install, discourse_uninstall,
 DOCKER = '/usr/bin/docker'
 ROCKON_URL = 'https://localhost/api/rockons'
 DCMD = [DOCKER, 'run', ]
-DCMD2 = list(DCMD) + ['daemon', '--restart=unless-stopped', ]
+DCMD2 = list(DCMD) + ['-d', '--restart=unless-stopped', ]
 
 import logging
 logger = logging.getLogger(__name__)
