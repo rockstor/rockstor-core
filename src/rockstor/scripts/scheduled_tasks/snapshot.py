26a19c6d6fc5d0d64948ce8c5729f8392be9be4c
Tomasz Kusmierz <tom.kusmierz@gmail.com>
2016-09-24 23:00:09 +0100

    Cleaning up some mess with readonly and writable use. Decided to go for what framework seems to use everywhere else.

--- src/rockstor/scripts/scheduled_tasks/snapshot.py
+++ src/rockstor/scripts/scheduled_tasks/snapshot.py
@@ -102,7 +102,8 @@ def main():
             #runaway snapshot creation beyond max_count+1.
             if(delete(aw, share, stype, prefix, max_count)):
                 data = {'snap_type': stype,
-                        'uvisible': meta['visible'], }
+                        'uvisible': meta['visible'],
+                        'writable': meta['writable'], }
                 headers = {'content-type': 'application/json'}
                 aw.api_call(url, data=data, calltype='post', headers=headers, save_error=False)
                 logger.debug('created snapshot at %s' % url)
