285aa5fbc80ff5c4fb4698b1f9ef63004ad539eb
Steven Franzen <sfranzen85@gmail.com>
2016-10-05 19:14:23 +0200

    Add pool usage bound algorithm
    Partial fix of #1454.
    
    This algorithm should give an accurate report of the maximum available
    space in a storage pool, regardless of raid configuration and disk
    sizes. It will replace the current figure reported by btrfs.pool_usage.

--- src/rockstor/storageadmin/models/pool.py
+++ src/rockstor/storageadmin/models/pool.py
@@ -20,6 +20,10 @@ from django.db import models
 from django.conf import settings
 from smart_manager.models import PoolUsage
 from fs.btrfs import pool_usage
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 class Pool(models.Model):
     """Name of the pool"""
@@ -50,6 +54,84 @@ class Pool(models.Model):
     @property
     def reclaimable(self, *args, **kwargs):
         return 0
+
+    @property
+    def usage_bound(self, disk_sizes=[], num_devices=0):
+        """Return the total amount of storage possible within this pool's set
+        of disks, in bytes.
+
+        Algorithm adapted from Hugo Mills' implementation at:
+        http://carfax.org.uk/btrfs-usage/js/btrfs-usage.js
+        """
+        if not disk_sizes:
+            disk_sizes = [int(size) * 1024 for size in self.disk_set
+                          .values_list('size', flat=True)
+                          .order_by('-size')]
+            num_devices = len(disk_sizes)
+
+        for index, size in enumerate(disk_sizes):
+            logger.info('Device %s size: %s' % (index, size))
+
+        # Determine RAID parameters
+        data_ratio = 1
+        stripes = 1
+        parity = 0
+        chunks = num_devices
+
+        if self.raid == 'single':
+            chunks = 1
+        elif self.raid == 'raid0':
+            stripes = 2
+        elif self.raid == 'raid1':
+            data_ratio = 2
+            chunks = 2
+        elif self.raid == 'raid10':
+            data_ratio = 2
+            stripes = min(2, int(num_devices / 2))
+        elif self.raid == 'raid5':
+            parity = 1
+        elif self.raid == 'raid6':
+            parity = 2
+
+        # Number of chunks to write at a time: as many as possible within the
+        # number of stripes
+        logger.info('Allocate %s chunks at a time' % chunks)
+        # Check for feasibility at the lower end
+        if num_devices < data_ratio * (stripes + parity):
+            return 0
+
+        # Compute the trivial bound
+        bound = int(sum(disk_sizes) / chunks)
+        logger.info('Trivial bound is %s / %s = %s'
+                    % (sum(disk_sizes), chunks, bound))
+
+        # For each partition point q, compute B_q (the test predicate) and
+        # modify the trivial bound if it passes.
+        bounding_q = -1
+        for q in range(chunks - 1):
+            slice = sum(disk_sizes[q + 1:])
+            b = int(slice / (chunks - q - 1))
+            logger.info('q = %s, bound is %s / %s = %s'
+                        % (q, slice, chunks-q-1, b))
+            if disk_sizes[q] >= b and b < bound:
+                bound = b
+                bounding_q = q
+
+        # The bound is the number of allocations we can make in total. If we
+        # have no bounding_q, then we have hit the trivial bound, and exhausted
+        # all space, so we can return immediately.
+        if bounding_q == -1:
+            return bound * ((chunks / data_ratio) - parity)
+
+        # If we have a bounding_q, then all the devices past q are full, and
+        # we can remove them. The devices up to q have been used in every one
+        # of the allocations, so we can just reduce them by bound.
+        disk_sizes = [size - bound for index, size in enumerate(disk_sizes)
+                      if index <= bounding_q]
+
+        new_bound = self.usage_bound(disk_sizes, bounding_q + 1)
+
+        return bound * ((chunks / data_ratio) - parity) + new_bound
 
     class Meta:
         app_label = 'storageadmin'
