58257825db23eb5766754c400d732a4a05f2e254
Philip Guyton <philip@yewtreeapps.com>
2016-11-26 13:23:24 +0000

    disable SMART on bcache virtual devices #1494
    By using 'bcache' and not 'bcache- ' we catch both the
    temp_name (incomplete udev rules) and the udev assigned
    by-id name.

--- src/rockstor/storageadmin/views/disk.py
+++ src/rockstor/storageadmin/views/disk.py
@@ -266,9 +266,9 @@ class DiskMixin(object):
                 # Also note that with no serial number some device types will
                 # not have a by-id type name expected by the smart subsystem.
                 # This has only been observed in no serial virtio devices.
-                if (re.match('fake-serial-', do.serial) is not None) or \
-                        (re.match('virtio-|md-|mmc-|nvme-|dm-name-luks-',
-                                  do.name) is not None):
+                if (re.match('fake-serial-', do.serial) is not None) or (
+                    re.match('virtio-|md-|mmc-|nvme-|dm-name-luks-|bcache',
+                             do.name) is not None):
                     # Virtio disks (named virtio-*), md devices (named md-*),
                     # and an sdcard reader that provides devs named mmc-* have
                     # no smart capability so avoid cluttering logs with
