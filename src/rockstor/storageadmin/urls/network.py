8db8298d41993070ebe7235b676a59b1b6e9e33b
Suman Chakravartula <suman@rockstor.com>
2017-01-15 15:19:35 -0800

    flake8 compliance: storageadmin/urls

--- src/rockstor/storageadmin/urls/network.py
+++ src/rockstor/storageadmin/urls/network.py
@@ -17,9 +17,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from django.conf.urls import patterns, url
-from storageadmin.views import ( NetworkConnectionListView,
-                                 NetworkConnectionDetailView,
-                                 NetworkDeviceListView, NetworkStateView)
+from storageadmin.views import (NetworkConnectionListView,
+                                NetworkConnectionDetailView,
+                                NetworkDeviceListView, NetworkStateView)
 
 
 urlpatterns = patterns(
@@ -27,7 +27,8 @@ urlpatterns = patterns(
     url(r'^$', NetworkStateView.as_view()),
     url(r'^/connections$', NetworkConnectionListView.as_view()),
     url(r'^/connections/(?P<id>\d+)$', NetworkConnectionDetailView.as_view()),
-    url(r'^/connections/(?P<id>\d+)/(?P<switch>up|down|reload)$', NetworkConnectionDetailView.as_view()),
+    url(r'^/connections/(?P<id>\d+)/(?P<switch>up|down|reload)$',
+        NetworkConnectionDetailView.as_view()),
     url(r'^/devices$', NetworkDeviceListView.as_view()),
     url(r'^/refresh$', NetworkStateView.as_view()),
     )
