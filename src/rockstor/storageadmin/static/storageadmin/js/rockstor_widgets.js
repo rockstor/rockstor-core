31d42d2326bb98c394997b69731f0ca4f79faa1e
Mirko Arena <mirko.arena@gmail.com>
2017-01-29 23:56:25 +0100

    #1632 Eslint rockstor_widget.js
    Signed-off-by: Mirko Arena <mirko.arena@gmail.com>

--- src/rockstor/storageadmin/static/storageadmin/js/rockstor_widgets.js
+++ src/rockstor/storageadmin/static/storageadmin/js/rockstor_widgets.js
@@ -3,35 +3,34 @@ RockStorWidgets.max_width = 10;
 RockStorWidgets.max_height = 2;
 
 RockStorWidgets.findByName = function(name) {
-  return _.find(RockStorWidgets.widgetDefs, function(widget) {
-    return widget.name == name;
-  });
+    return _.find(RockStorWidgets.widgetDefs, function(widget) {
+        return widget.name == name;
+    });
 };
 
 RockStorWidgets.findByCategory = function(category) {
-  return _.filter(RockStorWidgets.widgetDefs, function(widget) {
-    return widget.category == category;
-  });
+    return _.filter(RockStorWidgets.widgetDefs, function(widget) {
+        return widget.category == category;
+    });
 };
 
 RockStorWidgets.defaultWidgets = function() {
-  var tmp = _.filter(RockStorWidgets.widgetDefs, function(widget) {
-    return widget.defaultWidget;
-  });
-  return _.sortBy(tmp, function(w) {
-    if (!_.isUndefined(w.position) && !_.isNull(w.position)) {
-      return w.position;
-    } else {
-      return Number.MAX_VALUE;
-    }
-  });
+    var tmp = _.filter(RockStorWidgets.widgetDefs, function(widget) {
+        return widget.defaultWidget;
+    });
+    return _.sortBy(tmp, function(w) {
+        if (!_.isUndefined(w.position) && !_.isNull(w.position)) {
+            return w.position;
+        } else {
+            return Number.MAX_VALUE;
+        }
+    });
 };
 
 RockStorWidgets.defaultWidgetNames = function(name) {
-  return _.map(RockStorWidgets.defaultWidgets(), function(widget) {
-    return widget.name;
-  });
+    return _.map(RockStorWidgets.defaultWidgets(), function(widget) {
+        return widget.name;
+    });
 };
 
 RockStorWidgets.widgetDefs = [];
-
