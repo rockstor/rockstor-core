2a868814b40219c47ba40522ce383940807f0cbb
Mirko Arena <mirko.arena@gmail.com>
2017-02-16 12:31:26 +0100

    Task model update part 2
    Signed-off-by: Mirko Arena <mirko.arena@gmail.com>

--- src/rockstor/smart_manager/models/task_def.py
+++ src/rockstor/smart_manager/models/task_def.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+Copyright (c) 2012-2017 RockStor, Inc. <http://rockstor.com>
 This file is part of RockStor.
 
 RockStor is free software; you can redistribute it and/or modify
@@ -24,6 +24,9 @@ class TaskDefinition(models.Model):
     TASK_TYPES = [
         ('scrub',) * 2,
         ('snapshot',) * 2,
+        ('reboot',) * 2,
+        ('shutdown',) * 2,
+        ('custom',) * 2
         ]
     task_type = models.CharField(max_length=100, choices=TASK_TYPES)
     json_meta = models.CharField(max_length=8192)
