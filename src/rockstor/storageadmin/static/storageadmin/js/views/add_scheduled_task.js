ea6760c4d74d421a0b9528147e4d289438d8eace
Mirko Arena <mirko.arena@gmail.com>
2017-02-19 10:25:46 +0100

    RTC System Wake up custom fields
    Refs to #735 #1306 - Adding rtc wake up hour and minute fields to
    shutdown template plus auto toggle on rtc wake up checkbox
    
    Signed-off-by: Mirko Arena <mirko.arena@gmail.com>

--- src/rockstor/storageadmin/static/storageadmin/js/views/add_scheduled_task.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/add_scheduled_task.js
@@ -26,35 +26,43 @@
 
 
 AddScheduledTaskView = RockstorLayoutView.extend({
-    events: {
-        'click #js-cancel': 'cancel',
-        'change #task_type': 'renderOptionalFields'
-    },
+	events: {
+		"click #js-cancel": "cancel",
+		"change #task_type": "renderOptionalFields",
+        "click #wakeup": "switchRtcFields"
+	},
 
-    initialize: function() {
-        this.constructor.__super__.initialize.apply(this, arguments);
-        this.template = window.JST.scheduled_tasks_add_task;
-        this.snapshotFieldsTemplate = window.JST.scheduled_tasks_snapshot_fields;
-        this.scrubFieldsTemplate = window.JST.scheduled_tasks_scrub_fields;
-        this.shares = new ShareCollection();
-        this.pools = new PoolCollection();
-        this.shares.pageSize = RockStorGlobals.maxPageSize;
-        this.pools.pageSize = RockStorGlobals.maxPageSize;
-        this.dependencies.push(this.shares);
-        this.dependencies.push(this.pools);
-        this.taskDefId = this.options.taskDefId;
-        if (!_.isUndefined(this.taskDefId) && !_.isNull(this.taskDefId)) {
-            this.taskDef = new TaskDef({
-                id: this.taskDefId
-            });
-            this.dependencies.push(this.taskDef);
-        }
-        if (this.taskDefId == null) {
-            this.taskDefIdNull = true;
-        } else {
-            this.taskDefIdNull = false;
-        }
-    },
+	initialize: function() {
+		this.constructor.__super__.initialize.apply(this, arguments);
+		this.template = window.JST.scheduled_tasks_add_task;
+		this.snapshotFieldsTemplate = window.JST.scheduled_tasks_snapshot_fields;
+		this.scrubFieldsTemplate = window.JST.scheduled_tasks_scrub_fields;
+        this.shutdownFieldsTemplate = window.JST.scheduled_tasks_shutdown_fields;
+		this.shares = new ShareCollection();
+		this.pools = new PoolCollection();
+		this.shares.pageSize = RockStorGlobals.maxPageSize;
+		this.pools.pageSize = RockStorGlobals.maxPageSize;
+		this.dependencies.push(this.shares);
+		this.dependencies.push(this.pools);
+		this.taskDefId = this.options.taskDefId;
+        this.taskTypes = [
+            {name: 'scrub', description: 'Btrfs Scrub'},
+            {name: 'snapshot', description: 'Btrfs Snapshot'},
+            {name: 'reboot', description: 'System Reboot'},
+            {name: 'shutdown', description: 'System Shutdown'},
+            //{name: 'custom', description: 'User Custom Task'}
+        ];
+		if (!_.isUndefined(this.taskDefId) && !_.isNull(this.taskDefId)) {
+			this.taskDef = new TaskDef({id: this.taskDefId});
+			this.dependencies.push(this.taskDef);
+		}
+		if(this.taskDefId == null){
+			this.taskDefIdNull = true;
+		}else{
+			this.taskDefIdNull = false;
+		}
+        this.initHandlebarHelpers();
+	},
 
     render: function() {
         this.fetch(this.renderNewScheduledTask, this);
@@ -82,12 +90,7 @@ AddScheduledTaskView = RockstorLayoutView.extend({
 		$(this.el).html(this.template({
 			shares: this.shares,
 			pools: this.pools,
-			taskTypes: ['snapshot',
-                                    'scrub',
-                                    'reboot',
-                                    'shutdown',
-                                    'custom'
-                                   ],
+			taskTypes: this.taskTypes,
 			taskDef: this.taskDef,
 			taskObj: taskObj,
 			taskDefId: this.taskDefId,
@@ -203,6 +206,14 @@ AddScheduledTaskView = RockstorLayoutView.extend({
 				taskDefId: this.taskDefId,
 				taskDefIdNull: this.taskDefIdNull,
 			}));
+		} else if (taskType == 'shutdown') {
+			this.$('#optional-fields').html(this.shutdownFieldsTemplate({
+				taskDef: this.taskDef,
+				taskDefId: this.taskDefId,
+				taskDefIdNull: this.taskDefIdNull,
+			}));
+            this.rendergentleSelect('rtc_hour');
+            this.rendergentleSelect('rtc_minute');
 		} else {
             this.$('#optional-fields').empty();
         }
@@ -212,10 +223,52 @@ AddScheduledTaskView = RockstorLayoutView.extend({
 		});
 	},
 
-    cancel: function(event) {
-        event.preventDefault();
-        app_router.navigate('scheduled-tasks', {
-            trigger: true
+    // Render rtc wake clock selects adding gentleSelect beautifier
+    rendergentleSelect: function(field_name) {
+        if (field_name == 'rtc_hour') {
+            itemwidth = 20;
+            columns = 2;
+            title = 'Wake up Time: Hour';
+        } else {
+            itemwidth = 30;
+            columns = 4;
+            title = 'Wake up Time: Minute'
+        }
+        this.$('#' + field_name).gentleSelect({
+            itemWidth: itemwidth,
+            columns: columns,
+            title: title
         });
     },
+
+    switchRtcFields: function () {
+        // Adding some animation, on enable rtc wake up
+        // checked true/false toggle rtc clock fields
+        this.$('#rtc_container').fadeToggle(500);
+    },
+
+    initHandlebarHelpers: function(){
+
+        // Handlebars helper creating rtc clock hours and mins options
+        Handlebars.registerHelper('time_select', function(field_name) {
+            var html = '';
+            var time_iterator;
+            if (field_name == 'rtc_hour') {
+                time_iterator = 24;
+            } else if (field_name == 'rtc_minute') {
+                time_iterator = 60;
+            }
+            for (var i = 0; i < time_iterator; i++) {
+                html += '<option value="' + i + '">';
+                html += i < 10 ? '0' + i : i;
+                html += '</option>';
+            }
+            return new Handlebars.SafeString(html);
+        });
+    },
+
+	cancel: function(event) {
+		event.preventDefault();
+		app_router.navigate('scheduled-tasks', {trigger: true});
+	},
 });
