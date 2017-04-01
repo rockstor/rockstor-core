2297c69e19ef0c0b47858c0d34f4454a125db1cd
Philip Guyton <philip@yewtreeapps.com>
2017-04-01 19:41:13 +0100

    add cryptsetup luksFormat command wrapper #550

--- src/rockstor/system/luks.py
+++ src/rockstor/system/luks.py
@@ -15,8 +15,9 @@ General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
-
+import os
 import re
+from tempfile import mkstemp
 from system.osi import run_command
 import logging
 
@@ -68,3 +69,40 @@ def get_luks_container_dev(mapped_device_name, test=None):
             return line_fields[1]
     return container_dev
 
+
+def luks_format_disk(disk_byid, passphrase):
+    """
+    Formats disk_byid using supplied passphrase for master key encryption.
+    Simple run_command wrapper to execute 'cryptsetup luksFormat <dev> path'
+    Care is taken to immediately remove our temporary key-file (in ram) even 
+    in the event of an Exception.
+    :param disk_byid: by-id type name without path as found in db Disks.name.
+    :param passphrase: luks passphrase used to encrypt master key.
+    :return: o, e, rc tuple as returned by cryptsetup luksFormat command.
+    """
+    disk_byid_withpath = ('/dev/disk/by-id/%s' % disk_byid)
+    # Create a temp file to pass our passphrase to our cryptsetup command.
+    tfo, npath = mkstemp()
+    # Pythons _candidate_tempdir_list() should ensure our npath temp file is
+    # in memory (tmpfs). From https://docs.python.org/2/library/tempfile.html
+    # we have "Creates a temporary file in the most secure manner possible."
+    # Populate this file with our passphrase and use as cryptsetup keyfile.
+    try:
+        with open(npath, 'w') as passphrase_file_object:
+            passphrase_file_object.write(passphrase)
+        cmd = [CRYPTSETUP, 'luksFormat', disk_byid_withpath, npath]
+        out, err, rc = run_command(cmd)
+    except Exception as e:
+        msg = ('Exception while running command(%s): %s' %
+               (cmd, e.__str__()))
+        raise Exception(msg)
+    finally:
+        passphrase_file_object.close()
+        if os.path.exists(npath):
+            try:
+                os.remove(npath)
+            except Exception as e:
+                msg = ('Exception while removing temp file %s' %
+                       (npath, e.__str__()))
+                raise Exception(msg)
+    return out, err, rc
