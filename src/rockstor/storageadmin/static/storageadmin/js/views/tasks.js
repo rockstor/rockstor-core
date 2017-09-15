8ed7b8721e5346e5dbf86cdc83890639d64d3c36
Philip Guyton <philip@yewtreeapps.com>
2017-09-15 17:30:28 +0100

    task history share name as link #1243
    As we already indicate the share name as part
    of the subtitle we can provide a convenience
    link to it's details page. Also includes minor
    text tidy of subtitle and todo updates.

--- src/rockstor/storageadmin/static/storageadmin/js/views/tasks.js
+++ src/rockstor/storageadmin/static/storageadmin/js/views/tasks.js
@@ -80,15 +80,17 @@ TasksView = RockstorLayoutView.extend({
         Handlebars.registerHelper('display_snapshot_scrub', function() {
             var html = '';
             if (this.taskDef.get('task_type') == 'snapshot') {
-                // TODO: once snapshot task moved to new share api change this
-                // TODO: to a share details page link akin to Scrubs below.
-                html += 'Snapshot of Share [' + JSON.parse(this.taskDef.get('json_meta')).share_name + ']';
+                html += 'Snapshot of Share (<a href="#shares/';
+                // TODO: fix this to go direct to Snapshots tab.
+                html += JSON.parse(this.taskDef.get('json_meta')).share + '">';
+                html += JSON.parse(this.taskDef.get('json_meta')).share_name;
+                html += '</a>): see "Snapshots" tab for details.';
             } else if (this.taskDef.get('task_type') == 'scrub'){
                 html += 'Scrub of Pool (<a href="#pools/';
-                // TODO: fix this to go direct to Scrubs tab
+                // TODO: fix this to go direct to Scrubs tab.
                 html += JSON.parse(this.taskDef.get('json_meta')).pool + '">';
                 html += JSON.parse(this.taskDef.get('json_meta')).pool_name;
-                html += '</a>) See "Scrubs" tab for details.';
+                html += '</a>): see "Scrubs" tab for details.';
             } else {
                 html += this.taskDef.get('task_type');
             }
