d5e3533d09170db52ab51b5c179859c05d870e63
Paul B <pabir101@hhu.de>
2019-04-29 04:25:24 +0200

    implemented the new feature in the backend

--- src/rockstor/scripts/scheduled_tasks/reboot_shutdown.py
+++ src/rockstor/scripts/scheduled_tasks/reboot_shutdown.py
@@ -15,7 +15,6 @@ General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
-
 import sys
 import json
 from datetime import (datetime, timedelta)
@@ -23,7 +22,13 @@ import crontabwindow  # load crontabwindow module
 from smart_manager.models import (Task, TaskDefinition)
 from cli.api_wrapper import APIWrapper
 from django.utils.timezone import utc
+from system.osi import is_network_device_responding
+from csv import reader as csv_reader
+import re
+import time
+
 import logging
+
 logger = logging.getLogger(__name__)
 
 
@@ -31,6 +36,45 @@ def validate_shutdown_meta(meta):
     if (type(meta) != dict):
         raise Exception('meta must be a dictionary, not %s' % type(meta))
     return meta
+
+
+def all_devices_offline(addresses):
+    for address in addresses:
+        logger.debug('Pinging ' + address + '...')
+        if is_network_device_responding(address):
+            return False
+    return True
+
+
+def run_conditions_met(meta):
+    if meta['ping_scan']:
+        address_parser = csv_reader([meta['ping_scan_addresses']], delimiter=',')
+        addresses = list(address_parser)
+
+        if len(addresses) > 0:
+            addresses = addresses[0]
+            # remove all non word characters except from '.' and '-' to prevent
+            # the execution of shell code when running the ping command
+            addresses = [re.sub(r"[^\w\.-]", '', address) for address in addresses]
+            interval = float(meta['ping_scan_interval']) if float(meta['ping_scan_interval']) >= 5 else 5.0
+            iterations = int(meta['ping_scan_iterations']) if int(meta['ping_scan_iterations']) >= 1 else 1
+
+            logger.debug('Pinging devices...' + str(interval) + ', ' + str(iterations))
+            for i in range(iterations):
+                if not all_devices_offline(addresses):
+                    logger.debug('At least one pinged device is still online')
+                    return False
+                else:
+                    # don't sleep after last scan
+                    if (i+1) >= iterations:
+                        break
+                    logger.debug('No device answered, trying again in ' + str(interval) + ' seconds')
+                    # simply sleep as done in pool_scrub.py
+                    time.sleep(interval)
+
+            logger.debug('All pinged devices are offline')
+
+    return True
 
 
 def main():
@@ -47,6 +91,10 @@ def main():
             return
         meta = json.loads(tdo.json_meta)
         validate_shutdown_meta(meta)
+
+        if not run_conditions_met(meta):
+            logger.debug('Cron scheduled task not executed because the run conditions have not been met')
+            return
 
         now = datetime.utcnow().replace(second=0, microsecond=0, tzinfo=utc)
         schedule = now + timedelta(minutes=3)
