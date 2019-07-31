2364bdf62fab3c1c9b46ba9531ab8479cb453779
FroggyFlox <flox2b@gmail.com>
2019-07-31 13:06:38 -0400

    Add tests for valid and invalid PUT and DELETE requests.

--- src/rockstor/smart_manager/tests/test_task_scheduler.py
+++ src/rockstor/smart_manager/tests/test_task_scheduler.py
@@ -27,7 +27,7 @@ class TaskSchedulerTests(APITestCase):
     BASE_URL = "/api/sm/tasks"
 
     def session_login(self):
-        self.client.login(username='admin', password='admin')
+        self.client.login(username="admin", password="admin")
 
     def test_get(self):
         """
@@ -35,56 +35,66 @@ class TaskSchedulerTests(APITestCase):
         """
         # get scheduled task with valid id
         self.session_login()
-        response = self.client.get('{}/10'.format(self.BASE_URL))
-        self.assertEqual(response.status_code, status.HTTP_200_OK,
-                         msg=response)
+        response = self.client.get("{}/10".format(self.BASE_URL))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response)
 
         # get scheduled task with invalid id
         self.session_login()
-        response = self.client.get('{}/100'.format(self.BASE_URL))
-        self.assertEqual(response.status_code, status.HTTP_200_OK,
-                         msg=response)
+        response = self.client.get("{}/100".format(self.BASE_URL))
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response)
 
     def test_post_valid(self):
         """
         Test valid POST request
         """
-        datalist = [{
-            'task_type': 'scrub',
-            'name': 'scrubtest',
-            'enabled': False,
-            'crontab': '42 3 * * 5',
-            'meta': {'pool': '3'},
-            'crontabwindow': '*-*-*-*-*-*'
-        }, {
-            'task_type': 'snapshot',
-            'name': 'snapshot_test',
-            'enabled': False,
-            'crontab': '42 3 * * 5',
-            'meta': {"writable": True, "visible": True, "prefix": "snaptest", "share": "4", "max_count": "4"},
-            'crontabwindow': '*-*-*-*-*-*'
-        }, {
-            'task_type': 'reboot',
-            'name': 'sys_reboot_test',
-            'enabled': False,
-            'crontab': '42 3 * * 5',
-            'meta': {},
-            'crontabwindow': '*-*-*-*-*-*'
-        }, {
-            'task_type': 'shutdown',
-            'name': 'sys_shutdown_test',
-            'enabled': False,
-            'crontab': '42 3 * * 5',
-            'meta': {"wakeup": False, "rtc_hour": "0", "rtc_minute": "0"},
-            'crontabwindow': '*-*-*-*-*-*'
-        }, {
-            'task_type': 'suspend',
-            'name': 'sys_suspend_test',
-            'enabled': False,
-            'crontab': '42 3 * * 5',
-            'meta': {"wakeup": True, "rtc_hour": "0", "rtc_minute": "0"},
-            'crontabwindow': '*-*-*-*-*-*'
-        }]
+        datalist = [
+            {
+                "task_type": "scrub",
+                "name": "scrubtest",
+                "enabled": False,
+                "crontab": "42 3 * * 5",
+                "meta": {"pool": "3"},
+                "crontabwindow": "*-*-*-*-*-*",
+            },
+            {
+                "task_type": "snapshot",
+                "name": "snapshot_test",
+                "enabled": False,
+                "crontab": "42 3 * * 5",
+                "meta": {
+                    "writable": True,
+                    "visible": True,
+                    "prefix": "snaptest",
+                    "share": "4",
+                    "max_count": "4",
+                },
+                "crontabwindow": "*-*-*-*-*-*",
+            },
+            {
+                "task_type": "reboot",
+                "name": "sys_reboot_test",
+                "enabled": False,
+                "crontab": "42 3 * * 5",
+                "meta": {},
+                "crontabwindow": "*-*-*-*-*-*",
+            },
+            {
+                "task_type": "shutdown",
+                "name": "sys_shutdown_test",
+                "enabled": False,
+                "crontab": "42 3 * * 5",
+                "meta": {"wakeup": False, "rtc_hour": "0", "rtc_minute": "0"},
+                "crontabwindow": "*-*-*-*-*-*",
+            },
+            {
+                "task_type": "suspend",
+                "name": "sys_suspend_test",
+                "enabled": False,
+                "crontab": "42 3 * * 5",
+                "meta": {"wakeup": True, "rtc_hour": "0", "rtc_minute": "0"},
+                "crontabwindow": "*-*-*-*-*-*",
+            },
+        ]
         # data = {
         #     'task_type': 'scrub',
         #     'name': 'scrubtest',
@@ -96,10 +106,10 @@ class TaskSchedulerTests(APITestCase):
 
         self.session_login()
         for data in datalist:
-            response = self.client.post('{}/'.format(self.BASE_URL), data=data)
-            self.assertEqual(response.status_code,
-                             status.HTTP_200_OK,
-                             msg=response.content)
+            response = self.client.post("{}/".format(self.BASE_URL), data=data)
+            self.assertEqual(
+                response.status_code, status.HTTP_200_OK, msg=response.content
+            )
 
     def test_post_name_exists(self):
         """
@@ -107,19 +117,21 @@ class TaskSchedulerTests(APITestCase):
         It should return an exception.
         """
         data = {
-            'task_type': 'scrub',
-            'name': 'scurb-testpool01',
-            'enabled': False,
-            'crontab': '42 3 * * 5',
-            'meta': {'pool': '3'},
-            'crontabwindow': '*-*-*-*-*-*'
-            }
+            "task_type": "scrub",
+            "name": "scurb-testpool01",
+            "enabled": False,
+            "crontab": "42 3 * * 5",
+            "meta": {"pool": "3"},
+            "crontabwindow": "*-*-*-*-*-*",
+        }
 
         self.session_login()
-        response = self.client.post('{}/'.format(self.BASE_URL), data=data)
-        self.assertEqual(response.status_code,
-                         status.HTTP_500_INTERNAL_SERVER_ERROR,
-                         msg=response.content)
+        response = self.client.post("{}/".format(self.BASE_URL), data=data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            msg=response.content,
+        )
 
     def test_post_invalid_type(self):
         """
@@ -127,16 +139,118 @@ class TaskSchedulerTests(APITestCase):
         It should return an exception.
         """
         data = {
-            'task_type': 'unsupported_type',
-            'name': 'test_scrub',
-            'enabled': False,
-            'crontab': '42 3 * * 5',
-            'meta': {'pool': '3'},
-            'crontabwindow': '*-*-*-*-*-*'
+            "task_type": "unsupported_type",
+            "name": "test_scrub",
+            "enabled": False,
+            "crontab": "42 3 * * 5",
+            "meta": {"pool": "3"},
+            "crontabwindow": "*-*-*-*-*-*",
         }
 
         self.session_login()
-        response = self.client.post('{}/'.format(self.BASE_URL), data=data)
-        self.assertEqual(response.status_code,
-                         status.HTTP_500_INTERNAL_SERVER_ERROR,
-                         msg=response.content)
+        response = self.client.post("{}/".format(self.BASE_URL), data=data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            msg=response.content,
+        )
+
+    def test_put_valid(self):
+        """
+        Test PUT request with valid tdid
+        """
+        data = {
+            "task_type": "snapshot",
+            "name": "snapshot_test",
+            "enabled": False,
+            "crontab": "42 3 * * 5",
+            "meta": {
+                "writable": True,
+                "visible": True,
+                "prefix": "snaptest",
+                "share": "4",
+                "max_count": "4",
+            },
+            "crontabwindow": "*-*-*-*-*-*",
+        }
+        tdid = 15
+        self.session_login()
+        response = self.client.put("{}/{}".format(self.BASE_URL, tdid), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content)
+
+    def test_put_invalid(self):
+        """
+        Test PUT request with invalid tdid
+        """
+        data = {
+            "task_type": "snapshot",
+            "name": "snapshot_test",
+            "enabled": False,
+            "crontab": "42 3 * * 5",
+            "meta": {
+                "writable": True,
+                "visible": True,
+                "prefix": "snaptest",
+                "share": "4",
+                "max_count": "4",
+            },
+            "crontabwindow": "*-*-*-*-*-*",
+        }
+        tdid = 150
+        self.session_login()
+        response = self.client.put("{}/{}".format(self.BASE_URL, tdid), data=data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            msg=response.content,
+        )
+
+    def test_delete_valid(self):
+        """
+        Test delete request with valid tdid
+        """
+        data = {
+            "task_type": "snapshot",
+            "name": "snapshot_test",
+            "enabled": False,
+            "crontab": "42 3 * * 5",
+            "meta": {
+                "writable": True,
+                "visible": True,
+                "prefix": "snaptest",
+                "share": "4",
+                "max_count": "4",
+            },
+            "crontabwindow": "*-*-*-*-*-*",
+        }
+        tdid = 15
+        self.session_login()
+        response = self.client.delete("{}/{}".format(self.BASE_URL, tdid), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, msg=response.content)
+
+    def test_delete_invalid(self):
+        """
+        Test delete request with invalid tdid
+        """
+        data = {
+            "task_type": "snapshot",
+            "name": "snapshot_test",
+            "enabled": False,
+            "crontab": "42 3 * * 5",
+            "meta": {
+                "writable": True,
+                "visible": True,
+                "prefix": "snaptest",
+                "share": "4",
+                "max_count": "4",
+            },
+            "crontabwindow": "*-*-*-*-*-*",
+        }
+        tdid = 150
+        self.session_login()
+        response = self.client.delete("{}/{}".format(self.BASE_URL, tdid), data=data)
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_500_INTERNAL_SERVER_ERROR,
+            msg=response.content,
+        )
