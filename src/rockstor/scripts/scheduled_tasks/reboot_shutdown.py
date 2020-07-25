d5815170ea38c9cb6709b69220f7f5807b83ed94
FroggyFlox <flox2b@gmail.com>
2020-07-25 12:02:42 -0400

    Black formatting + Copyright update: scripts/scheduled_tasks

--- src/rockstor/scripts/scheduled_tasks/reboot_shutdown.py
+++ src/rockstor/scripts/scheduled_tasks/reboot_shutdown.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2012-2017 RockStor, Inc. <http://rockstor.com>
+Copyright (c) 2012-2020 RockStor, Inc. <http://rockstor.com>
 This file is part of RockStor.
 
 RockStor is free software; you can redistribute it and/or modify
@@ -17,9 +17,9 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import sys
 import json
-from datetime import (datetime, timedelta)
+from datetime import datetime, timedelta
 import crontabwindow  # load crontabwindow module
-from smart_manager.models import (Task, TaskDefinition)
+from smart_manager.models import Task, TaskDefinition
 from cli.api_wrapper import APIWrapper
 from django.utils.timezone import utc
 from system.osi import is_network_device_responding
@@ -33,108 +33,126 @@ logger = logging.getLogger(__name__)
 
 
 def validate_shutdown_meta(meta):
-    if (type(meta) != dict):
-        raise Exception('meta must be a dictionary, not %s' % type(meta))
+    if type(meta) != dict:
+        raise Exception("meta must be a dictionary, not %s" % type(meta))
     return meta
 
 
 def all_devices_offline(addresses):
     for address in addresses:
-        logger.debug('Pinging {}...'.format(address))
+        logger.debug("Pinging {}...".format(address))
         if is_network_device_responding(address):
             return False
     return True
 
 
 def run_conditions_met(meta):
-    if meta['ping_scan']:
-        address_parser = csv_reader([meta['ping_scan_addresses']], delimiter=',')
+    if meta["ping_scan"]:
+        address_parser = csv_reader([meta["ping_scan_addresses"]], delimiter=",")
         addresses = list(address_parser)
 
         if len(addresses) > 0:
             addresses = addresses[0]
             # remove all non word characters except from '.' and '-' to prevent
             # the execution of shell code when running the ping command
-            addresses = [re.sub(r"[^\w\.-]", '', address) for address in addresses]
-            interval = float(meta['ping_scan_interval']) if float(meta['ping_scan_interval']) >= 5 else 5.0
-            iterations = int(meta['ping_scan_iterations']) if int(meta['ping_scan_iterations']) >= 1 else 1
+            addresses = [re.sub(r"[^\w\.-]", "", address) for address in addresses]
+            interval = (
+                float(meta["ping_scan_interval"])
+                if float(meta["ping_scan_interval"]) >= 5
+                else 5.0
+            )
+            iterations = (
+                int(meta["ping_scan_iterations"])
+                if int(meta["ping_scan_iterations"]) >= 1
+                else 1
+            )
 
-            logger.debug('Pinging devices: {}'.format(addresses))
+            logger.debug("Pinging devices: {}".format(addresses))
             for i in range(iterations):
                 if not all_devices_offline(addresses):
-                    logger.debug('At least one pinged device is still online')
+                    logger.debug("At least one pinged device is still online")
                     return False
                 else:
                     # don't sleep after last scan
-                    if (i+1) >= iterations:
+                    if (i + 1) >= iterations:
                         break
-                    logger.debug('No device answered, trying again in {} seconds.'.format(interval))
+                    logger.debug(
+                        "No device answered, trying again in {} seconds.".format(
+                            interval
+                        )
+                    )
                     # simply sleep as done in pool_scrub.py
                     time.sleep(interval)
 
-            logger.debug('All pinged devices are offline')
+            logger.debug("All pinged devices are offline")
 
     return True
 
 
 def main():
     tid = int(sys.argv[1])
-    cwindow = sys.argv[2] if len(sys.argv) > 2 else '*-*-*-*-*-*'
-    if (crontabwindow.crontab_range(cwindow)):
+    cwindow = sys.argv[2] if len(sys.argv) > 2 else "*-*-*-*-*-*"
+    if crontabwindow.crontab_range(cwindow):
         # Performance note: immediately check task execution time/day window
         # range to avoid other calls
         tdo = TaskDefinition.objects.get(id=tid)
         aw = APIWrapper()
-        if (tdo.task_type not in ['reboot', 'shutdown', 'suspend']):
-            logger.error('task_type(%s) is not a system reboot, '
-                         'shutdown or suspend.' % tdo.task_type)
+        if tdo.task_type not in ["reboot", "shutdown", "suspend"]:
+            logger.error(
+                "task_type(%s) is not a system reboot, "
+                "shutdown or suspend." % tdo.task_type
+            )
             return
         meta = json.loads(tdo.json_meta)
         validate_shutdown_meta(meta)
 
         if not run_conditions_met(meta):
-            logger.debug('Cron scheduled task not executed because the run conditions have not been met')
+            logger.debug(
+                "Cron scheduled task not executed because the run conditions have not been met"
+            )
             return
 
         now = datetime.utcnow().replace(second=0, microsecond=0, tzinfo=utc)
         schedule = now + timedelta(minutes=3)
-        t = Task(task_def=tdo, state='scheduled', start=now, end=schedule)
+        t = Task(task_def=tdo, state="scheduled", start=now, end=schedule)
 
         try:
             # set default command url before checking if it's a shutdown
             # and if we have an rtc wake up
-            url = ('commands/%s' % tdo.task_type)
+            url = "commands/%s" % tdo.task_type
 
             # if task_type is shutdown and rtc wake up true
             # parse crontab hour & minute vs rtc hour & minute to state
             # if wake will occur same day or next day, finally update
             # command url adding wake up epoch time
-            if (tdo.task_type in ['shutdown', 'suspend'] and meta['wakeup']):
+            if tdo.task_type in ["shutdown", "suspend"] and meta["wakeup"]:
                 crontab_fields = tdo.crontab.split()
-                crontab_time = (int(crontab_fields[1]) * 60 +
-                                int(crontab_fields[0]))
-                wakeup_time = meta['rtc_hour'] * 60 + meta['rtc_minute']
+                crontab_time = int(crontab_fields[1]) * 60 + int(crontab_fields[0])
+                wakeup_time = meta["rtc_hour"] * 60 + meta["rtc_minute"]
                 # rtc wake up requires UTC epoch, but users on WebUI set time
                 # thinking to localtime, so first we set wake up time,
                 # update it if wake up is on next day, finally move it to UTC
                 # and get its epoch
-                epoch = datetime.now().replace(hour=int(meta['rtc_hour']),
-                                               minute=int(meta['rtc_minute']),
-                                               second=0, microsecond=0)
+                epoch = datetime.now().replace(
+                    hour=int(meta["rtc_hour"]),
+                    minute=int(meta["rtc_minute"]),
+                    second=0,
+                    microsecond=0,
+                )
                 # if wake up < crontab time wake up will run next day
-                if (crontab_time > wakeup_time):
+                if crontab_time > wakeup_time:
                     epoch += timedelta(days=1)
 
-                epoch = epoch.strftime('%s')
-                url = ('%s/%s' % (url, epoch))
+                epoch = epoch.strftime("%s")
+                url = "%s/%s" % (url, epoch)
 
-            aw.api_call(url, data=None, calltype='post', save_error=False)
-            logger.debug('System %s scheduled' % tdo.task_type)
-            t.state = 'finished'
+            aw.api_call(url, data=None, calltype="post", save_error=False)
+            logger.debug("System %s scheduled" % tdo.task_type)
+            t.state = "finished"
 
         except Exception as e:
-            t.state = 'failed'
-            logger.error('Failed to schedule system %s' % tdo.task_type)
+            t.state = "failed"
+            logger.error("Failed to schedule system %s" % tdo.task_type)
             logger.exception(e)
 
         finally:
@@ -142,10 +160,11 @@ def main():
             t.save()
 
     else:
-        logger.debug('Cron scheduled task not executed because outside '
-                     'time/day window ranges')
+        logger.debug(
+            "Cron scheduled task not executed because outside time/day window ranges"
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # takes two arguments. taskdef object id and crontabwindow.
     main()
