f65fd1faf302499c10228f8ebe7b0a04cbaee567
FroggyFlox <flox2b@gmail.com>
2020-07-25 19:57:50 -0400

    Black formatting + Copyright update: smart_manager/urls

--- src/rockstor/smart_manager/urls/sprobes.py
+++ src/rockstor/smart_manager/urls/sprobes.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+Copyright (c) 2012-2020 RockStor, Inc. <http://rockstor.com>
 This file is part of RockStor.
 
 RockStor is free software; you can redistribute it and/or modify
@@ -17,68 +17,93 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from django.conf.urls import patterns, url
-from smart_manager.views import (SProbeView, MemInfoView, NetStatView,
-                                 DiskStatView, NFSDistribView,
-                                 NFSDClientDistribView, NFSDShareDistribView,
-                                 NFSDShareClientDistribView, CPUMetricView,
-                                 NFSDUidGidDistributionView, LoadAvgView,
-                                 SProbeMetadataView, SProbeMetadataDetailView,)
+from smart_manager.views import (
+    SProbeView,
+    MemInfoView,
+    NetStatView,
+    DiskStatView,
+    NFSDistribView,
+    NFSDClientDistribView,
+    NFSDShareDistribView,
+    NFSDShareClientDistribView,
+    CPUMetricView,
+    NFSDUidGidDistributionView,
+    LoadAvgView,
+    SProbeMetadataView,
+    SProbeMetadataDetailView,
+)
 
 
 urlpatterns = patterns(
-    '',
+    "",
     # Smart probes
-    url(r'^$', SProbeView.as_view(), name='probe-view'),
-    url(r'^metadata$', SProbeMetadataView.as_view(), name='probe-view'),
-    url(r'^metadata/(?P<pid>[0-9]+)$', SProbeMetadataDetailView.as_view(),
-        name='probe-view'),
-
+    url(r"^$", SProbeView.as_view(), name="probe-view"),
+    url(r"^metadata$", SProbeMetadataView.as_view(), name="probe-view"),
+    url(
+        r"^metadata/(?P<pid>[0-9]+)$",
+        SProbeMetadataDetailView.as_view(),
+        name="probe-view",
+    ),
     # Generic smart probes
-    url(r'^diskstat/$', DiskStatView.as_view(),
-        name='diskstat-view'),
-    url(r'^meminfo/$', MemInfoView.as_view(),
-        name='meminfo-view'),
-    url(r'^netstat/$', NetStatView.as_view(),
-        name='netstat-view'),
-    url(r'^cpumetric/$', CPUMetricView.as_view(), name='cpumetric-view'),
-    url(r'^loadavg$', LoadAvgView.as_view(), name='loadavg-view'),
-
-
+    url(r"^diskstat/$", DiskStatView.as_view(), name="diskstat-view"),
+    url(r"^meminfo/$", MemInfoView.as_view(), name="meminfo-view"),
+    url(r"^netstat/$", NetStatView.as_view(), name="netstat-view"),
+    url(r"^cpumetric/$", CPUMetricView.as_view(), name="cpumetric-view"),
+    url(r"^loadavg$", LoadAvgView.as_view(), name="loadavg-view"),
     # Advanced smart probes
-    url(r'^nfs-1$', NFSDistribView.as_view(), name='nfsdistrib-view'),
-    url(r'^nfs-1/(?P<pid>[0-9]+)$', NFSDistribView.as_view(),
-        name='nfsdistrib-view'),
-    url(r'^nfs-1/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$',
-        NFSDistribView.as_view(), name='nfsdistrib-view'),
-
-    url(r'^nfs-2$', NFSDClientDistribView.as_view(),
-        name='nfsclientdistrib-view'),
-    url(r'^nfs-2/(?P<pid>[0-9]+)$',
-        NFSDClientDistribView.as_view(), name='nfsclientdistrib-view'),
-    url(r'^nfs-2/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$',
-        NFSDClientDistribView.as_view(), name='nfsclientdistrib-view'),
-
-    url(r'^nfs-3$', NFSDShareDistribView.as_view(),
-        name='nfssharedistrib-view'),
-    url(r'^nfs-3/(?P<pid>[0-9]+)$',
-        NFSDShareDistribView.as_view(), name='nfssharedistrib-view'),
-    url(r'^nfs-3/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$',
-        NFSDShareDistribView.as_view(), name='nfssharedistrib-view'),
-
-    url(r'^nfs-4$', NFSDShareClientDistribView.as_view(),
-        name='nfsshareclientdistrib-view'),
-    url(r'^nfs-4/(?P<pid>[0-9]+)$',
+    url(r"^nfs-1$", NFSDistribView.as_view(), name="nfsdistrib-view"),
+    url(r"^nfs-1/(?P<pid>[0-9]+)$", NFSDistribView.as_view(), name="nfsdistrib-view"),
+    url(
+        r"^nfs-1/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$",
+        NFSDistribView.as_view(),
+        name="nfsdistrib-view",
+    ),
+    url(r"^nfs-2$", NFSDClientDistribView.as_view(), name="nfsclientdistrib-view"),
+    url(
+        r"^nfs-2/(?P<pid>[0-9]+)$",
+        NFSDClientDistribView.as_view(),
+        name="nfsclientdistrib-view",
+    ),
+    url(
+        r"^nfs-2/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$",
+        NFSDClientDistribView.as_view(),
+        name="nfsclientdistrib-view",
+    ),
+    url(r"^nfs-3$", NFSDShareDistribView.as_view(), name="nfssharedistrib-view"),
+    url(
+        r"^nfs-3/(?P<pid>[0-9]+)$",
+        NFSDShareDistribView.as_view(),
+        name="nfssharedistrib-view",
+    ),
+    url(
+        r"^nfs-3/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$",
+        NFSDShareDistribView.as_view(),
+        name="nfssharedistrib-view",
+    ),
+    url(
+        r"^nfs-4$",
         NFSDShareClientDistribView.as_view(),
-        name='nfsshareclientdistrib-view'),
-    url(r'^nfs-4/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$',
+        name="nfsshareclientdistrib-view",
+    ),
+    url(
+        r"^nfs-4/(?P<pid>[0-9]+)$",
         NFSDShareClientDistribView.as_view(),
-        name='nfsshareclientdistrib-view'),
-
-    url(r'^nfs-5$', NFSDUidGidDistributionView.as_view(),
-        name='nfsuidgid-view'),
-    url(r'^nfs-5/(?P<pid>[0-9]+)$',
+        name="nfsshareclientdistrib-view",
+    ),
+    url(
+        r"^nfs-4/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$",
+        NFSDShareClientDistribView.as_view(),
+        name="nfsshareclientdistrib-view",
+    ),
+    url(r"^nfs-5$", NFSDUidGidDistributionView.as_view(), name="nfsuidgid-view"),
+    url(
+        r"^nfs-5/(?P<pid>[0-9]+)$",
         NFSDUidGidDistributionView.as_view(),
-        name='nfsuidgid-view'),
-    url(r'^nfs-5/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$',
-        NFSDUidGidDistributionView.as_view(), name='nfsuidgid-view'),
+        name="nfsuidgid-view",
+    ),
+    url(
+        r"^nfs-5/(?P<pid>[0-9]+)/(?P<command>[a-z]+)$",
+        NFSDUidGidDistributionView.as_view(),
+        name="nfsuidgid-view",
+    ),
 )
--- src/rockstor/smart_manager/urls/tasks.py
+++ src/rockstor/smart_manager/urls/tasks.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2012-2013 RockStor, Inc. <http://rockstor.com>
+Copyright (c) 2012-2020 RockStor, Inc. <http://rockstor.com>
 This file is part of RockStor.
 
 RockStor is free software; you can redistribute it and/or modify
@@ -17,17 +17,20 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from django.conf.urls import patterns, url
-from smart_manager.views import (TaskSchedulerListView,
-                                 TaskLogView, TaskTypeView,
-                                 TaskSchedulerDetailView)
+from smart_manager.views import (
+    TaskSchedulerListView,
+    TaskLogView,
+    TaskTypeView,
+    TaskSchedulerDetailView,
+)
 
 urlpatterns = patterns(
-    '',
-    url(r'^$', TaskSchedulerListView.as_view(),),
-    url(r'^(?P<tdid>\d+)$', TaskSchedulerDetailView.as_view(),),
-    url(r'^log$', TaskLogView.as_view(),),
-    url(r'^log/(?P<command>prune)$', TaskLogView.as_view(),),
-    url(r'^log/taskdef/(?P<tdid>\d+)', TaskLogView.as_view(),),
-    url(r'^log/(?P<tid>\d+)', TaskLogView.as_view(),),
-    url(r'^types$', TaskTypeView.as_view(),),
+    "",
+    url(r"^$", TaskSchedulerListView.as_view(),),
+    url(r"^(?P<tdid>\d+)$", TaskSchedulerDetailView.as_view(),),
+    url(r"^log$", TaskLogView.as_view(),),
+    url(r"^log/(?P<command>prune)$", TaskLogView.as_view(),),
+    url(r"^log/taskdef/(?P<tdid>\d+)", TaskLogView.as_view(),),
+    url(r"^log/(?P<tid>\d+)", TaskLogView.as_view(),),
+    url(r"^types$", TaskTypeView.as_view(),),
 )
