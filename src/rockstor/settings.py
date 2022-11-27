10a4fbe118062cbbbca902a7a60a08a452f97844
Philip Guyton <philip@yewtreeapps.com>
2022-11-27 21:59:28 +0000

    activate default static files FileSystemFinder #2432
    We previously didn't use this and instead untared our
    custom static files directly into ./static however
    these files were then not recognised and consequently
    not post-processed by django-admin collectstatic or
    buildbuild equivalent. By setting up an unfortunately
    static jslibs dir, we at least return to a more proper
    collectstatic post-process and avoid inadvertently
    tampering with the nature of the generated final
    static directory.
    We may well have to re-visit / re-consider this.

--- src/rockstor/settings.py
+++ src/rockstor/settings.py
@@ -93,18 +93,15 @@ MEDIA_ROOT = os.path.join('/opt/rockstor', 'static')
 DEFAULT_CB_DIR = os.path.join(MEDIA_ROOT, 'config-backups')
 
 # Additional locations of static files
-#STATICFILES_DIRS = (
-#    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-#    # Always use forward slashes, even on Windows.
-#    # Don't forget to use absolute paths, not relative paths.
-#    ('storageadmin', '/opt/rockstor/src/rockstor/storageadmin/static/storageadmin')
-#)
+STATICFILES_DIRS = (
+   ('/opt/rockstor/jslibs',)
+)
 
 # List of finder classes that know how to find static files in
 # various locations.
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    # 'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.FileSystemFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
