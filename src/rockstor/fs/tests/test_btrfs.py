79c9ffd234de9d161564d490e8bcda76786991e6
Philip Guyton <phillxnet@users.noreply.github.com>
2023-06-06 19:45:55 +0100

    Update src/rockstor/fs/tests/test_btrfs.py copywrite
    Co-authored-by: Hooverdan96 <35113775+Hooverdan96@users.noreply.github.com>

--- src/rockstor/fs/tests/test_btrfs.py
+++ src/rockstor/fs/tests/test_btrfs.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2012-2017 RockStor, Inc. <https://rockstor.com>
+Copyright (c) 2012-2023 RockStor, Inc. <https://rockstor.com>
 This file is part of RockStor.
 RockStor is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published
