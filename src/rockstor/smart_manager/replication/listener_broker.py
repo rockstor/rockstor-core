545ee229cff81682af0225520935c90f5a840dc3
Philip Guyton <philip@yewtreeapps.com>
2023-12-16 18:13:42 +0000

    (t) replication spawn error #2766
    Modernise previously missed replication imports re Py3.*

--- src/rockstor/smart_manager/replication/listener_broker.py
+++ src/rockstor/smart_manager/replication/listener_broker.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2012-2020 RockStor, Inc. <http://rockstor.com>
+Copyright (c) 2012-2023 RockStor, Inc. <https://rockstor.com>
 This file is part of RockStor.
 
 RockStor is free software; you can redistribute it and/or modify
@@ -13,7 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
 from multiprocessing import Process
@@ -24,9 +24,9 @@ import time
 from storageadmin.models import NetworkConnection, Appliance
 from smart_manager.models import ReplicaTrail, ReplicaShare, Replica, Service
 from django.conf import settings
-from sender import Sender
-from receiver import Receiver
-from util import ReplicationMixin
+from smart_manager.replication.sender import Sender
+from smart_manager.replication.receiver import Receiver
+from smart_manager.replication.util import ReplicationMixin
 from cli import APIWrapper
 import logging
 
--- src/rockstor/smart_manager/replication/receiver.py
+++ src/rockstor/smart_manager/replication/receiver.py
@@ -1,5 +1,5 @@
 """
-Copyright (c) 2012-2020 RockStor, Inc. <http://rockstor.com>
+Copyright (c) 2012-2023 RockStor, Inc. <https://rockstor.com>
 This file is part of RockStor.
 
 RockStor is free software; you can redistribute it and/or modify
@@ -13,7 +13,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
 from multiprocessing import Process
@@ -26,12 +26,11 @@ import time
 from django.conf import settings
 from django import db
 from contextlib import contextmanager
-from util import ReplicationMixin
+from smart_manager.replication.util import ReplicationMixin
 from fs.btrfs import get_oldest_snap, remove_share, set_property, is_subvol, mount_share
 from system.osi import run_command
 from storageadmin.models import Pool, Share, Appliance
 from smart_manager.models import ReplicaShare, ReceiveTrail
-import shutil
 from cli import APIWrapper
 import logging
 
