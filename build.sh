dd809daaaaf133ad43c6599ef53e65b7be7fc0f9
Philip Guyton <philip@yewtreeapps.com>
2024-01-23 17:54:46 +0000

    Failure to remove legacy poetry version in 5.0.6-0 rpm - continued4 #2782
    - remove deleted-in-%post RPM scriptlet .venv related path.

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ if which poetry && poetry --version | grep -q "1.1.15"; then
   curl -sSL https://install.python-poetry.org | python3 - --uninstall
   rm --force /root/.local/bin/poetry  # remove dangling dead link.
 fi
-# bash substring replace: null all legacy poetry paths - mid-instance
-PATH="${PATH//':/root/.local/bin'/''}"
-# bash substring remove (front): our above added temp legacy poetry path.
-PATH="${PATH#'/root/.local/bin:'}"
+PATH="${PATH//'/root/.local/bin:'/''}" # null all legacy poetry paths
+# We are run, outside of development, only by RPM's %posttrans.
+# As such our .venv dir has already been removed in %post (update mode).
+PATH="${PATH//'/opt/rockstor/.venv/bin:'/''}" # null now removed .venv from path.
 echo "build.sh has PATH=$PATH"
 # Install Poetry via PIPX as a global app
 # https://peps.python.org/pep-0668/#guide-users-towards-virtual-environments
@@ -31,6 +31,7 @@ python3.11 -m pipx install poetry==1.7.1
 # https://github.com/python-poetry/poetry/issues/3078
 export LANG=C.UTF-8
 export PYTHONIOENCODING=utf8
+# /usr/local/bin/poetry -> /opt/pipx/venvs/poetry
 /usr/local/bin/poetry install --no-interaction --no-ansi > poetry-install.txt 2>&1
 echo
 
