118ca800fe54889b7cdf6bf6de0630f0ff5323a0
Philip Guyton <philip@yewtreeapps.com>
2024-02-06 14:42:49 +0000

    (t) Samba shares not accessible - 5.0.6-0 & 5.0.7-0 - continued #2794
    Update poetry-plugin-dotenv to latest 0.6.10

--- build.sh
+++ build.sh
@@ -35,7 +35,7 @@ pipx ensurepath
 pipx install --python python3.11 poetry==1.7.1
 # https://pypi.org/project/poetry-dotenv-plugin/
 # https://python-poetry.org/docs/master/plugins/#using-plugins
-pipx inject --verbose poetry poetry-plugin-dotenv==0.6.5
+pipx inject --verbose poetry poetry-plugin-dotenv==0.6.10
 pipx list
 
 # Install project dependencies defined in cwd pyproject.toml using poetry.toml
