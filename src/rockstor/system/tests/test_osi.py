ac5ef5e35f981a62d6ca90f7c2931d2cfb6fac2f
Philip Guyton <philip@yewtreeapps.com>
2024-03-28 12:07:27 +0000

    fix remaining issues with prior test_root_disk() #2749

--- src/rockstor/system/tests/test_osi.py
+++ src/rockstor/system/tests/test_osi.py
@@ -36,9 +36,9 @@ class Pool(object):
 
 class OSITests(unittest.TestCase):
     """
-    The tests in this suite can be run via the following command:
-    cd <root dir of rockstor ie /opt/rockstor>
-    ./bin/test --settings=test-settings -v 3 -p test_osi*
+    The tests in this suite can be run via the following:
+    cd /opt/rockstor/src/rockstor
+    poetry run django-admin test -v 2 -p test_osi*
     """
 
     def setUp(self):
@@ -2854,66 +2854,47 @@ class OSITests(unittest.TestCase):
     #
     def test_root_disk(self):
         """
-        test root_disk() for expected function of identifying the base device name for the
-        root mount point by mocking a variety of outputs from "/proc/mounts"
-        Test earmarked for post Python 3 move as some changes were made to how read call
-        is interpreted in mock re readlines() and mock_open()
-        https://docs.python.org/3/library/unittest.mock.html#mock-open
-        :param self:
-        :return:
+        Test root_disk() for expected function of identifying the base device name for the
+        root mount point by mocking a range of builtins.open("/proc/mounts") returns.
         """
 
         # common SD/microSD card reader/driver device name:
         proc_mount_out = [
-            """
-/dev/mmcblk1p3 / btrfs rw,noatime,compress=lzo,ssd,space_cache,subvolid=258,subvol=/@/.snapshots/1/snapshot 0 0
-"""
+            "/dev/mmcblk1p3 / btrfs rw,noatime,compress=lzo,ssd,space_cache,subvolid=258,subvol=/@/.snapshots/1/snapshot 0 0\n"  # noqa E501
         ]
         expected_result = ["/dev/mmcblk1"]
+
         # nvme device:
         proc_mount_out.append(
-            """
-/dev/nvme0n1p3 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0
-"""
+            "/dev/nvme0n1p3 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0\n"
         )
         expected_result.append("/dev/nvme0n1")
+
         # md device, typically md126p3 or md0p3
         proc_mount_out.append(
-            """
-/dev/md126p3 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0
-"""
+            "/dev/md126p3 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0\n"
         )
         expected_result.append("/dev/md126")
+
         # Regular virtio device:
         proc_mount_out.append(
-            """
-/dev/vda3 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0
-"""
+            "/dev/vda3 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0\n"
         )
         expected_result.append("/dev/vda")
+
         # root in luks device:
         proc_mount_out.append(
-            """
-/dev/mapper/luks-705349f4-becf-4344-98a7-064ceba181e7 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0
-"""
+            "/dev/mapper/luks-705349f4-becf-4344-98a7-064ceba181e7 / btrfs rw,relatime,space_cache,subvolid=259,subvol=/@/.snapshots/1/snapshot 0 0\n"  # noqa E501
         )
         expected_result.append("/dev/mapper/luks-705349f4-becf-4344-98a7-064ceba181e7")
 
-        # Based on the following article:
-        # https://nickolaskraus.org/articles/how-to-mock-the-built-in-function-open/
-        # Python 3 has "builtins.open" note the "s"
         for proc_out, expected in zip(proc_mount_out, expected_result):
             fake_open = mock_open(read_data=proc_out)
             with patch("builtins.open", fake_open) as mocked_open:
                 returned = root_disk()
-                # We assert a single call was made on "/proc/mounts"
+                # We assert a single call to builtins.open was made using "/proc/mounts"
                 mocked_open.assert_called_once_with("/proc/mounts")
-
-                # The following shows that a handle.read().splitlines() works
-                # ... but not handle.readlines() such as we use in root_disk()
-                # But readlines() != splitlines() on break !!! readlines splits on \n only.
-                # https://discuss.python.org/t/changing-str-splitlines-to-match-file-readlines/174
-                mocked_open.assertEqual(returned, expected)
+                self.assertEqual(returned, expected)
 
     def test_run_command(self):
         self.patch_popen = patch("system.osi.subprocess.Popen")
