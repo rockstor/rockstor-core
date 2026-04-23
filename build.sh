fe9e5142750ae6a652838689afe25c343dec4951
Philip Guyton <philip@yewtreeapps.com>
2026-04-23 17:09:46 +0100

    build.sh errors - standalone use #3107
    Fix a path fault in an `echo` notification, and the
    associated `source` of our .env file that failed with
    consequences when not run under systemd rockstor-build.servcie
    via `EnvironmentFile=/opt/rockstor/.env` where the required
    contents were already established.

--- build.sh
+++ build.sh
@@ -88,10 +88,11 @@ fi
 /usr/bin/gpg --quick-generate-key --batch --passphrase '' rockstor@localhost || true
 # Init 'pass' in .env defined PASSWORD_STORE_DIR using above GPG key, and generate Django SECRET_KEY
 set -o allexport
-echo "Sourcing $(pwd).env"
-source .env  # also read by rockstor-build.service
+echo "Sourcing $(pwd)/.env"
+source $(pwd)/.env  # also read by rockstor-build.service via "EnvironmentFile=/opt/rockstor/.env"
 set +o allexport
-/usr/bin/pass init rockstor@localhost
+# Ensure password-store is initialized:
+/usr/bin/pass init rockstor@localhost || true
 /usr/bin/pass generate --no-symbols --force python-keyring/rockstor/SECRET_KEY 100
 
 # Collect all static files in the STATIC_ROOT subdirectory. See settings.py.
